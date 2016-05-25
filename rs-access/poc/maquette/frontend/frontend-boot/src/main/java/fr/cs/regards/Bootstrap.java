98fae8b837061b8c5aa6b797f0d5934469a962a0
Sebastien Binda <sebastien.binda@c-s.fr>
2016-05-25 18:41:33 +0200

    [rs-access] Packaging spring boot de la webapp

--- rs-access/poc/maquette/frontend/frontend-boot/src/main/java/fr/cs/regards/Bootstrap.java
+++ rs-access/poc/maquette/frontend/frontend-boot/src/main/java/fr/cs/regards/Bootstrap.java
@@ -2,12 +2,13 @@ package fr.cs.regards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class SampleJettyApplication {
+public class Bootstrap {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleJettyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Bootstrap.class, args);
+    }
 }
