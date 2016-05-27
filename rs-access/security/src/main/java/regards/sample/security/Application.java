b5795483f23583c3bad9333f33a1e4e8b6f07b09
Marc Sordi <marc.sordi@c-s.fr>
2016-05-27 14:27:57 +0200

    [rs-access] POC Security

---
+++ rs-access/security/src/main/java/regards/sample/security/Application.java
@@ -0,0 +1,16 @@
+package regards.sample.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableAuthorizationServer
+@EnableResourceServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ rs-access/security/src/main/java/regards/sample/security/auth/MethodAutorizationService.java
@@ -0,0 +1,11 @@
+package regards.sample.security.auth;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public interface MethodAutorizationService {
+
+	Optional<List<GrantedAuthority>> getAuthorities(ResourceAccess access);
+}
