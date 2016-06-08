3de9dec692090fe45b3c980b21a6bf00c358fe5d
Sebastien Binda <sebastien.binda@c-s.fr>
2016-06-08 11:00:08 +0200

    [rs-access] Split des tests.

--- rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/tests/portalApp/projects/ProjectsActionsTest.js
+++ rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/tests/portalApp/projects/ProjectsActionsTest.js
@@ -11,57 +11,7 @@ const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
 
 // Ce fichier permet de tester les actions et reducers liés aux projets
-
-describe('Testing Projects reducers', () => {
-  it('Should return the initial state', () => {
-    expect(reducers.projects(undefined, {})).to.eql({
-        isFetching : false,
-        items: [],
-        lastUpdate: ''
-      });
-  });
-
-  it('Should set projects fetching true in state', () => {
-    const initstate = {
-        isFetching : false,
-        items: [],
-        lastUpdate: ''
-    };
-
-    const action = requestProjects();
-
-    const expectedResult = {
-        isFetching : true,
-        items: [],
-        lastUpdate: ''
-    };
-
-    const result = reducers.projects(initstate, action);
-    expect(result).to.eql(expectedResult);
-  });
-
-  it('Should add projects to state', () => {
-    const initstate = {
-        isFetching : true,
-        items: [],
-        lastUpdate: ''
-    };
-
-    const action = receiveProjects([{"name":"cdpp"},{"name":"ssalto"}]);
-
-    const expectedResult = {
-        isFetching : false,
-        items: [{"name":"cdpp"},{"name":"ssalto"}],
-        lastUpdate: ''
-    };
-
-    const result = reducers.projects(initstate, action);
-    expectedResult.lastUpdate = result.lastUpdate;
-    expect(result).to.eql(expectedResult);
-  });
-});
-
-describe('Testing projects async actions. (Retreive projects from backend)', () => {
+describe('Testing projects actions.', () => {
 
   afterEach(() => {
     nock.cleanAll()
