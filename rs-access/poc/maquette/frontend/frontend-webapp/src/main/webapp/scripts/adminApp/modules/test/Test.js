fc833f1100504ffd6f0a760a98b483e63fe7cec4
Sebastien Binda <sebastien.binda@c-s.fr>
2016-06-13 17:46:28 +0200

    [rs-access] Gestion de sauvegarde du context dans l'url

--- rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/scripts/adminApp/modules/test/Test.js
+++ rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/scripts/adminApp/modules/test/Test.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getThemeStyles } from 'common/theme/ThemeUtils';
+import { browserHistory } from 'react-router'
 
 class Test extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      inputValue:''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentWillMount(){
+    // Read query parameters
+    const { value } = this.props.location.query;
+    this.setState({
+      inputValue: value
+    })
+  }
+
+  onChange(e){
+    let { value } = this.props.location.query;
+    value = e.target.value;
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  onClick(){
+    // Update query parameters of the current url
+    const { location } = this.props;
+    const route = location.pathname + "?value=" + this.state.inputValue;
+    browserHistory.push(route)
+  }
   render(){
     const styles = getThemeStyles(this.props.theme, 'adminApp/styles');
     return (
       <div className={styles["grid-basics-example"]}>
+        <input type='text' value={this.state.inputValue} onChange={this.onChange}/>
+        <input type="button" onClick={this.onClick} value="update"/>
         <div className={styles.row + " " + styles.display}>
           <div className={styles.columns + " " + styles["small-2"] + " " + styles["large-4"]}>4 columns</div>
           <div className={styles.columns + " " + styles["small-4"] + " " + styles["large-4"]}>4 columns</div>
@@ -25,6 +60,11 @@ class Test extends React.Component {
       </div>
     );
   }
+}
+
+Test.contextTypes = {
+  router: React.PropTypes.object,
+  route : React.PropTypes.object
 }
 
 const mapStateToProps = (state) => {
