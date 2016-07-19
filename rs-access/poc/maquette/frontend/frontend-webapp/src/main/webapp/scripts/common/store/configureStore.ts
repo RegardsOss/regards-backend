121d41a8c9f64b848e0eb4ce8fd0845a66ced1ef
Xavier <xbrochard@localhost.localdomain>
2016-07-19 11:07:53 +0200

    [rs-access] Added a configurable logger middlware

--- rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/scripts/common/store/configureStore.ts
+++ rs-access/poc/maquette/frontend/frontend-webapp/src/main/webapp/scripts/common/store/configureStore.ts
@@ -1,24 +1,30 @@
-import thunk from 'redux-thunk'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import loggerMiddleware from '../logger/ActionLoggerMiddleware'
+// Middlewares
+import thunk from 'redux-thunk'
 import authorizationMiddleware from '../authentication/AuthorizationMiddleware'
 var { apiMiddleware } = require('redux-api-middleware');
+import * as createLogger from 'redux-logger'
 // Root reducer
 import rootReducer from '../../reducer'
 
 export default function configureStore(preloadedState:any):any {
+  const logger = createLogger(); // Pass an options object for specific configuration
+
+  // Define the used middlewares (order matters)
+  const middlewares = [
+    thunk, // lets us dispatch() functions
+    authorizationMiddleware, // inject authorization headers in all request actions
+    apiMiddleware, // middleware for calling an REST API
+    logger // Logger must be the last middleware in chain, otherwise it will log thunk and promise, not actual actions
+  ]
+
   // Create the application store
   let win:any = window
   const store = createStore(
     rootReducer,
     preloadedState,
     compose(
-      applyMiddleware(
-        thunk, // lets us dispatch() functions
-        loggerMiddleware, // logs any dispatched action
-        authorizationMiddleware, // inject authorization headers in all request actions
-        apiMiddleware // middleware for calling an REST API
-      ),
+      applyMiddleware(...middlewares),
       win["devToolsExtension"] ? win["devToolsExtension"]() : (f:any) => f // Enable redux dev tools
     )
   )
