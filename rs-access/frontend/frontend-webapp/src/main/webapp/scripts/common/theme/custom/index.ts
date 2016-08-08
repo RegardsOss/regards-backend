6273ab7889db157dec3790a47daea6edbeab505d
lmieulet <leo.mieulet@c-s.fr>
2016-08-08 10:01:44 +0200

    [rs-access] ajoute des fichiers manquants

--- rs-access/frontend/frontend-webapp/src/main/webapp/scripts/common/theme/custom/index.ts
+++ rs-access/frontend/frontend-webapp/src/main/webapp/scripts/common/theme/custom/index.ts
@@ -3,7 +3,7 @@ import ssaltoTheme from "./ssaltoTheme"
 import lightBaseTheme from "./lightBaseTheme"
 import darkBaseTheme from "./darkBaseTheme"
 // Import here more custom themes
-
+//import './background.jpg'
 // Register them here
 export default {
   cdppTheme,
