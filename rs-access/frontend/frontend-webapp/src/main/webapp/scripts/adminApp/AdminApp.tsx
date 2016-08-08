cc424c309a6ef5340f1a0aa1d6e568bd25da36b2
lmieulet <leo.mieulet@c-s.fr>
2016-08-08 18:10:28 +0200

    [rs-access] Sépare la partie connexion de la partie layout. Mais ça ne fonctionne toujours pas si résolution > 1200px

--- rs-access/frontend/frontend-webapp/src/main/webapp/scripts/adminApp/AdminApp.tsx
+++ rs-access/frontend/frontend-webapp/src/main/webapp/scripts/adminApp/AdminApp.tsx
@@ -2,21 +2,12 @@
 import * as React from "react"
 import { connect } from "react-redux"
 import { logout } from "../common/authentication/AuthenticateActions"
-import Authentication from "./modules/authentication/containers/AuthenticationContainer"
 import { AuthenticationType } from "../common/authentication/AuthenticationTypes"
 import { isAuthenticated } from "../common/authentication/AuthenticateUtils"
-import Layout from "../common/layout/containers/Layout"
-import MenuContainer from "./modules/menu/containers/MenuContainer"
-import AppBar from "material-ui/AppBar"
-import IconMenu from "material-ui/IconMenu"
-import MenuItem from "material-ui/MenuItem"
-import IconButton from "material-ui/IconButton"
-import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert"
 import ThemeHelper from "../common/theme/ThemeHelper"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-import SelectTheme from "../common/theme/containers/SelectTheme"
-import SelectLanguage from "../common/i18n/containers/SelectLocaleContainer"
-
+import AuthenticationLayout from "./modules/authentication/containers/AuthenticationLayout"
+import MainAdminLayout from "./MainAdminLayout"
 interface AminAppProps {
   router: any,
   route: any,
@@ -28,25 +19,10 @@ interface AminAppProps {
   onLogout: () => void
 }
 
-const AdminAppBarIcon = (
-  <div>
-    <IconMenu
-      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-      anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-      targetOrigin={{horizontal: 'left', vertical: 'top'}}
-    >
-      <MenuItem primaryText="Refresh"/>
-      <MenuItem primaryText="Send feedback"/>
-      <MenuItem primaryText="Settings"/>
-      <MenuItem primaryText="Help"/>
-      <MenuItem primaryText="Sign out"/>
-    </IconMenu>
-  </div>
-)
 
 /**
  * React component to manage Administration application.
- * This component display admin layout or login form if the user is not connected
+ * This component displays admin layout or login form if the user is not connected
  */
 class AdminApp extends React.Component<AminAppProps, any> {
   constructor () {
@@ -56,38 +32,27 @@ class AdminApp extends React.Component<AminAppProps, any> {
 
   render (): JSX.Element {
     const {theme, authentication, content} = this.props
-
     // Build theme
     const muiTheme = ThemeHelper.getByName(theme)
 
     // Authentication
     const authenticated = isAuthenticated(authentication)
+    let hmi: any = [];
     if (authenticated === false) {
-      const layoutStyle = Object.assign({}, {display: "flex", alignItems: "center"}, muiTheme.adminApp.loginForm);
-      return (
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <Layout style={layoutStyle}>
-            <div key='selectTheme'><SelectTheme /></div>
-            <div key='authentication'><Authentication /></div>
-            <div key='selectLanguage'><SelectLanguage locales={['en','fr']}/></div>
-          </Layout>
-        </MuiThemeProvider>
-      )
+      hmi.push(<AuthenticationLayout key="1"/>)
     } else {
-      return (
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <div>
-            <Layout>
-              <div key='sideBar'><MenuContainer /></div>
-              <div key='appBar'><AppBar title="Regards admin dashboard" iconElementRight={AdminAppBarIcon}/></div>
-              <div key='content'>{content}</div>
-              <div key='selectTheme'><SelectTheme /></div>
-              <div key='selectLanguage'><SelectLanguage locales={['en','fr']}/></div>
-            </Layout>
-          </div>
-        </MuiThemeProvider>
-      )
+      hmi.push(<MainAdminLayout key="2" {...this.props}>
+        {content}
+      </MainAdminLayout>);
     }
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          {hmi}
+        </div>
+      </MuiThemeProvider>
+    )
+
   }
 }
 
