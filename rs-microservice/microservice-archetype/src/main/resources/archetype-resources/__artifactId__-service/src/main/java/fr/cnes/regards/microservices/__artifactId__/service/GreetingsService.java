804ade13b9a5e87ba2175e696bca9fa4a6f86ca2
Sebastien Binda <sebastien.binda@c-s.fr>
2016-08-11 18:49:39 +0200

    [rs-microservice] Correction archetype

--- rs-microservice/microservice-archetype/src/main/resources/archetype-resources/__artifactId__-service/src/main/java/fr/cnes/regards/microservices/__artifactId__/service/GreetingsService.java
+++ rs-microservice/microservice-archetype/src/main/resources/archetype-resources/__artifactId__-service/src/main/java/fr/cnes/regards/microservices/__artifactId__/service/GreetingsService.java
@@ -1,21 +1,16 @@
 package fr.cnes.regards.microservices.${artifactId}.service;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.stereotype.Service;
 
-import fr.cnes.regards.microservices.myMicroServicePlugin.domain.Greeting;
+import fr.cnes.regards.microservices.${artifactId}.domain.Greeting;
+import fr.cnes.regards.microservices.${artifactId}.service.actions.GreetingAction;
 
 @Service
 public class GreetingsService implements IGreetingsService {
 
-    private static final String template = "Hello, %s!";
-
-    private final AtomicLong counter = new AtomicLong();
-
     @Override
     public Greeting getGreeting(String pName) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, pName));
+        return new GreetingAction(pName).execute();
     }
 
 }
