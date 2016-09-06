1517aecb73e81b5e99e55530c5252dc85818bad0
xbrochard <xavier-alexandre.brochard@c-s.fr>
2016-09-06 15:33:52 +0200

    [rs-access] Implémentation bouchonnée de l'api projets

--- rs-access/backend/backend-mock/src/main/java/fr/cnes/regards/microservices/backend/controllers/administration/ProjectController.java
+++ rs-access/backend/backend-mock/src/main/java/fr/cnes/regards/microservices/backend/controllers/administration/ProjectController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -27,40 +29,90 @@ public class ProjectController {
     MethodAutorizationService authService_;
 
     /**
-     * Method to iniate REST resources authorizations.
+     * Method to initiate REST resources authorizations.
      */
     @PostConstruct
     public void initAuthorisations() {
         authService_.setAutorities("/api/project@GET", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
         authService_.setAutorities("/api/projects@GET", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
-    }
-
-    @ResourceAccess
-    @RequestMapping(value = "/project", method = RequestMethod.GET)
-    public @ResponseBody HttpEntity<Project> getProject(
-            @RequestParam(value = "name", required = true) String pProjectName) {
-        Project project = new Project("cdpp");
-        project.add(linkTo(methodOn(ProjectController.class).getProject("cdpp")).withSelfRel());
-        return new ResponseEntity<>(project, HttpStatus.OK);
+        authService_.setAutorities("/api/projects@POST", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
+        authService_.setAutorities("/api/projects/{project_id}@GET", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
+        authService_.setAutorities("/api/projects/{project_id}@PUT", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
+        authService_.setAutorities("/api/projects/{project_id}@DELETE", new RoleAuthority("PUBLIC"), new RoleAuthority("ADMIN"));
     }
 
     @ResourceAccess
     @RequestMapping(value = "/projects", method = RequestMethod.GET)
     public @ResponseBody HttpEntity<List<Project>> getProjects() {
+        return new ResponseEntity<>(getInMemory(), HttpStatus.OK);
+    }
+
+    @ResourceAccess
+    @RequestMapping(value = "/projects", method = RequestMethod.POST)
+    public @ResponseBody HttpEntity<Project> addProject() {
+        List<Project> projects = getInMemory();
+        Project project = new Project("newProject");
+        project.setProjectId(3L);
+        project.add(linkTo(methodOn(ProjectController.class).getProject(3L)).withSelfRel());
+        projects.add(project);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @ResourceAccess
+    @RequestMapping(value = "/projects/{project_id}", method = RequestMethod.GET)
+    public @ResponseBody HttpEntity<Project> getProject(@PathVariable("project_id") Long project_id) {
+        Logger.getGlobal().info("project_id" + project_id);
+        Project project = getInMemory()
+                .stream()
+                .filter(p -> p.getProjectId() == project_id)
+                .findFirst()
+                .get();
+
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @ResourceAccess
+    @RequestMapping(value = "/projects/{project_id}", method = RequestMethod.PUT)
+    public @ResponseBody HttpEntity<Project> updateProject(@PathVariable("project_id") Long project_id) {
+        Project project = getInMemory()
+                .stream()
+                .filter(p -> p.getProjectId() == project_id)
+                .findFirst()
+                .get();
+
+        project.setName("Updated name");
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @ResourceAccess
+    @RequestMapping(value = "/projects/{project_id}", method = RequestMethod.DELETE)
+    public @ResponseBody HttpEntity<List<Project>> deleteProject(@PathVariable("project_id") Long project_id) {
+        List<Project> projects = getInMemory()
+                .stream()
+                .filter(p -> p.getProjectId() != project_id)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    private List<Project> getInMemory() {
         List<Project> projects = new ArrayList<>();
 
         Project project = new Project("cdpp");
+        project.setProjectId(0L);
         String[] cdppAdmins = {"Alice", "David", "Bob"};
-        project.add(linkTo(methodOn(ProjectController.class).getProject("cdpp")).withSelfRel());
+        project.add(linkTo(methodOn(ProjectController.class).getProject(0L)).withSelfRel());
         project.add(linkTo(methodOn(ProjectAdminController.class).getProjectAdminsByNames(cdppAdmins)).withRel("users"));
         projects.add(project);
 
         project = new Project("ssalto");
+        project.setProjectId(1L);
         String[] ssaltoAdmins = {"Carl", "David"};
-        project.add(linkTo(methodOn(ProjectController.class).getProject("ssalto")).withSelfRel());
+        project.add(linkTo(methodOn(ProjectController.class).getProject(1L)).withSelfRel());
         project.add(linkTo(methodOn(ProjectAdminController.class).getProjectAdminsByNames(ssaltoAdmins)).withRel("users"));
         projects.add(project);
-        return new ResponseEntity<>(projects, HttpStatus.OK);
+
+        return projects;
     }
 
 }
