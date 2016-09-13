c6063d7ec7468c59d099cb7bc4948dc89c8a2745
Marc Sordi <marc.sordi@c-s.fr>
2016-09-13 17:52:45 +0200

    [rs-microservice] Identification et description d'une ressource

--- rs-microservice/microservice-core/src/main/java/fr/cnes/regards/microservices/core/auth/ResourceAccess.java
+++ rs-microservice/microservice-core/src/main/java/fr/cnes/regards/microservices/core/auth/ResourceAccess.java
@@ -1,6 +1,5 @@
 package fr.cnes.regards.microservices.core.auth;
 
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,9 +7,24 @@ import java.lang.annotation.Target;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
-
-@Target({ElementType.METHOD})
+@Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("VOID")
 public @interface ResourceAccess {
+
+    /**
+     * Set the name of the current resource.<br>
+     * Resource name is associated to the HTTP verb to identify the resource access (i.e. the endpoint). This
+     * association must be unique by microservice.
+     *
+     * @return resource name.
+     */
+    String name();
+
+    /**
+     * Describe the current feature
+     *
+     * @return feature description
+     */
+    String description();
 }
