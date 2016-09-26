fec2cb5d85cfc7a6423497d35044cb5ff9d2527b
xbrochard <xbrochard@localhost>
2016-09-26 14:55:06 +0200

    [rs-microservice] [TESTS] Added a "test" profile activation

--- rs-microservice/microservice-core-test/src/main/java/fr/cnes/regards/microservices/core/test/RegardsIntegrationTest.java
+++ rs-microservice/microservice-core-test/src/main/java/fr/cnes/regards/microservices/core/test/RegardsIntegrationTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -37,6 +38,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public abstract class RegardsIntegrationTest {
 
