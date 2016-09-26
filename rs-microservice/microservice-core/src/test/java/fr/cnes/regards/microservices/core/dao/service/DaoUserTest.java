32ad413b66de88433ae7440b7247598ca9091e24
Sebastien Binda <sebastien.binda@c-s.fr>
2016-09-26 13:35:19 +0200

    [rs-microservice] [DAO] Ajout de commentaires sut les tests de transactions.

--- rs-microservice/microservice-core/src/test/java/fr/cnes/regards/microservices/core/dao/service/DaoUserTest.java
+++ rs-microservice/microservice-core/src/test/java/fr/cnes/regards/microservices/core/dao/service/DaoUserTest.java
@@ -9,10 +9,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.cnes.regards.microservices.core.dao.MultiTenancyDaoTest;
 import fr.cnes.regards.microservices.core.dao.pojo.projects.User;
 import fr.cnes.regards.microservices.core.dao.repository.projects.UserRepository;
 import fr.cnes.regards.microservices.core.dao.util.CurrentTenantIdentifierResolverMock;
@@ -24,10 +23,10 @@ import fr.cnes.regards.microservices.core.dao.util.CurrentTenantIdentifierResolv
  * @author CS
  * @since 1.0-SNAPSHOT
  */
-@Service
-public class TransactionServiceTest {
+@Component
+public class DaoUserTest {
 
-    static final Logger LOG = LoggerFactory.getLogger(MultiTenancyDaoTest.class);
+    static final Logger LOG = LoggerFactory.getLogger(DaoUserTest.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -35,39 +34,61 @@ public class TransactionServiceTest {
     @Autowired
     private CurrentTenantIdentifierResolverMock tenantResolver;
 
+    /**
+     *
+     * Test adding a user witho error. Rollback must be done.
+     *
+     * @param pTenant
+     * @since 1.0-SNAPSHOT
+     */
     @Transactional(transactionManager = "projectsJpaTransactionManager", rollbackFor = Exception.class)
     public void addWithError(String pTenant) throws Exception {
-
         tenantResolver.setTenant(pTenant);
-
         User plop = userRepository.save(new User("doNotSave", "thisUser"));
         LOG.info("new user created id=" + plop.getId());
         plop = userRepository.save(new User("doNotSave", "thisUser"));
         LOG.info("new user created id=" + plop.getId());
         plop = userRepository.save(new User("doNotSave", "thisUser"));
         LOG.info("new user created id=" + plop.getId());
-
-        throw new Exception("plop");
+        throw new Exception("Generated test error to check for dao rollback");
 
     }
 
+    /**
+     *
+     * Test adding a user without error
+     *
+     * @param pTenant
+     * @since 1.0-SNAPSHOT
+     */
     public void addWithoutError(String pTenant) {
-
         tenantResolver.setTenant(pTenant);
-
         User plop = userRepository.save(new User("doNotSave", "thisUser"));
-
-        LOG.info("new user created id=" + plop.getId());
-
+        LOG.info("New user created id=" + plop.getId());
     }
 
+    /**
+     *
+     * Test getting all users from a given tenant
+     *
+     * @param pTenant
+     * @since 1.0-SNAPSHOT
+     */
     public List<User> getUsers(String pTenant) {
+        tenantResolver.setTenant(pTenant);
         Iterable<User> list = userRepository.findAll();
         List<User> results = new ArrayList<>();
         list.forEach(user -> results.add(user));
         return results;
     }
 
+    /**
+     *
+     * Test methid to delete all users from a given tenant
+     *
+     * @param pTenant
+     * @since 1.0-SNAPSHOT
+     */
     public void deleteAll(String pTenant) {
         tenantResolver.setTenant(pTenant);
         userRepository.deleteAll();
