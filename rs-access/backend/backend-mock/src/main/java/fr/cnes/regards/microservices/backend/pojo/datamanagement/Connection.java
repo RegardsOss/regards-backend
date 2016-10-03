ee8c5e7d4263f09d29544f657d9eb3556a06fb14
lmieulet <leo.mieulet@c-s.fr>
2016-10-03 11:39:33 +0200

    [rs-access] Replace the microservice to the root folder

--- rs-access/backend/backend-mock/src/main/java/fr/cnes/regards/microservices/backend/pojo/datamanagement/Connection.java
+++
@@ -1,9 +0,0 @@
-package fr.cnes.regards.microservices.backend.pojo.datamanagement;
-
-import org.springframework.hateoas.ResourceSupport;
-
-public class Connection extends ResourceSupport {
-    private Long connectionId;
-
-
-}
