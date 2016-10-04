c2473bc257f66b53e439cfe9c736b5d8e2e6c419
xbrochard <xbrochard@pc13381.idsi0.si.c-s.fr>
2016-10-04 10:33:13 +0200

    [rs-microservice] Relocated the test profile on RIT (was mistakenly moved)

--- rs-microservice/microservice-core-test/src/main/java/fr/cnes/regards/microservices/core/test/RegardsIntegrationTest.java
+++ rs-microservice/microservice-core-test/src/main/java/fr/cnes/regards/microservices/core/test/RegardsIntegrationTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,6 +41,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = { RegardsIntegrationTestConfiguration.class })
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public abstract class RegardsIntegrationTest {
 
     private Logger logger;
