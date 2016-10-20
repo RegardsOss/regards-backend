645da005d4c6da014caafea5eda3ece9e6edfb11
Marc Sordi <marc.sordi@c-s.fr>
2016-10-20 17:22:55 +0200

    [rs-microservice] regards integration test

--- rs-microservice/regards-starters/security-regards-autoconfigure/src/main/java/fr/cnes/regards/framework/security/autoconfigure/filter/JWTAuthenticationFilter.java
+++ rs-microservice/regards-starters/security-regards-autoconfigure/src/main/java/fr/cnes/regards/framework/security/autoconfigure/filter/JWTAuthenticationFilter.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
+import fr.cnes.regards.framework.security.autoconfigure.HttpConstants;
 import fr.cnes.regards.framework.security.utils.jwt.JWTAuthentication;
 
 /**
@@ -30,16 +31,6 @@ import fr.cnes.regards.framework.security.utils.jwt.JWTAuthentication;
  *
  */
 public class JWTAuthenticationFilter extends GenericFilterBean {
-
-    /**
-     * Authorization header
-     */
-    private static final String HEADER_AUTHORIZATION = "Authorization";
-
-    /**
-     * Authorization header scheme
-     */
-    private static final String HEADER_AUTH_SCHEMA = "Bearer";
 
     /**
      * Default security authentication manager
@@ -57,16 +48,16 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
         final HttpServletResponse response = (HttpServletResponse) pResponse;
 
         // Retrieve authentication header
-        String jwt = request.getHeader(HEADER_AUTHORIZATION);
+        String jwt = request.getHeader(HttpConstants.AUTHORIZATION);
         if (jwt == null) {
             throw new InsufficientAuthenticationException("Authorization header not found");
         }
 
         // Extract JWT from retrieved header
-        if (!jwt.startsWith(HEADER_AUTH_SCHEMA)) {
+        if (!jwt.startsWith(HttpConstants.BEARER)) {
             throw new InsufficientAuthenticationException("Authorization schema not found");
         }
-        jwt = jwt.substring(HEADER_AUTH_SCHEMA.length()).trim();
+        jwt = jwt.substring(HttpConstants.BEARER.length()).trim();
 
         // Init authentication object
         final Authentication jwtAuthentication = new JWTAuthentication(jwt);
