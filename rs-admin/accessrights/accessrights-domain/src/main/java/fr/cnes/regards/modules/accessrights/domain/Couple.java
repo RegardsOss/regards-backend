50b1e49aae2286971ede491178269535d1c0c796
svissier <sylvain.vissiere-guerinet@c-s.fr>
2016-11-09 10:30:03 +0100

    [rs-admin] sonar issue correction

--- rs-admin/accessrights/accessrights-domain/src/main/java/fr/cnes/regards/modules/accessrights/domain/Couple.java
+++ rs-admin/accessrights/accessrights-domain/src/main/java/fr/cnes/regards/modules/accessrights/domain/Couple.java
@@ -12,29 +12,30 @@ public class Couple<T, V> {
     private T first;
 
     private V second;
-    
-    public Couple(){}
+
+    public Couple() {
+        // default constructor
+    }
 
     public Couple(T t, V v) {
         this.first = t;
         this.second = v;
     }
 
-	public T getFirst() {
-		return first;
-	}
+    public T getFirst() {
+        return first;
+    }
 
-	public void setFirst(T first) {
-		this.first = first;
-	}
+    public void setFirst(T first) {
+        this.first = first;
+    }
 
-	public V getSecond() {
-		return second;
-	}
+    public V getSecond() {
+        return second;
+    }
 
-	public void setSecond(V second) {
-		this.second = second;
-	}
-
+    public void setSecond(V second) {
+        this.second = second;
+    }
 
 }
