31b80cbcc20a5d59642c001a173bbd1752f208c6
Sebastien Binda <sebastien.binda@c-s.fr>
2016-11-10 19:04:46 +0100

    [rs-microservice] ModuleEntityNotFoundException constructor change.

--- rs-microservice/regards-starters/module-regards/src/main/java/fr/cnes/regards/framework/module/rest/exception/ModuleEntityNotFoundException.java
+++ rs-microservice/regards-starters/module-regards/src/main/java/fr/cnes/regards/framework/module/rest/exception/ModuleEntityNotFoundException.java
@@ -13,7 +13,7 @@ public class ModuleEntityNotFoundException extends ModuleException {
 
     private static final long serialVersionUID = 8218291903574163437L;
 
-    public ModuleEntityNotFoundException(final Long pEntityIdentifier, final Class<?> pEntityClass) {
+    public ModuleEntityNotFoundException(final String pEntityIdentifier, final Class<?> pEntityClass) {
         super(String.format("Entity of type \"%s\" with id \"%s\" not found.", pEntityClass.getName(),
                             pEntityIdentifier));
     }
