fd8811fd813bb173ddc3677d338492f5d59bd59a
sbinda <sbinda@po14173LX>
2016-11-16 14:38:21 +0100

    [rs-microservice] [Microservice Web Mvc] Remove configureContentNegotiation in MicroserviceWebConfiguration

--- rs-microservice/regards-starters/microservice-regards/src/main/java/fr/cnes/regards/framework/microservice/web/MicroserviceWebConfiguration.java
+++ rs-microservice/regards-starters/microservice-regards/src/main/java/fr/cnes/regards/framework/microservice/web/MicroserviceWebConfiguration.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -31,14 +30,6 @@ public class MicroserviceWebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void configurePathMatch(final PathMatchConfigurer pMatcher) {
         pMatcher.setUseRegisteredSuffixPatternMatch(true);
-    }
-
-    @Override
-    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
-        // Tell Spring to not interprete the path extensions as request media type
-        // We must disable this in order to user emails in path variables (like "/user/user@email.com") otherwise
-        // Spring will try to interprete ".com" as a media type (just like ".pdf", ".html"...)
-        configurer.favorPathExtension(false);
     }
 
 }
