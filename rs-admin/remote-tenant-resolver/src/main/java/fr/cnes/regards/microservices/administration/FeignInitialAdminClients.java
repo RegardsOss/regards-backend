8372c81c5637a48a5c08bc39c398a867fb0a6233
sbinda <sebastien.binda@c-s.fr>
2016-11-23 18:07:51 +0100

    [rs-admin] Fix Feign clients use during microservice boot.

---
+++ rs-admin/remote-tenant-resolver/src/main/java/fr/cnes/regards/microservices/administration/FeignInitialAdminClients.java
@@ -0,0 +1,67 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.microservices.administration;
+
+import java.util.List;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
+import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
+
+import com.netflix.config.ConfigurationManager;
+
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.hystrix.HystrixFeign;
+import fr.cnes.regards.client.core.TokenClientProvider;
+import fr.cnes.regards.modules.project.client.rest.IProjectsClient;
+
+/**
+ *
+ * Class FeignInitialAdminClients
+ *
+ * During the microservice starting process (spring context boot), the feign clients are badly initialized and don'find
+ * the servers with the eureka registry. This class is used as a bean to initiate the needed clients to the
+ * administration service during the microservice boot process. Here the feign clients are manually configured to access
+ * the first instance found of microservice administration.
+ *
+ * @author Sébastien Binda
+ * @since 1.0-SNAPSHOT
+ */
+public class FeignInitialAdminClients {
+
+    /**
+     * Initial Feign client to administration service to retrieve informations about projects
+     */
+    private IProjectsClient projectsClient;
+
+    /**
+     * Eureka discovery client
+     */
+    private final DiscoveryClient discoveryClient;
+
+    public FeignInitialAdminClients(final DiscoveryClient pDiscoveryClient) {
+        super();
+        discoveryClient = pDiscoveryClient;
+
+        final List<ServiceInstance> instances = discoveryClient.getInstances("rs-admin");
+        if (!instances.isEmpty()) {
+            // Init hystrix configuration
+            ConfigurationManager.getConfigInstance().setProperty("hystrix.command.default.execution.isolation.strategy",
+                                                                 "SEMAPHORE");
+            ConfigurationManager.getConfigInstance()
+                    .setProperty("hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", 60000);
+
+            projectsClient = HystrixFeign.builder().contract(new SpringMvcContract()).encoder(new GsonEncoder())
+                    .decoder(new ResponseEntityDecoder(new GsonDecoder()))
+                    .target(new TokenClientProvider<>(IProjectsClient.class, instances.get(0).getUri().toString()));
+        }
+    }
+
+    public IProjectsClient getProjectsClient() {
+        return projectsClient;
+    }
+
+}
--- rs-admin/remote-tenant-resolver/src/main/java/fr/cnes/regards/microservices/administration/MicroserviceAutoConfiguration.java
+++ rs-admin/remote-tenant-resolver/src/main/java/fr/cnes/regards/microservices/administration/MicroserviceAutoConfiguration.java
@@ -3,24 +3,14 @@
  */
 package fr.cnes.regards.microservices.administration;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
-import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import feign.Feign;
-import feign.gson.GsonDecoder;
-import feign.gson.GsonEncoder;
-import fr.cnes.regards.client.core.TokenClientProvider;
 import fr.cnes.regards.framework.jpa.multitenant.autoconfigure.DataSourcesAutoConfiguration;
 import fr.cnes.regards.framework.jpa.multitenant.resolver.ITenantConnectionResolver;
 import fr.cnes.regards.framework.multitenant.autoconfigure.tenant.ITenantResolver;
@@ -57,12 +47,6 @@ public class MicroserviceAutoConfiguration {
     private JWTService jwtService;
 
     /**
-     * Eureka discovery client
-     */
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    /**
      *
      * multintenantResolver
      *
@@ -75,9 +59,8 @@ public class MicroserviceAutoConfiguration {
      */
     @Bean
     @ConditionalOnProperty(name = "regards.eureka.client.enabled", havingValue = "true", matchIfMissing = true)
-    ITenantConnectionResolver multintenantResolver(
-            @Qualifier("initProjectsClient") final IProjectsClient pProjectsClient,
-            @Qualifier("initProjectConnectionsClient") final IProjectConnectionClient pProjectConnectionClient) {
+    ITenantConnectionResolver multintenantResolver(final IProjectsClient pProjectsClient,
+            final IProjectConnectionClient pProjectConnectionClient) {
         return new MicroserviceTenantConnectionResolver(microserviceName, jwtService, pProjectsClient,
                 pProjectConnectionClient);
     }
@@ -95,8 +78,7 @@ public class MicroserviceAutoConfiguration {
      */
     @Bean
     @ConditionalOnProperty(name = "regards.eureka.client.enabled", havingValue = "true", matchIfMissing = true)
-    IAuthoritiesProvider authoritiesProvider(@Qualifier("initResourcesClient") final IResourcesClient pResourcesClient,
-            @Qualifier("initRolesClient") final IRolesClient pRolesClient) {
+    IAuthoritiesProvider authoritiesProvider(final IResourcesClient pResourcesClient, final IRolesClient pRolesClient) {
         return new MicroserviceAuthoritiesProvider(microserviceName, pResourcesClient, pRolesClient);
     }
 
@@ -104,65 +86,32 @@ public class MicroserviceAutoConfiguration {
      *
      * Remote tenant resolver. Retrieve tenants from the administration service.
      *
+     * @param pInitClients
+     *            Do not use the IProjectsClient. It must not be initialized at this time. To do so, we use the specials
+     *            administration clients manually configured {@link FeignInitialAdminClients}
+     *
      * @return RemoteTenantResolver
      * @since 1.0-SNAPSHOT
      */
     @Bean
     @ConditionalOnProperty(name = "regards.eureka.client.enabled", havingValue = "true", matchIfMissing = true)
-    ITenantResolver tenantResolver(@Qualifier("initProjectsClient") final IProjectsClient pProjectsClient) {
-        return new RemoteTenantResolver(pProjectsClient);
+    ITenantResolver tenantResolver(final FeignInitialAdminClients pInitClients) {
+        return new RemoteTenantResolver(pInitClients.getProjectsClient());
     }
 
+    /**
+     *
+     * Bean that contains initial Feign clients that cannot be initialized with feign annotations. Because there are
+     * used too early during the spring boot process.
+     *
+     * @param pDiscoveryClient
+     *            Eureka discovery client to find administration microservice
+     * @return Feign clients for administration service
+     * @since 1.0-SNAPSHOT
+     */
     @Bean
-    @Qualifier("initProjectsClient")
-    IProjectsClient getProjectsClient() {
-        final List<ServiceInstance> instances = discoveryClient.getInstances("rs-admin");
-        if (!instances.isEmpty()) {
-            return Feign.builder().contract(new SpringMvcContract()).encoder(new GsonEncoder())
-                    .decoder(new ResponseEntityDecoder(new GsonDecoder()))
-                    .target(new TokenClientProvider<>(IProjectsClient.class, instances.get(0).getUri().toString()));
-        } else {
-            return null;
-        }
-    }
-
-    @Bean
-    @Qualifier("initProjectConnectionsClient")
-    IProjectConnectionClient getProjectConnectionsClient() {
-        final List<ServiceInstance> instances = discoveryClient.getInstances("rs-admin");
-        if (!instances.isEmpty()) {
-            return Feign.builder().contract(new SpringMvcContract()).encoder(new GsonEncoder())
-                    .decoder(new ResponseEntityDecoder(new GsonDecoder())).target(new TokenClientProvider<>(
-                            IProjectConnectionClient.class, instances.get(0).getUri().toString()));
-        } else {
-            return null;
-        }
-    }
-
-    @Bean
-    @Qualifier("initResourcesClient")
-    public IResourcesClient getResourcesClient() {
-        final List<ServiceInstance> instances = discoveryClient.getInstances("rs-admin");
-        if (!instances.isEmpty()) {
-            return Feign.builder().contract(new SpringMvcContract()).encoder(new GsonEncoder())
-                    .decoder(new ResponseEntityDecoder(new GsonDecoder()))
-                    .target(new TokenClientProvider<>(IResourcesClient.class, instances.get(0).getUri().toString()));
-        } else {
-            return null;
-        }
-    }
-
-    @Bean
-    @Qualifier("initRolesClient")
-    public IRolesClient getRolesClient() {
-        final List<ServiceInstance> instances = discoveryClient.getInstances("rs-admin");
-        if (!instances.isEmpty()) {
-            return Feign.builder().contract(new SpringMvcContract()).encoder(new GsonEncoder())
-                    .decoder(new ResponseEntityDecoder(new GsonDecoder()))
-                    .target(new TokenClientProvider<>(IRolesClient.class, instances.get(0).getUri().toString()));
-        } else {
-            return null;
-        }
+    FeignInitialAdminClients initClients(final DiscoveryClient pDiscoveryClient) {
+        return new FeignInitialAdminClients(pDiscoveryClient);
     }
 
 }
