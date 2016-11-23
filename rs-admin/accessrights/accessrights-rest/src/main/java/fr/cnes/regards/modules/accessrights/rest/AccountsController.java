2c05af940ff59d9818f3db71a58c2d0c6328163e
xbrochar <xavier-alexandre.brochard@c-s.fr>
2016-11-23 15:11:50 +0100

    [rs-admin] [Accounts] Client and Controller do not use common signature anymore.

--- rs-admin/accessrights/accessrights-rest/src/main/java/fr/cnes/regards/modules/accessrights/rest/AccountsController.java
+++ rs-admin/accessrights/accessrights-rest/src/main/java/fr/cnes/regards/modules/accessrights/rest/AccountsController.java
@@ -16,7 +16,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.cnes.regards.framework.module.annotation.ModuleInfo;
@@ -32,12 +35,12 @@ import fr.cnes.regards.modules.accessrights.domain.instance.AccountSettings;
 import fr.cnes.regards.modules.accessrights.service.account.IAccountService;
 import fr.cnes.regards.modules.accessrights.service.account.IAccountSettingsService;
 import fr.cnes.regards.modules.accessrights.service.account.IAccountTransitions;
-import fr.cnes.regards.modules.accessrights.signature.IAccountsSignature;
 
 @RestController
 @ModuleInfo(name = "users", version = "1.0-SNAPSHOT", author = "REGARDS", legalOwner = "CS",
         documentation = "http://test")
-public class AccountsController implements IAccountsSignature {
+@RequestMapping(path = "/accounts")
+public class AccountsController {
 
     /**
      * Class logger
@@ -53,7 +56,13 @@ public class AccountsController implements IAccountsSignature {
     @Autowired
     private IAccountSettingsService accountSettingsService;
 
-    @Override
+    /**
+     * Retrieve the list of all {@link Account}s.
+     *
+     * @return The accounts list
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
     @ResourceAccess(description = "retrieve the list of account in the instance")
     public ResponseEntity<List<Resource<Account>>> retrieveAccountList() {
         final List<Account> accounts = accountService.retrieveAccountList();
@@ -62,7 +71,15 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Create a new account in state PENDING from the passed values
+     *
+     * @param pNewAccount
+     *            The data transfer object containing values to create the account from
+     * @return the created account
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
     @ResourceAccess(description = "create an new account")
     public ResponseEntity<Resource<Account>> createAccount(@Valid @RequestBody final Account pNewAccount)
             throws EntityException {
@@ -72,7 +89,15 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(resource, HttpStatus.CREATED);
     }
 
-    @Override
+    /**
+     * Retrieve the {@link Account} of passed <code>id</code>.
+     *
+     * @param pAccountId
+     *            The {@link Account}'s <code>id</code>
+     * @return The account
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_id}", method = RequestMethod.GET)
     @ResourceAccess(description = "retrieve the account account_id")
     public ResponseEntity<Resource<Account>> retrieveAccount(@PathVariable("account_id") final Long accountId)
             throws EntityNotFoundException {
@@ -81,7 +106,16 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
-    @Override
+    /**
+     *
+     * Retrieve an account by his unique email
+     *
+     * @param pAccountEmail
+     *            email of the account to retrieve
+     * @return Account
+     */
+    @ResponseBody
+    @RequestMapping(value = "/account/{account_email}", method = RequestMethod.GET)
     @ResourceAccess(description = "retrieve the account with his unique email")
     public ResponseEntity<Resource<Account>> retrieveAccounByEmail(
             @PathVariable("account_email") final String pAccountEmail) {
@@ -98,7 +132,16 @@ public class AccountsController implements IAccountsSignature {
 
     }
 
-    @Override
+    /**
+     * Update an {@link Account} with passed values.
+     *
+     * @param pAccountId
+     *            The <code>id</code> of the {@link Account} to update
+     * @param pUpdatedAccount
+     *            The new values to set
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_id}", method = RequestMethod.PUT)
     @ResourceAccess(description = "update the account account_id according to the body specified")
     public ResponseEntity<Void> updateAccount(@PathVariable("account_id") final Long accountId,
             @Valid @RequestBody final Account pUpdatedAccount) throws EntityException {
@@ -106,7 +149,15 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Remove on {@link Account} from db.<br>
+     * Only remove if no project user for any tenant.
+     *
+     * @param pAccountId
+     *            The account <code>id</code>
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_id}", method = RequestMethod.DELETE)
     @ResourceAccess(description = "remove the account account_id")
     public ResponseEntity<Void> removeAccount(@PathVariable("account_id") final Long pAccountId)
             throws ModuleException {
@@ -115,7 +166,17 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Do not respect REST architecture because the request comes from a mail client, ideally should be a PUT
+     *
+     * @param pAccountId
+     *            The account id
+     * @param pUnlockCode
+     *            the unlock code
+     * @return void
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_id}/unlock/{unlock_code}", method = RequestMethod.GET)
     @ResourceAccess(description = "unlock the account account_id according to the code unlock_code")
     public ResponseEntity<Void> unlockAccount(@PathVariable("account_id") final Long pAccountId,
             @PathVariable("unlock_code") final String pUnlockCode) throws ModuleException {
@@ -124,7 +185,18 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Change the passord of an {@link Account}.
+     *
+     * @param pAccountId
+     *            The {@link Account}'s <code>id</code>
+     * @param pResetCode
+     *            The reset code. Required to allow a password change
+     * @param pNewPassword
+     *            The new <code>password</code>
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_id}/password/{reset_code}", method = RequestMethod.PUT)
     @ResourceAccess(description = "change the passsword of account account_id according to the code reset_code")
     public ResponseEntity<Void> changeAccountPassword(@PathVariable("account_id") final Long accountId,
             @PathVariable("reset_code") final String resetCode, @Valid @RequestBody final String pNewPassword)
@@ -133,7 +205,18 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Send to the user an email containing a code:<br>
+     * - to reset password<br>
+     * - to unlock the account
+     *
+     * @param pEmail
+     *            The {@link Account}'s <code>email</code>
+     * @param pType
+     *            The type of code
+     */
+    @ResponseBody
+    @RequestMapping(value = "/code", method = RequestMethod.GET)
     @ResourceAccess(description = "send a code of type type to the email specified")
     public ResponseEntity<Void> sendAccountCode(@RequestParam("email") final String email,
             @RequestParam("type") final CodeType type) throws EntityNotFoundException {
@@ -141,25 +224,50 @@ public class AccountsController implements IAccountsSignature {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Retrieve the {@link AccountSettings} for the instance.
+     *
+     * @return The {@link AccountSettings} wrapped in a {@link Resource} and a {@link ResponseEntity}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
     @ResourceAccess(description = "retrieve the list of setting managing the accounts")
     public ResponseEntity<Resource<AccountSettings>> retrieveAccountSettings() {
         final AccountSettings settings = accountSettingsService.retrieve();
         return new ResponseEntity<>(new Resource<>(settings), HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Update the {@link AccountSettings} for the instance.
+     *
+     * @param pSettings
+     *            The {@link AccountSettings}
+     * @return The updated account settings
+     */
+    @ResponseBody
+    @RequestMapping(value = "/settings", method = RequestMethod.PUT)
     @ResourceAccess(description = "update the setting managing the account")
     public ResponseEntity<Void> updateAccountSetting(@Valid @RequestBody final AccountSettings pUpdatedAccountSetting) {
         accountSettingsService.update(pUpdatedAccountSetting);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Override
+    /**
+     * Return <code>true</code> if the passed <code>pPassword</code> is equal to the one set on the {@link Account} of
+     * passed <code>email</code>
+     *
+     * @param pEmail
+     *            The {@link Account}'s <code>email</code>
+     * @param pPassword
+     *            The password to check
+     * @return <code>true</code> if the password is valid, else <code>false</code>
+     */
+    @ResponseBody
+    @RequestMapping(value = "/{account_email}/validate", method = RequestMethod.GET)
     @ResourceAccess(description = "Validate the account password")
-    public ResponseEntity<AccountStatus> validatePassword(@PathVariable("account_login") final String pLogin,
+    public ResponseEntity<AccountStatus> validatePassword(@PathVariable("account_email") final String pEmail,
             @RequestParam("password") final String pPassword) throws EntityNotFoundException {
-        if (accountService.validatePassword(pLogin, pPassword)) {
+        if (accountService.validatePassword(pEmail, pPassword)) {
             return new ResponseEntity<>(AccountStatus.ACTIVE, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(AccountStatus.INACTIVE, HttpStatus.OK);
