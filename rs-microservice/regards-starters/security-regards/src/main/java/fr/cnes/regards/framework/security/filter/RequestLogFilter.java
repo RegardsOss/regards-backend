84abfc25a11da17b870f77d5ef2f74d5e4ea4daa
sbinda <sebastien.binda@c-s.fr>
2016-11-23 18:54:11 +0100

    [rs-microservice] Improve log of request received by a microservice with a new Filter.

---
+++ rs-microservice/regards-starters/security-regards/src/main/java/fr/cnes/regards/framework/security/filter/RequestLogFilter.java
@@ -0,0 +1,40 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.framework.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * Class RequestLogFilter
+ *
+ * Filter to log request received by a microservice
+ *
+ * @author Sébastien Binda
+ * @since 1.0-SNAPSHOT
+ */
+public class RequestLogFilter extends OncePerRequestFilter {
+
+    /**
+     * Class logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(RequestLogFilter.class);
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest pRequest, final HttpServletResponse pResponse,
+            final FilterChain pFilterChain) throws ServletException, IOException {
+        LOG.info("Request receive : " + pRequest.getRequestURI());
+        pFilterChain.doFilter(pRequest, pResponse);
+    }
+
+}
