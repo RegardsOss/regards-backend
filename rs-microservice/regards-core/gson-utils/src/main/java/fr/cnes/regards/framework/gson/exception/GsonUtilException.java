dac71489b476d072e979f6d0d9cc5bb2f83061a3
Marc Sordi <marc.sordi@c-s.fr>
2016-12-01 11:34:10 +0100

    [rs-microservice] set default role + trace hibernate + utils

---
+++ rs-microservice/regards-core/gson-utils/src/main/java/fr/cnes/regards/framework/gson/exception/GsonUtilException.java
@@ -0,0 +1,21 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.framework.gson.exception;
+
+/**
+ *
+ * Gson util exception
+ *
+ * @author Marc Sordi
+ *
+ */
+public class GsonUtilException extends Exception {
+
+    private static final long serialVersionUID = -8322539282517073620L;
+
+    public GsonUtilException(String pErrorMessage) {
+        super(pErrorMessage);
+    }
+
+}
---
+++ rs-microservice/regards-core/gson-utils/src/main/java/fr/cnes/regards/framework/gson/stream/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+/**
+ * GSON stream utils
+ * 
+ * @author Marc Sordi
+ *
+ */
+package fr.cnes.regards.framework.gson.stream;
