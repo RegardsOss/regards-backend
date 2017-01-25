9dd5da77d6bffbb886b402b565585449e458f7db
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-01-25 11:21:01 +0100

    [rs-storage] cleaning repo

--- rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/DataStorageManager.java
+++ rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/DataStorageManager.java
@@ -6,7 +6,9 @@ package fr.cnes.regards.modules.storage.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
 import fr.cnes.regards.modules.storage.domain.AIP;
+import fr.cnes.regards.modules.storage.plugins.datastorage.IDataStorage;
 
 /**
  * @author Sylvain Vissiere-Guerinet
@@ -17,6 +19,10 @@ public class DataStorageManager {
     // TODO
     @Autowired
     private IAllocationStrategy allocationStrategy;
+
+    private IPluginService pluginService;
+
+    private IDataStorage dataStorage;
 
     public void storeAip(AIP pAip) {
         // get All data storage impl
