b8a59bc83afdbb006616e6bf3dea4a3051208388
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-01-30 14:32:44 +0100

    [rs-dam] created DataSource with model for DataSet needs

---
+++ rs-dam/datasources/datasources-domain/src/main/java/fr/cnes/regards/modules/datasources/domain/DataSource.java
@@ -0,0 +1,30 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.datasources.domain;
+
+import fr.cnes.regards.modules.models.domain.Model;
+
+/**
+ * @author Sylvain Vissiere-Guerinet
+ *
+ *
+ *         FIXME: to be completed
+ */
+public class DataSource {
+
+    private Model modelOfObjects;
+
+    public DataSource() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Model getModelOfObjects() {
+        return modelOfObjects;
+    }
+
+    public void setModelOfObjects(Model pModelOfObjects) {
+        modelOfObjects = pModelOfObjects;
+    }
+
+}
