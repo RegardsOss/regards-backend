6c6593ee367359651ff6f53827b909682f216cbf
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-02-21 10:45:42 +0100

    [rs-catalog] added pluginInterface for filters, converter, representation, might be at the wrong place for now

---
+++ rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/IRepresentation.java
@@ -0,0 +1,15 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.search.service;
+
+import fr.cnes.regards.framework.modules.plugins.annotations.PluginInterface;
+
+/**
+ * @author Sylvain Vissiere-Guerinet
+ *
+ */
+@PluginInterface(description = "plugin interface for representation plugins")
+public interface IRepresentation {
+
+}
