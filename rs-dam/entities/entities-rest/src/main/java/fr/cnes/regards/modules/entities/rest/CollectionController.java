4d432277d087325c5a2893beab84afdb23142ab9
Olivier <olivier.rousselot@gmail.com>
2017-02-21 10:26:40 +0100

    [rs-dam] rename (first)

---
+++ rs-dam/entities/entities-rest/src/main/java/fr/cnes/regards/modules/entities/rest/CollectionController.java
@@ -0,0 +1,231 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.entities.rest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.cnes.regards.framework.hateoas.IResourceController;
+import fr.cnes.regards.framework.hateoas.IResourceService;
+import fr.cnes.regards.framework.hateoas.LinkRels;
+import fr.cnes.regards.framework.hateoas.MethodParamFactory;
+import fr.cnes.regards.framework.module.annotation.ModuleInfo;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
+import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.security.annotation.ResourceAccess;
+import fr.cnes.regards.modules.entities.domain.Collection;
+import fr.cnes.regards.modules.entities.service.ICollectionService;
+import fr.cnes.regards.modules.entities.urn.UniformResourceName;
+import fr.cnes.regards.plugins.utils.PluginUtilsException;
+
+/**
+ * @author lmieulet
+ *
+ */
+@RestController
+// CHECKSTYLE:OFF
+@ModuleInfo(name = "collections", version = "1.0-SNAPSHOT", author = "REGARDS", legalOwner = "CS",
+        documentation = "http://test")
+// CHECKSTYLE:ON
+@RequestMapping(value = "/collections")
+public class CollectionController implements IResourceController<Collection> {
+
+    /**
+     * Service
+     */
+    @Autowired
+    private ICollectionService collectionService;
+
+    /**
+     * HATEOAS service
+     */
+    @Autowired
+    private IResourceService resourceService;
+
+    /**
+     * Entry point to retrieve {@link Collection}s
+     *
+     * @return all {@link Collection}s
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResourceAccess(description = "endpoint to retrieve the list fo all collections")
+    public HttpEntity<List<Resource<Collection>>> retrieveCollections() {
+
+        final List<Collection> collections = collectionService.retrieveCollectionList();
+        final List<Resource<Collection>> resources = toResources(collections);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+
+    }
+
+    /**
+     * Entry point to retrieve a collection using its id
+     *
+     * @param pCollectionId
+     *            {@link Collection} id
+     * @return {@link Collection} as a {@link Resource}
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{collection_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResourceAccess(description = "Retrieve a collection")
+    public HttpEntity<Resource<Collection>> retrieveCollection(@PathVariable("collection_id") Long pCollectionId) {
+        final Collection collection = collectionService.retrieveCollectionById(pCollectionId);
+        final Resource<Collection> resource = toResource(collection);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    /**
+     * Entry point to update a collection using its id
+     *
+     * @param pCollectionId
+     *            {@link Collection} id
+     * @param pCollection
+     *            {@link Collection}
+     * @return update {@link Collection} as a {@link Resource}
+     * @throws ModuleException
+     *             if error occurs!
+     * @throws PluginUtilsException
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = "/{collection_id}")
+    @ResponseBody
+    @ResourceAccess(description = "Update a collection")
+    public HttpEntity<Resource<Collection>> updateCollection(@PathVariable("collection_id") Long pCollectionId,
+            @Valid @RequestBody Collection pCollection, BindingResult pResult)
+            throws ModuleException, PluginUtilsException {
+
+        // Validate dynamic model
+        collectionService.validate(pCollection, pResult, false);
+
+        final Collection collection = collectionService.update(pCollectionId, pCollection);
+        final Resource<Collection> resource = toResource(collection);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    /**
+     *
+     * Entry point to delete a collection using its id
+     *
+     * @param pCollectionId
+     *            {@link Collection} id
+     * @return nothing
+     * @throws EntityNotFoundException
+     * @throws PluginUtilsException
+     */
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{collection_id}")
+    @ResponseBody
+    @ResourceAccess(description = "delete the collection of collection_id")
+    public HttpEntity<Void> deleteCollection(@PathVariable("collection_id") Long pCollectionId)
+            throws EntityNotFoundException, PluginUtilsException {
+        collectionService.delete(pCollectionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Entry point to create a collection
+     *
+     * @param pCollection
+     *            {@link Collection} to create
+     * @param pResult
+     *            validation errors
+     * @return {@link Collection} as a {@link Resource}
+     * @throws ModuleException
+     *             if validation fails
+     * @throws IOException
+     * @throws PluginUtilsException
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    @ResourceAccess(description = "create a new collection according to what is passed as parameter")
+    public HttpEntity<Resource<Collection>> createCollection(@Valid @RequestBody Collection pCollection,
+            BindingResult pResult) throws ModuleException, IOException, PluginUtilsException {
+
+        // Validate dynamic model
+        collectionService.validate(pCollection, pResult, false);
+
+        final Collection collection = collectionService.create(pCollection, null);
+        final Resource<Collection> resource = toResource(collection);
+        return new ResponseEntity<>(resource, HttpStatus.CREATED);
+    }
+
+    /**
+     * Entry point to handle dissociation of {@link Collection} specified by its id to other entities
+     *
+     * @param pCollectionId
+     *            {@link Collection} id
+     * @param pToBeDissociated
+     *            entity to dissociate
+     * @return {@link Collection} as a {@link Resource}
+     * @throws ModuleException
+     *             if error occurs
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = "/{collection_id}/dissociate")
+    @ResponseBody
+    @ResourceAccess(description = "Dissociate a collection from  a list of entities")
+    public HttpEntity<Resource<Collection>> dissociateCollection(@PathVariable("collection_id") Long pCollectionId,
+            @Valid @RequestBody Set<UniformResourceName> pToBeDissociated) throws ModuleException {
+        final Collection collection = (Collection) collectionService.dissociate(pCollectionId,
+                                                                                        pToBeDissociated);
+        final Resource<Collection> resource = toResource(collection);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    /**
+     * Entry point to handle association of {@link Collection} specified by its id to other entities
+     *
+     * @param pCollectionId
+     *            {@link Collection} id
+     * @param pToBeAssociatedWith
+     *            entities to be associated
+     * @return {@link Collection} as a {@link Resource}
+     * @throws ModuleException
+     *             if error occurs
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = "/{collection_id}/associate")
+    @ResponseBody
+    @ResourceAccess(description = "associate the collection of id collection_id to the list of entities in parameter")
+    public HttpEntity<Resource<Collection>> associateCollections(@PathVariable("collection_id") Long pCollectionId,
+            @Valid @RequestBody Set<UniformResourceName> pToBeAssociatedWith) throws ModuleException {
+        final Collection collection = (Collection) collectionService.associate(pCollectionId,
+                                                                                       pToBeAssociatedWith);
+        final Resource<Collection> resource = toResource(collection);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    @Override
+    public Resource<Collection> toResource(Collection pElement, Object... pExtras) {
+        final Resource<Collection> resource = resourceService.toResource(pElement);
+        resourceService.addLink(resource, this.getClass(), "retrieveCollection", LinkRels.SELF,
+                                MethodParamFactory.build(Long.class, pElement.getId()));
+        resourceService.addLink(resource, this.getClass(), "retrieveCollectionList", LinkRels.LIST);
+        resourceService.addLink(resource, this.getClass(), "deleteCollection", LinkRels.DELETE,
+                                MethodParamFactory.build(Long.class, pElement.getId()));
+        resourceService.addLink(resource, this.getClass(), "updateCollection", LinkRels.UPDATE,
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(Collection.class));
+        resourceService.addLink(resource, this.getClass(), "dissociateCollection", "dissociate",
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(Set.class));
+        resourceService.addLink(resource, this.getClass(), "associateCollection", "associate",
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(Set.class));
+        return resource;
+    }
+}
---
+++ rs-dam/entities/entities-rest/src/main/java/fr/cnes/regards/modules/entities/rest/DatasetController.java
@@ -0,0 +1,212 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.entities.rest;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import fr.cnes.regards.framework.hateoas.IResourceController;
+import fr.cnes.regards.framework.hateoas.IResourceService;
+import fr.cnes.regards.framework.hateoas.LinkRels;
+import fr.cnes.regards.framework.hateoas.MethodParamFactory;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
+import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.security.annotation.ResourceAccess;
+import fr.cnes.regards.modules.entities.domain.DataSet;
+import fr.cnes.regards.modules.entities.domain.DescriptionFile;
+import fr.cnes.regards.modules.entities.service.DatasetService;
+import fr.cnes.regards.modules.entities.urn.UniformResourceName;
+import fr.cnes.regards.plugins.utils.PluginUtilsException;
+
+/**
+ * @author Sylvain Vissiere-Guerinet
+ *
+ */
+@RestController
+@RequestMapping(value = DatasetController.DATASET_PATH, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class DatasetController implements IResourceController<DataSet> {
+
+    public static final String DATASET_PATH = "/datasets";
+
+    public static final String DATASET_ID_PATH = "/{dataset_id}";
+
+    public static final String DATASET_ID_ASSOCIATE_PATH = DATASET_ID_PATH + "/associate";
+
+    public static final String DATASET_ID_DISSOCIATE_PATH = DATASET_ID_PATH + "/dissociate";
+
+    public static final String DATASET_ID_DESCRIPTION_PATH = DATASET_ID_PATH + "/description";
+
+    public static final String DATASET_ID_SERVICES_PATH = DATASET_ID_PATH + "/services";
+
+    @Autowired
+    private IResourceService resourceService;
+
+    @Autowired
+    private DatasetService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    @ResourceAccess(description = "create and send the dataset")
+    public HttpEntity<Resource<DataSet>> createDataset(@Valid @RequestPart("dataset") DataSet pDataSet,
+            @RequestPart("file") MultipartFile descriptionFile, BindingResult pResult)
+            throws ModuleException, IOException, PluginUtilsException {
+
+        // Validate dynamic model
+        service.validate(pDataSet, pResult, false);
+
+        DataSet created = service.create(pDataSet, descriptionFile);
+        return new ResponseEntity<>(toResource(created), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    @ResourceAccess(description = "endpoint to retrieve the list of all datasets")
+    public HttpEntity<PagedResources<Resource<DataSet>>> retrieveDatasets(final Pageable pPageable,
+            final PagedResourcesAssembler<DataSet> pAssembler) {
+
+        final Page<DataSet> datasets = service.retrieveDataSets(pPageable);
+        final PagedResources<Resource<DataSet>> resources = toPagedResources(datasets, pAssembler);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = DATASET_ID_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Retrieves a dataset")
+    public HttpEntity<Resource<DataSet>> retrieveDataset(@PathVariable("dataset_id") Long pDataSetId)
+            throws EntityNotFoundException {
+        DataSet dataSet = service.retrieveDataSet(pDataSetId);
+        final Resource<DataSet> resource = toResource(dataSet);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = DATASET_ID_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Deletes a dataset")
+    public HttpEntity<Void> deleteDataset(@PathVariable("dataset_id") Long pDataSetId)
+            throws EntityNotFoundException, PluginUtilsException {
+        service.delete(pDataSetId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = DATASET_ID_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Updates a DataSet")
+    public HttpEntity<Resource<DataSet>> updateDataset(@PathVariable("dataset_id") Long pDataSetId,
+            @Valid @RequestBody DataSet pDataSet, BindingResult pResult) throws ModuleException, PluginUtilsException {
+
+        // Validate dynamic model
+        service.validate(pDataSet, pResult, false);
+
+        DataSet dataSet = service.update(pDataSetId, pDataSet);
+        final Resource<DataSet> resource = toResource(dataSet);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = DATASET_ID_SERVICES_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Retrieves the list of service configurations of a DataSet")
+    public HttpEntity<List<Long>> retrieveDatasetServices(@PathVariable("dataset_id") Long pDataSetId)
+            throws EntityNotFoundException {
+        List<Long> services = service.retrieveDataSetServices(pDataSetId);
+        return new ResponseEntity<>(services, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = DATASET_ID_DESCRIPTION_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Retrieve the description file of the specified DataSet")
+    public HttpEntity<byte[]> retrieveDescription(@PathVariable("dataset_id") Long pDataSetId)
+            throws EntityNotFoundException {
+        DescriptionFile descriptionToSend = service.retrieveDataSetDescription(pDataSetId);
+        return null;
+    }
+
+    /**
+     * Entry point to handle dissociation of {@link DataSet} specified by its id to other entities
+     *
+     * @param pDataSetId
+     *            {@link DataSet} id
+     * @param pToBeDissociated
+     *            entity to dissociate
+     * @return {@link DataSet} as a {@link Resource}
+     * @throws ModuleException
+     *             if error occurs
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = DATASET_ID_DISSOCIATE_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "Dissociate a list of entities from a dataset")
+    public HttpEntity<Resource<DataSet>> dissociateDataset(@PathVariable("dataset_id") Long pDataSetId,
+            @Valid @RequestBody Set<UniformResourceName> pToBeDissociated) throws ModuleException {
+        final DataSet dataSet = (DataSet) service.dissociate(pDataSetId, pToBeDissociated);
+        final Resource<DataSet> resource = toResource(dataSet);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    /**
+     * Entry point to handle association of {@link DataSet} specified by its id to other entities
+     *
+     * @param pDataSetId
+     *            {@link DataSet} id
+     * @param pToBeAssociatedWith
+     *            entities to be associated
+     * @return {@link DataSet} as a {@link Resource}
+     * @throws ModuleException
+     *             if error occurs
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = DATASET_ID_ASSOCIATE_PATH)
+    @ResponseBody
+    @ResourceAccess(description = "associate the list of entities to the dataset")
+    public HttpEntity<Resource<DataSet>> associateDataset(@PathVariable("dataset_id") Long pDataSetId,
+            @Valid @RequestBody Set<UniformResourceName> pToBeAssociatedWith) throws ModuleException {
+        final DataSet dataset = (DataSet) service.associate(pDataSetId, pToBeAssociatedWith);
+        final Resource<DataSet> resource = toResource(dataset);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    @Override
+    public Resource<DataSet> toResource(DataSet pElement, Object... pExtras) {
+        final Resource<DataSet> resource = resourceService.toResource(pElement);
+        resourceService.addLink(resource, this.getClass(), "retrieveDataset", LinkRels.SELF,
+                                MethodParamFactory.build(Long.class, pElement.getId()));
+        resourceService.addLink(resource, this.getClass(), "retrieveDatasets", LinkRels.LIST,
+                                MethodParamFactory.build(Pageable.class),
+                                MethodParamFactory.build(PagedResourcesAssembler.class));
+        resourceService.addLink(resource, this.getClass(), "deleteDataset", LinkRels.DELETE,
+                                MethodParamFactory.build(Long.class, pElement.getId()));
+        resourceService.addLink(resource, this.getClass(), "updateDataset", LinkRels.UPDATE,
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(DataSet.class), MethodParamFactory.build(BindingResult.class));
+        resourceService.addLink(resource, this.getClass(), "dissociateDataset", "dissociate",
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(Set.class));
+        resourceService.addLink(resource, this.getClass(), "associateDataset", "associate",
+                                MethodParamFactory.build(Long.class, pElement.getId()),
+                                MethodParamFactory.build(Set.class));
+        return resource;
+    }
+
+}
