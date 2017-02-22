2ffb07280b2f5f709b70f3bcabe95c84950d6e61
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-02-22 11:36:57 +0100

    [rs-catalog] add IService interface, not implemented yet

---
+++ rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/IService.java
@@ -0,0 +1,18 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.search.service;
+
+import fr.cnes.regards.framework.modules.plugins.annotations.PluginInterface;
+
+/**
+ *
+ * TODO: implement and see what the interface shoudl define
+ *
+ * @author Sylvain Vissiere-Guerinet
+ *
+ */
+@PluginInterface(description = "TODO")
+public interface IService {
+
+}
