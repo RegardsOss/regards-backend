77c1666bc95a4018e6852688f98cda4fa5be826f
Marc Sordi <marc.sordi@c-s.fr>
2017-02-23 11:45:05 +0100

    [rs-microservice] add FIXME

--- rs-microservice/regards-starters/jpa-multitenant-regards/src/main/java/fr/cnes/regards/framework/jpa/multitenant/resolver/DataSourceBasedMultiTenantConnectionProviderImpl.java
+++ rs-microservice/regards-starters/jpa-multitenant-regards/src/main/java/fr/cnes/regards/framework/jpa/multitenant/resolver/DataSourceBasedMultiTenantConnectionProviderImpl.java
@@ -118,11 +118,11 @@ public class DataSourceBasedMultiTenantConnectionProviderImpl
      * @since 1.0-SNAPSHOT
      */
     private void listenForNewTenant() {
-        // FIXME delete comment if ok
-        // if (amqpSubscriber != null) {
-        final IHandler<NewTenantEvent> tenantHandler = new NewTenantHandler(this, microserviceName);
-        amqpSubscriber.subscribeTo(NewTenantEvent.class, tenantHandler);
-        // }
+        // FIXME this subscriber must not be null
+        if (amqpSubscriber != null) {
+            final IHandler<NewTenantEvent> tenantHandler = new NewTenantHandler(this, microserviceName);
+            amqpSubscriber.subscribeTo(NewTenantEvent.class, tenantHandler);
+        }
     }
 
     /**
