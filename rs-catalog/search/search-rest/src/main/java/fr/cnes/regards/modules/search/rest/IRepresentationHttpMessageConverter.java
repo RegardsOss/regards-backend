99665b2b4d95b21d01054b3ee3eda19743eebe4a
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-02-23 09:16:46 +0100

    [rs-catalog] added Client feign for plugins from catalog and mechanics for IRepresentation plugin

---
+++ rs-catalog/search/search-rest/src/main/java/fr/cnes/regards/modules/search/rest/IRepresentationHttpMessageConverter.java
@@ -0,0 +1,95 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.search.rest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
+import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
+import fr.cnes.regards.modules.search.domain.AbstractIndexedEntity;
+import fr.cnes.regards.modules.search.service.IRepresentation;
+
+/**
+ * @author Sylvain Vissiere-Guerinet
+ *
+ */
+public class IRepresentationHttpMessageConverter implements HttpMessageConverter<AbstractIndexedEntity> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IRepresentationHttpMessageConverter.class);
+
+    private final IPluginService pluginService;
+
+    /**
+     * Map of pluginConfiguration to be used for a specific MediaType
+     *
+     * TODO: handle deletion of pluginConf
+     */
+    private final Map<MediaType, PluginConfiguration> enabledRepresentationPluginMap;
+
+    public IRepresentationHttpMessageConverter(IPluginService pPluginService)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        super();
+        pluginService = pPluginService;
+        List<PluginConfiguration> representationConfigurations = pluginService
+                .getPluginConfigurationsByType(IRepresentation.class);
+        enabledRepresentationPluginMap = new HashMap<>();
+        for (PluginConfiguration representationConf : representationConfigurations) {
+            MediaType handledType = ((IRepresentation) Class.forName(representationConf.getPluginClassName())
+                    .newInstance()).getHandledMediaType();
+            enabledRepresentationPluginMap.put(handledType, representationConf);
+        }
+
+    }
+
+    @Override
+    public boolean canRead(Class<?> pClazz, MediaType pMediaType) {
+        // IRepresentation plugin are only to be used for outputs.
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Class<?> pClazz, MediaType pMediaType) {
+        return AbstractIndexedEntity.class.isInstance(pClazz);
+    }
+
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return new ArrayList<>(enabledRepresentationPluginMap.keySet());
+    }
+
+    @Override
+    public AbstractIndexedEntity read(Class<? extends AbstractIndexedEntity> pClazz, HttpInputMessage pInputMessage)
+            throws IOException, HttpMessageNotReadableException {
+        // should never be called as this converter cannot read anything
+        return null;
+    }
+
+    @Override
+    public void write(AbstractIndexedEntity pT, MediaType pContentType, HttpOutputMessage pOutputMessage)
+            throws IOException, HttpMessageNotWritableException {
+        PluginConfiguration confToUse = enabledRepresentationPluginMap.get(pContentType);
+        try {
+            IRepresentation pluginToUse = pluginService.getPlugin(confToUse.getId());
+            pOutputMessage.getBody().write(pluginToUse.transform(pT, pContentType.getCharset()));
+            pOutputMessage.getBody().flush();
+        } catch (ModuleException e) {
+            LOG.error(String.format("Could not instance a plugin for the required media type %s", pContentType), e);
+        }
+    }
+
+}
