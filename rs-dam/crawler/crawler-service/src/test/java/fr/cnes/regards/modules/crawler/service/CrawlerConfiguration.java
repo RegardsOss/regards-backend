efdde5f178dc555ef5604802ca327f8ffb89a54c
Olivier <olivier.rousselot@gmail.com>
2017-03-09 11:54:09 +0100

    [rs-dam] Tests

--- rs-dam/crawler/crawler-service/src/test/java/fr/cnes/regards/modules/crawler/service/CrawlerConfiguration.java
+++ rs-dam/crawler/crawler-service/src/test/java/fr/cnes/regards/modules/crawler/service/CrawlerConfiguration.java
@@ -4,25 +4,21 @@ import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import fr.cnes.regards.framework.hateoas.IResourceService;
 
 @Configuration
-@ComponentScan(
-        basePackages = { "fr.cnes.regards.modules.crawler", "fr.cnes.regards.modules.entities",
-                "fr.cnes.regards.modules.models", "fr.cnes.regards.modules.datasources",
-                "fr.cnes.regards.framework.modules.plugins.service" },
-        excludeFilters = { @Filter(classes = { CrawlerConfiguration.class }, type = FilterType.ASSIGNABLE_TYPE) })
+@ComponentScan(basePackages = { "fr.cnes.regards.modules.crawler", "fr.cnes.regards.modules.entities",
+        "fr.cnes.regards.modules.models", "fr.cnes.regards.modules.datasources",
+        "fr.cnes.regards.framework.modules.plugins.service" })
 @EnableAutoConfiguration
 @PropertySource(value = { "classpath:test.properties", "classpath:test_ds.properties",
         "classpath:test_${user.name}.properties" }, ignoreResourceNotFound = true)
 @EnableAsync
-public class CrawlerConfiguration4DataSource {
+public class CrawlerConfiguration {
 
     @Bean
     public IResourceService getResourceService() {
