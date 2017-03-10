0ca6a6b1f390b72261e35b7ffb0ce85834ae42e8
Olivier <olivier.rousselot@gmail.com>
2017-03-10 17:21:39 +0100

    [rs-dam] Test

--- rs-dam/crawler/crawler-service/src/test/java/fr/cnes/regards/modules/crawler/service/CrawlerServiceTest.java
+++ rs-dam/crawler/crawler-service/src/test/java/fr/cnes/regards/modules/crawler/service/CrawlerServiceTest.java
@@ -28,6 +28,7 @@ import com.google.common.collect.Sets;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginParameter;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginParametersFactory;
+import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
 import fr.cnes.regards.modules.datasources.domain.DataSourceAttributeMapping;
 import fr.cnes.regards.modules.datasources.domain.DataSourceModelMapping;
 import fr.cnes.regards.modules.datasources.plugins.DefaultOracleConnectionPlugin;
@@ -86,8 +87,12 @@ public class CrawlerServiceTest {
 
     private final ModelMappingAdapter adapter = new ModelMappingAdapter();
 
+    @Autowired
+    private IRuntimeTenantResolver tenantResolver;
+
     @Before
     public void tearUp() throws DataSourcesPluginException, PluginUtilsException, SQLException {
+        tenantResolver.forceTenant(TENANT);
         /*
          * Initialize the DataSourceAttributeMapping
          */
