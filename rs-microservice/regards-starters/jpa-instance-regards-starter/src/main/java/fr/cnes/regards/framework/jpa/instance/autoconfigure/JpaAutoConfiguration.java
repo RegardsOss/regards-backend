018c0631276818db3a795e470492452630683816
sbinda <sebastien.binda@c-s.fr>
2017-03-31 18:20:57 +0200

    [rs-microservice] [JPA instance starter] Fix disable InstanceEntity annotation.

--- rs-microservice/regards-starters/jpa-instance-regards-starter/src/main/java/fr/cnes/regards/framework/jpa/instance/autoconfigure/JpaAutoConfiguration.java
+++ rs-microservice/regards-starters/jpa-instance-regards-starter/src/main/java/fr/cnes/regards/framework/jpa/instance/autoconfigure/JpaAutoConfiguration.java
@@ -5,18 +5,17 @@ package fr.cnes.regards.framework.jpa.instance.autoconfigure;
 
 import java.lang.annotation.Annotation;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import fr.cnes.regards.framework.gson.autoconfigure.GsonAutoConfiguration;
-import fr.cnes.regards.framework.jpa.annotation.InstanceEntity;
 import fr.cnes.regards.framework.jpa.exception.MultiDataBasesException;
 import fr.cnes.regards.framework.jpa.instance.properties.InstanceDaoProperties;
 import fr.cnes.regards.framework.jpa.utils.DaoUtils;
@@ -25,31 +24,28 @@ import fr.cnes.regards.framework.jpa.utils.DaoUtils;
  *
  * Class InstanceJpaAutoConfiguration
  *
- * Configuration class to define hibernate/jpa instance database strategy This class use @InstanceEntity annotation to
- * find JPA Entities and Repositories.
+ * Configuration class to define hibernate/jpa instance database strategy
  *
  * @author Sébastien Binda
  * @since 1.0-SNAPSHOT
  */
 @Configuration
-@Conditional(value = EnableInstanceCondition.class)
-@EnableJpaRepositories(
-        includeFilters = { @ComponentScan.Filter(value = InstanceEntity.class, type = FilterType.ANNOTATION) },
-        basePackages = DaoUtils.ROOT_PACKAGE, entityManagerFactoryRef = "instanceEntityManagerFactory",
+@Conditional(value = DisableInstanceCondition.class)
+@EnableJpaRepositories(basePackages = DaoUtils.ROOT_PACKAGE, entityManagerFactoryRef = "instanceEntityManagerFactory",
         transactionManagerRef = InstanceDaoProperties.INSTANCE_TRANSACTION_MANAGER)
 @EnableTransactionManagement
 @EnableConfigurationProperties(InstanceDaoProperties.class)
 @ConditionalOnProperty(prefix = "regards.jpa", name = "instance.enabled", matchIfMissing = true)
 @AutoConfigureAfter(value = { GsonAutoConfiguration.class })
-public class InstanceJpaAutoConfiguration extends AbstractJpaAutoConfiguration {
+public class JpaAutoConfiguration extends AbstractJpaAutoConfiguration {
 
-    public InstanceJpaAutoConfiguration() throws MultiDataBasesException {
+    public JpaAutoConfiguration() throws MultiDataBasesException {
         super();
     }
 
     @Override
     public Class<? extends Annotation> getEntityAnnotationScan() {
-        return InstanceEntity.class;
+        return Entity.class;
     }
 
 }
