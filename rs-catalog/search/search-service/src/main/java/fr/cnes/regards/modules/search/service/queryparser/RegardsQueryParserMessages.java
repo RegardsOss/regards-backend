fdc8da0a51cdf9271731708872ebe4e2941fa7aa
Olivier <olivier.rousselot@gmail.com>
2017-04-03 17:56:56 +0200

    [rs-catalog] Sonar

--- rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/queryparser/RegardsQueryParserMessages.java
+++ rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/queryparser/RegardsQueryParserMessages.java
@@ -7,7 +7,7 @@ import org.apache.lucene.queryparser.flexible.messages.NLS;
  * <a>resources/RegardsQueryParserMessages.properties</a>
  *
  * Do <b>*NOT*</b> listen to SonarQube about making the fields final and providing accessors.
- * This code implements the correct {@link NLS} API (it is inspired from Lucene classes), wheter or not SonarQube is happy about it.
+ * This code implements the correct {@link NLS} API (it is inspired from Lucene classes), wether or not SonarQube is happy about it.
  *
  * @author Xavier-Alexandre Brochard
  */
@@ -16,13 +16,13 @@ public class RegardsQueryParserMessages extends NLS {
     private static final String BUNDLE_NAME = RegardsQueryParserMessages.class.getName();
 
     // static string must match the strings in the property files.
-    public static String FIELD_TYPE_UNDETERMINATED;
+    public static String FIELD_TYPE_UNDETERMINATED; // NOSONAR
 
-    public static String UNSUPPORTED_ATTRIBUTE_TYPE;
+    public static String UNSUPPORTED_ATTRIBUTE_TYPE; // NOSONAR
 
-    public static String MIDDLE_WILDCARD_NOT_ALLOWED;
+    public static String MIDDLE_WILDCARD_NOT_ALLOWED; // NOSONAR
 
-    public static String RANGE_NUMERIC_CANNOT_BE_EMPTY;
+    public static String RANGE_NUMERIC_CANNOT_BE_EMPTY; // NOSONAR
 
     private RegardsQueryParserMessages() {
         // Do not instantiate
