cbf534222d403f39cbcd27e693cdf701884bfdfd
Marc Sordi <marc.sordi@c-s.fr>
2017-04-05 14:04:40 +0200

    [rs-microservice] manage bootsrap static tenant

--- rs-microservice/regards-starters/multitenant-regards-starter/src/main/java/fr/cnes/regards/framework/multitenant/autoconfigure/MultitenantAutoConfiguration.java
+++ rs-microservice/regards-starters/multitenant-regards-starter/src/main/java/fr/cnes/regards/framework/multitenant/autoconfigure/MultitenantAutoConfiguration.java
@@ -5,6 +5,7 @@ package fr.cnes.regards.framework.multitenant.autoconfigure;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +22,7 @@ import fr.cnes.regards.framework.multitenant.test.SingleRuntimeTenantResolver;
  *
  */
 @Configuration
+@EnableConfigurationProperties(MultitenantBootstrapProperties.class)
 public class MultitenantAutoConfiguration {
 
     /**
