3992aff0bb4511d8823f2165668c646f5fe61d17
Olivier <olivier.rousselot@gmail.com>
2017-04-19 16:02:21 +0200

    [rs-dam] Sonar corrections

--- rs-dam/crawler/crawler-service/src/main/java/fr/cnes/regards/modules/crawler/service/CrawlerInitializer.java
+++ rs-dam/crawler/crawler-service/src/main/java/fr/cnes/regards/modules/crawler/service/CrawlerInitializer.java
@@ -1,10 +1,6 @@
 package fr.cnes.regards.modules.crawler.service;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
