b27a5a0059769a53122645cb6edc3ec3c893a54d
Olivier <olivier.rousselot@gmail.com>
2017-04-28 16:36:59 +0200

    [rs-microservice] Set UTC ISO 8601 date

--- rs-microservice/regards-starters/gson-regards/src/main/java/fr/cnes/regards/framework/gson/adapters/LocalDateTimeAdapter.java
+++ rs-microservice/regards-starters/gson-regards/src/main/java/fr/cnes/regards/framework/gson/adapters/LocalDateTimeAdapter.java
@@ -22,30 +22,30 @@ import com.google.gson.stream.JsonWriter;
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 
     /**
-     * ISO date time official support
+     * ISO date time official support (UTC)
      */
-    private static final DateTimeFormatter ISO_DATE_TIME_OPTIONAL_OFFSET;
+    private static final DateTimeFormatter ISO_DATE_TIME_UTC;
     static {
-        ISO_DATE_TIME_OPTIONAL_OFFSET = new DateTimeFormatterBuilder().parseCaseInsensitive()
-                .append(DateTimeFormatter.ISO_LOCAL_DATE_TIME).optionalStart().appendOffsetId().toFormatter();
+        ISO_DATE_TIME_UTC = new DateTimeFormatterBuilder().parseCaseInsensitive()
+                .append(DateTimeFormatter.ISO_INSTANT).optionalStart().appendOffsetId().toFormatter();
     }
 
     @Override
     public void write(JsonWriter pOut, LocalDateTime pValue) throws IOException {
-        pOut.value(pValue.format(ISO_DATE_TIME_OPTIONAL_OFFSET));
+        pOut.value(pValue.format(ISO_DATE_TIME_UTC));
     }
 
     @Override
     public LocalDateTime read(JsonReader pIn) throws IOException {
-        return LocalDateTime.parse(pIn.nextString(), ISO_DATE_TIME_OPTIONAL_OFFSET);
+        return LocalDateTime.parse(pIn.nextString(), ISO_DATE_TIME_UTC);
     }
 
     public static LocalDateTime parse(String date) {
-        return LocalDateTime.from(ISO_DATE_TIME_OPTIONAL_OFFSET.parse(date));
+        return LocalDateTime.from(ISO_DATE_TIME_UTC.parse(date));
     }
 
     public static String format(LocalDateTime date) {
-        return ISO_DATE_TIME_OPTIONAL_OFFSET.format(date);
+        return ISO_DATE_TIME_UTC.format(date);
     }
 
 }
