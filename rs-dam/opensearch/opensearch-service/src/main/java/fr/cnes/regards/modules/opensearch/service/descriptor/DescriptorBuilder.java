eeca7adc17dc110bacc77b3329d499a8e549130f
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-05-05 17:44:17 +0200

    [rs-dam] WIP: initialization

---
+++ rs-dam/opensearch/opensearch-service/src/main/java/fr/cnes/regards/modules/opensearch/service/descriptor/DescriptorBuilder.java
@@ -0,0 +1,13 @@
+/*
+ * LICENSE_PLACEHOLDER
+ */
+package fr.cnes.regards.modules.opensearch.service.descriptor;
+
+
+/**
+ * @author Sylvain Vissiere-Guerinet
+ *
+ */
+public class DescriptorBuilder {
+
+}
