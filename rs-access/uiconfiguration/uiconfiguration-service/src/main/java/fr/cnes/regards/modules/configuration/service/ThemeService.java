ca676f0aebb1ffebab9ece1ec67a27aedfbdc528
sbinda <sebastien.binda@c-s.fr>
2017-05-10 19:37:32 +0200

    [rs-access] Initialize default UI themes.

--- rs-access/uiconfiguration/uiconfiguration-service/src/main/java/fr/cnes/regards/modules/configuration/service/ThemeService.java
+++ rs-access/uiconfiguration/uiconfiguration-service/src/main/java/fr/cnes/regards/modules/configuration/service/ThemeService.java
@@ -3,9 +3,14 @@
  */
 package fr.cnes.regards.modules.configuration.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,9 +19,27 @@ import fr.cnes.regards.framework.module.rest.exception.EntityInvalidException;
 import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.modules.configuration.dao.IThemeRepository;
 import fr.cnes.regards.modules.configuration.domain.Theme;
+import fr.cnes.regards.modules.configuration.service.exception.InitUIException;
 
 @Service(value = "themeService")
-public class ThemeService implements IThemeService {
+public class ThemeService extends AbstractUiConfigurationService implements IThemeService {
+
+    /**
+     * Class logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ModuleService.class);
+
+    /**
+     * The default configuration for dark theme
+     */
+    @Value("classpath:DefaultDarkTheme.json")
+    private Resource defaultDarkThemeResource;
+
+    /**
+     * The default configuration for light theme
+     */
+    @Value("classpath:DefaultLightTheme.json")
+    private Resource defaultLightThemeResource;
 
     @Autowired
     private IThemeRepository repository;
@@ -81,6 +104,41 @@ public class ThemeService implements IThemeService {
             theme.setActive(false);
             repository.save(theme);
         }
+    }
+
+    @Override
+    protected void initProjectUI(final String pTenant) {
+        final List<Theme> themes = repository.findAll();
+        if ((themes == null) || themes.isEmpty()) {
+            final Theme defaultTheme = new Theme();
+            defaultTheme.setName("Dark");
+            defaultTheme.setActive(true);
+            try {
+                defaultTheme.setConfiguration(readDefaultFileResource(defaultDarkThemeResource));
+            } catch (final IOException e) {
+                LOG.error(e.getMessage(), e);
+                throw new InitUIException(e);
+            }
+            repository.save(defaultTheme);
+
+            final Theme defaultLightTheme = new Theme();
+            defaultLightTheme.setName("Light");
+            defaultLightTheme.setActive(false);
+            try {
+                defaultLightTheme.setConfiguration(readDefaultFileResource(defaultLightThemeResource));
+            } catch (final IOException e) {
+                LOG.error(e.getMessage(), e);
+                throw new InitUIException(e);
+            }
+            repository.save(defaultLightTheme);
+        }
+
+    }
+
+    @Override
+    protected void initInstanceUI() {
+        // Initialize with the same default theme as projects
+        this.initProjectUI(null);
     }
 
 }
