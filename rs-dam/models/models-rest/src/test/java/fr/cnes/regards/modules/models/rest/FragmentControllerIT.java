c8566e0e2152ae9d4667093b68407aecb7f5b1ee
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-05-18 09:43:07 +0200

    [rs-dam] lets hope it works.....

--- rs-dam/models/models-rest/src/test/java/fr/cnes/regards/modules/models/rest/FragmentControllerIT.java
+++ rs-dam/models/models-rest/src/test/java/fr/cnes/regards/modules/models/rest/FragmentControllerIT.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,6 +68,11 @@ public class FragmentControllerIT extends AbstractRegardsTransactionalIT {
     @Override
     protected Logger getLogger() {
         return LOGGER;
+    }
+
+    @Before
+    public void cleanUp() {
+        fragmentRepository.deleteAll();
     }
 
     @Test
