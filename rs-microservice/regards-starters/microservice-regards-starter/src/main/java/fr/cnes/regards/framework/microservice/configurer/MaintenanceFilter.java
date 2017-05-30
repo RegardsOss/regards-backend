a02d6f9b415700d0de1e1560289f1ce5af265548
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-05-30 10:38:48 +0200

    [rs-microservice] add log into maintenance filter and fix test

--- rs-microservice/regards-starters/microservice-regards-starter/src/main/java/fr/cnes/regards/framework/microservice/configurer/MaintenanceFilter.java
+++ rs-microservice/regards-starters/microservice-regards-starter/src/main/java/fr/cnes/regards/framework/microservice/configurer/MaintenanceFilter.java
@@ -3,17 +3,19 @@
  */
 package fr.cnes.regards.framework.microservice.configurer;
 
-import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.google.common.net.HttpHeaders;
 import fr.cnes.regards.framework.microservice.manager.MaintenanceManager;
 import fr.cnes.regards.framework.microservice.rest.MaintenanceController;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
@@ -24,6 +26,13 @@ import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
  *
  */
 public class MaintenanceFilter extends OncePerRequestFilter {
+
+    /**
+     * {verb}@{path} request from {x-forwarded-for} was ignored because the service is in Maintenance!
+     */
+    private static String REQUEST_IGNORED="{}@{} request from {} was ignored because the service is in Maintenance!";
+
+    private static Logger LOG= LoggerFactory.getLogger(MaintenanceFilter.class);
 
     private final IRuntimeTenantResolver resolver;
 
@@ -43,6 +52,8 @@ public class MaintenanceFilter extends OncePerRequestFilter {
                     && pRequest.getRequestURI().contains(MaintenanceController.MAINTENANCE_URL)
                     && pRequest.getRequestURI().contains(MaintenanceController.DISABLE))
                     && MaintenanceManager.getMaintenance(resolver.getTenant())) {
+                LOG.error(REQUEST_IGNORED, pRequest.getMethod(), pRequest.getRequestURI(), pRequest.getHeader(
+                        HttpHeaders.X_FORWARDED_FOR));
                 pResponse.sendError(HttpStatus.SERVICE_UNAVAILABLE.value(), "Tenant in maintenance");
             } else {
                 pFilterChain.doFilter(pRequest, pResponse);
