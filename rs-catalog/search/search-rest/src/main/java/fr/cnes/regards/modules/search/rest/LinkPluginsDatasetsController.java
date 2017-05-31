2b25eb49a5eedd37703dc47f0de6a26f58f0608c
lmieulet <leo.mieulet@c-s.fr>
2017-05-31 17:55:19 +0200

    [rs-catalog] Update LinkPluginsDatasetsController to return Resource<LinkPluginsDatasets>

--- rs-catalog/search/search-rest/src/main/java/fr/cnes/regards/modules/search/rest/LinkPluginsDatasetsController.java
+++ rs-catalog/search/search-rest/src/main/java/fr/cnes/regards/modules/search/rest/LinkPluginsDatasetsController.java
@@ -3,7 +3,9 @@
  */
 package fr.cnes.regards.modules.search.rest;
 
+import fr.cnes.regards.framework.hateoas.IResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.cnes.regards.framework.hateoas.IResourceService;
 import fr.cnes.regards.framework.module.rest.exception.EntityException;
 import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.security.annotation.ResourceAccess;
@@ -32,22 +35,31 @@ public class LinkPluginsDatasetsController {
     @Autowired
     private ILinkPluginsDatasetsService linkService;
 
+
+    /**
+     * The resource service. Autowired by Spring.
+     */
+    @Autowired
+    private IResourceService resourceService;
+
     @RequestMapping(method = RequestMethod.GET)
     @ResourceAccess(description = "endpoint allowing to retrieve which plugins are to be applied to a given dataset")
     @ResponseBody
-    public ResponseEntity<LinkPluginsDatasets> retrieveLink(@PathVariable("datasetId") final String pDatasetId)
+    public ResponseEntity<Resource<LinkPluginsDatasets>> retrieveLink(@PathVariable("datasetId") final String pDatasetId)
             throws EntityNotFoundException {
         final LinkPluginsDatasets link = linkService.retrieveLink(pDatasetId);
-        return new ResponseEntity<>(link, HttpStatus.OK);
+        Resource<LinkPluginsDatasets> resource = resourceService.toResource(link);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @ResourceAccess(description = "endpoint allowing to modify which plugins are to be applied to a given dataset")
     @ResponseBody
-    public ResponseEntity<LinkPluginsDatasets> updateLink(@PathVariable("datasetId") final String pDatasetId,
+    public ResponseEntity<Resource<LinkPluginsDatasets>> updateLink(@PathVariable("datasetId") final String pDatasetId,
             @RequestBody final LinkPluginsDatasets pUpdatedLink) throws EntityException {
         final LinkPluginsDatasets link = linkService.updateLink(pDatasetId, pUpdatedLink);
-        return new ResponseEntity<>(link, HttpStatus.OK);
+        Resource<LinkPluginsDatasets> resource = resourceService.toResource(link);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
 }
