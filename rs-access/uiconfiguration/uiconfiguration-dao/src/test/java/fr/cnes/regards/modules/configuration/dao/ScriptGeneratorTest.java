2bce0a0805613748f0c41f12f9c928ba2667f18a
Marc Sordi <marc.sordi@c-s.fr>
2017-05-31 12:19:50 +0200

    [rs-access] ignore script generation test

--- rs-access/uiconfiguration/uiconfiguration-dao/src/test/java/fr/cnes/regards/modules/configuration/dao/ScriptGeneratorTest.java
+++ rs-access/uiconfiguration/uiconfiguration-dao/src/test/java/fr/cnes/regards/modules/configuration/dao/ScriptGeneratorTest.java
@@ -1,10 +1,13 @@
 package fr.cnes.regards.modules.configuration.dao;
 
+import org.junit.Ignore;
+
 import fr.cnes.regards.framework.jpa.multitenant.test.AbstractScriptGeneratorTest;
 
 /**
  * @author Sylvain VISSIERE-GUERINET
  */
-public class ScriptGeneratorTest extends AbstractScriptGeneratorTest{
+@Ignore
+public class ScriptGeneratorTest extends AbstractScriptGeneratorTest {
 
 }
