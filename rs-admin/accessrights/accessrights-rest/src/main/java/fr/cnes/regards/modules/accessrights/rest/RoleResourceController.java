064237cc3d7344ae3e1a32ba92f95cf6b13555c4
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-06-13 09:44:10 +0200

    [rs-admin] added hateoas link to allow removal of a resource

--- rs-admin/accessrights/accessrights-rest/src/main/java/fr/cnes/regards/modules/accessrights/rest/RoleResourceController.java
+++ rs-admin/accessrights/accessrights-rest/src/main/java/fr/cnes/regards/modules/accessrights/rest/RoleResourceController.java
@@ -23,6 +23,7 @@ import fr.cnes.regards.framework.hateoas.IResourceController;
 import fr.cnes.regards.framework.hateoas.IResourceService;
 import fr.cnes.regards.framework.hateoas.LinkRels;
 import fr.cnes.regards.framework.hateoas.MethodParamFactory;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.security.annotation.ResourceAccess;
 import fr.cnes.regards.framework.security.role.DefaultRole;
@@ -96,21 +97,48 @@ public class RoleResourceController implements IResourceController<ResourcesAcce
     }
 
     @Override
-    public Resource<ResourcesAccess> toResource(ResourcesAccess pElement, Object... pExtras) {
+    public Resource<ResourcesAccess> toResource(ResourcesAccess resourcesAccess, Object... pExtras) {
 
         Assert.notNull(pExtras);
         String roleName = (String) pExtras[0];
 
-        Resource<ResourcesAccess> resource = hateoasService.toResource(pElement);
+        Resource<ResourcesAccess> resource = hateoasService.toResource(resourcesAccess);
         hateoasService.addLink(resource, this.getClass(), "getRoleResources", LinkRels.LIST,
                                MethodParamFactory.build(String.class, roleName));
         hateoasService.addLink(resource, this.getClass(), "addRoleResource", LinkRels.CREATE,
                                MethodParamFactory.build(String.class, roleName),
                                MethodParamFactory.build(ResourcesAccess.class));
-        hateoasService.addLink(resource, this.getClass(), "deleteRoleResource", LinkRels.DELETE,
-                               MethodParamFactory.build(String.class, roleName),
-                               MethodParamFactory.build(Long.class, pElement.getId()));
+
+        if (isRemovable(resourcesAccess, roleName)) {
+            hateoasService.addLink(resource, this.getClass(), "deleteRoleResource", LinkRels.DELETE,
+                                   MethodParamFactory.build(String.class, roleName),
+                                   MethodParamFactory.build(Long.class, resourcesAccess.getId()));
+        }
         return resource;
+    }
+
+    /**
+     * Removal of resource accesses is tricky: we can only remove a resource which is attached to PUBLIC if we are working on a native role.
+     *
+     * @param resourcesAccess
+     * @param roleName
+     * @return whether the resource access can be removed from the given role
+     */
+    private boolean isRemovable(ResourcesAccess resourcesAccess, String roleName) {
+        try {
+            Role currentRole = roleService.retrieveRole(roleName);
+            if (!currentRole.isNative()) {
+                Role publicRole = roleService.retrieveRole(DefaultRole.PUBLIC.name());
+                Set<ResourcesAccess> publicResources = roleService.retrieveRoleResourcesAccesses(publicRole.getId());
+                return !publicResources.contains(
+                        resourcesAccess); // on a custom role we can only remove a resource access if IT IS NOT one of public
+            }
+            return true; // we can remove any resources from any native role but PROJECT_ADMIN
+        } catch (EntityNotFoundException e) {
+            // This exception cannot occur as Role PUBLIC is a native role and not deletable!
+            // Moreover, As we are at the end of execution fo other method using roleName if it doesn't represent anything then it will be fired before here.
+            return false;
+        }
     }
 
 }
