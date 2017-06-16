f866dc09a52f7252166089a10d793cb50e8327b2
Marc Sordi <marc.sordi@c-s.fr>
2017-06-16 10:24:42 +0200

    [rs-catalog] facet & query tests

--- rs-catalog/search/search-rest/src/test/java/fr/cnes/regards/modules/search/rest/CatalogControllerSearchIT.java
+++ rs-catalog/search/search-rest/src/test/java/fr/cnes/regards/modules/search/rest/CatalogControllerSearchIT.java
@@ -3,8 +3,11 @@
  */
 package fr.cnes.regards.modules.search.rest;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -27,7 +30,12 @@ import fr.cnes.regards.framework.test.integration.AbstractRegardsTransactionalIT
 import fr.cnes.regards.framework.test.integration.RequestParamBuilder;
 import fr.cnes.regards.framework.test.report.annotation.Requirement;
 import fr.cnes.regards.modules.accessrights.client.IProjectUsersClient;
+import fr.cnes.regards.modules.entities.domain.DataObject;
 import fr.cnes.regards.modules.entities.domain.StaticProperties;
+import fr.cnes.regards.modules.entities.domain.attribute.AbstractAttribute;
+import fr.cnes.regards.modules.entities.domain.attribute.IntegerAttribute;
+import fr.cnes.regards.modules.entities.domain.attribute.builder.AttributeBuilder;
+import fr.cnes.regards.modules.entities.gson.MultitenantFlattenedAttributeAdapterFactory;
 import fr.cnes.regards.modules.indexer.dao.IEsRepository;
 import fr.cnes.regards.modules.models.client.IAttributeModelClient;
 import fr.cnes.regards.modules.models.domain.attributes.AttributeModel;
@@ -71,6 +79,9 @@ public class CatalogControllerSearchIT extends AbstractRegardsTransactionalIT {
     @Autowired
     private IEsRepository esRepository;
 
+    @Autowired
+    private MultitenantFlattenedAttributeAdapterFactory factory;
+
     @Before
     public void setUp() throws Exception {
 
@@ -78,6 +89,7 @@ public class CatalogControllerSearchIT extends AbstractRegardsTransactionalIT {
         Mockito.when(projectUserClient.isAdmin(Mockito.anyString())).thenReturn(ResponseEntity.ok(true));
 
         // Manage index
+        esRepository.deleteIndex(DEFAULT_TENANT);
         if (!esRepository.indexExists(DEFAULT_TENANT)) {
             esRepository.createIndex(DEFAULT_TENANT);
         }
@@ -132,7 +144,44 @@ public class CatalogControllerSearchIT extends AbstractRegardsTransactionalIT {
                 + attModel.buildJsonPath(StaticProperties.PROPERTIES)
                 + ":\"lmlml*\" AND (tags:(URN\\:AIP\\:DATASET\\:project1\\:3874e16e\\-f729\\-49c0\\-bae0\\-2c6903ec64e4\\:V1)))");
         performDefaultGet(CatalogController.DATAOBJECTS_SEARCH, expectations, "Error searching dataobjects", builder);
+    }
 
+    @Test
+    @Ignore
+    public final void search() {
+        final List<ResultMatcher> expectations = new ArrayList<>();
+        expectations.add(MockMvcResultMatchers.status().isOk());
+
+        // Mock required attribute
+        String attributeName = "Number";
+        AttributeModel attModel = AttributeModelBuilder.build(attributeName, AttributeType.INTEGER, null)
+                .defaultFragment().get();
+        List<AttributeModel> attModels = Lists.newArrayList(attModel);
+        Mockito.when(attributeModelClient.getAttributes(Mockito.any(), Mockito.any()))
+                .thenReturn(ResponseEntity.ok(HateoasUtils.wrapList(attModels)));
+
+        // Register attributes in serialization factory
+        factory.registerSubtype(DEFAULT_TENANT, IntegerAttribute.class, attModel.getName());
+
+        // DATA
+        List<DataObject> dos = new ArrayList<>();
+        OffsetDateTime creationDate = OffsetDateTime.now();
+        for (int i = 0; i < 100000; i++) {
+            DataObject dataObject = new DataObject(null, DEFAULT_TENANT, "do" + i);
+            Set<AbstractAttribute<?>> ppties = new HashSet<>();
+            ppties.add(AttributeBuilder.buildInteger(attModel.getName(), i));
+            dataObject.setCreationDate(creationDate);
+            dataObject.setProperties(ppties);
+            dos.add(dataObject);
+        }
+        esRepository.saveBulk(DEFAULT_TENANT, dos);
+
+        // REQUEST
+        RequestParamBuilder builder = RequestParamBuilder.build();
+        // builder.param("q", "(" + attModel.buildJsonPath(StaticProperties.PROPERTIES) + ":[* TO 10])");
+        // builder.param("facets", attModel.buildJsonPath(StaticProperties.PROPERTIES));
+        builder.param("facets", "creationDate");
+        performDefaultGet(CatalogController.DATAOBJECTS_SEARCH, expectations, "Error searching dataobjects", builder);
     }
 
     @Override
