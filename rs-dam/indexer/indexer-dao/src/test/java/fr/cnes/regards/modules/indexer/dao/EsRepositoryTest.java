95bb450cb09520183ddc16239297e0d10eb34390
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-08-03 16:20:39 +0200

    [rs-dam] fix stupid issue of work with maven

--- rs-dam/indexer/indexer-dao/src/test/java/fr/cnes/regards/modules/indexer/dao/EsRepositoryTest.java
+++ rs-dam/indexer/indexer-dao/src/test/java/fr/cnes/regards/modules/indexer/dao/EsRepositoryTest.java
@@ -53,7 +53,8 @@ public class EsRepositoryTest {
     public static void setUp() throws Exception {
         Map<String, String> propMap = Maps.newHashMap();
         boolean repositoryOK = true;
-        Stream<String> props = Files.lines(Paths.get("src/test/resources"));
+        // we get the properties into target/test-classes because this is where maven will put the filtered file(with real values and not placeholder)
+        Stream<String> props = Files.lines(Paths.get("target/test-classes/test.properties"));
         props.forEach(l -> {
             String[] keyVal = l.split("=");
             propMap.put(keyVal[0], keyVal[1]);
