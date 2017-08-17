00472a2d7a14f54515605b2f6715d1a87deff3ff
Marc Sordi <marc.sordi@c-s.fr>
2017-08-17 18:26:34 +0200

    [rs-microservice] clean ci

--- rs-microservice/regards-ci/wait-for-cots.sh
+++ rs-microservice/regards-ci/wait-for-cots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-/wait-for-it.sh -t 0 rs_rabbitmq:5672 --strict -- echo "rabbitmq is up"
-/wait-for-it.sh -t 0 rs_postgres:5432 --strict -- echo "postgres is up"
+/wait-for-it.sh -t 0 rs_rabbitmq:5672
+/wait-for-it.sh -t 0 rs_postgres:5432
