aca12fd58e3216fda99ca9e2cecb3f91f7a80ca0
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-08-21 14:25:16 +0200

    [rs-catalog] Add client for "/datasets/search " endpoint

--- rs-catalog/search/search-client/src/main/java/fr/cnes/regards/modules/search/client/ISearchDatasetsClient.java
+++ rs-catalog/search/search-client/src/main/java/fr/cnes/regards/modules/search/client/ISearchDatasetsClient.java
@@ -17,16 +17,16 @@ import fr.cnes.regards.framework.feign.annotation.RestClient;
 import fr.cnes.regards.modules.search.rest.CatalogController;
 
 /**
- * Feign client for calling rs-catalog's {@link CatalogController#searchCollections}
+ * Feign client for calling rs-catalog's {@link CatalogController#searchDatasets}
  *
  * @author Xavier-Alexandre Brochard
  */
 @RestClient(name = "rs-catalog")
-@RequestMapping(value = CatalogController.COLLECTIONS_SEARCH, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+@RequestMapping(value = CatalogController.DATASETS_SEARCH, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @FunctionalInterface
-public interface ISearchCollectionsClient {
+public interface ISearchDatasetsClient {
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<JsonObject> searchCollections(@RequestParam final Map<String, String> allParams);
+    public ResponseEntity<JsonObject> searchDatasets(@RequestParam final Map<String, String> allParams);
 }
