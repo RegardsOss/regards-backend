a259ff68e7956c65b031163541a402eb077c6f68
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-08-30 11:57:22 +0200

    [rs-microservice] add real download into DownloadUtils

--- rs-microservice/regards-core/file-utils/src/main/java/fr/cnes/regards/framework/file/utils/DownloadUtils.java
+++ rs-microservice/regards-core/file-utils/src/main/java/fr/cnes/regards/framework/file/utils/DownloadUtils.java
@@ -1,8 +1,18 @@
 package fr.cnes.regards.framework.file.utils;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.*;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Sylvain VISSIERE-GUERINET
@@ -15,8 +25,33 @@ public class DownloadUtils {
      * @return
      * @throws IOException
      */
-    public static InputStream download(URL source) throws IOException {
-        return downloadThroughProxy(source, Proxy.NO_PROXY);
+    public static InputStream getInputStream(URL source) throws IOException {
+        return getInputStreamThroughProxy(source, Proxy.NO_PROXY);
+    }
+
+    public static String download(URL source, Path destination, Charset encoding, String checksumAlgorithm)
+            throws IOException, NoSuchAlgorithmException {
+        return downloadThroughProxy(source, destination, encoding, checksumAlgorithm, Proxy.NO_PROXY);
+    }
+
+    public static String downloadThroughProxy(URL source, Path destination, Charset encoding, String checksumAlgorithm, Proxy proxy)
+            throws IOException, NoSuchAlgorithmException {
+        BufferedWriter writer = Files
+                .newBufferedWriter(destination, encoding, StandardOpenOption.CREATE);
+        InputStream sourceStream = DownloadUtils.getInputStreamThroughProxy(source, proxy);
+        int read;
+        while ((read = sourceStream.read()) != -1) {
+            writer.write(read);
+        }
+        writer.flush();
+        writer.close();
+        // Now that it is stored, lets checked that it is correctly stored!
+        InputStream is = Files.newInputStream(destination);
+        DigestInputStream dis = new DigestInputStream(is, MessageDigest.getInstance(checksumAlgorithm));
+        while (dis.read() != -1) {
+        }
+        dis.close();
+        return ChecksumUtils.getHexChecksum(dis.getMessageDigest().digest());
     }
 
     /**
@@ -26,7 +61,7 @@ public class DownloadUtils {
      * @return
      * @throws IOException
      */
-    public static InputStream downloadThroughProxy(URL source, Proxy proxy) throws IOException {
+    public static InputStream getInputStreamThroughProxy(URL source, Proxy proxy) throws IOException {
         URLConnection connection = source.openConnection(proxy);
         connection.setDoInput(true); //that's the default but lets set it explicitly for understanding
         connection.connect();
