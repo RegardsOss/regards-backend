282e0bfb7de1bd019a3f9cda3c96015b39b037ed
Olivier <olivier.rousselot@gmail.com>
2017-09-06 17:02:39 +0200

    [rs-microservice] Add config for async job service

---
+++ rs-microservice/regards-default-modules/jobs/jobs-service/src/main/java/fr/cnes/regards/framework/modules/jobs/service/JobServiceConfiguration.java
@@ -0,0 +1,13 @@
+package fr.cnes.regards.framework.modules.jobs.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author oroussel
+ */
+@Configuration
+@EnableAsync
+public class JobServiceConfiguration {
+
+}
