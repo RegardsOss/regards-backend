ca1b2f0b72856e09d0dfebf006af6120e27446b9
Marc Sordi <marc.sordi@c-s.fr>
2017-09-19 14:17:28 +0200

    [rs-microservice] Fix bootstrap issue with spring cloud upgrade

--- rs-microservice/regards-starters/cloud-regards-starter/src/main/java/fr/cnes/regards/framework/cloud/autoconfigure/CloudAutoConfiguration.java
+++ rs-microservice/regards-starters/cloud-regards-starter/src/main/java/fr/cnes/regards/framework/cloud/autoconfigure/CloudAutoConfiguration.java
@@ -18,15 +18,15 @@
  */
 package fr.cnes.regards.framework.cloud.autoconfigure;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnProperty(prefix = "regards.cloud", name = "enabled", matchIfMissing = true)
-@EnableEurekaClient
 @EnableDiscoveryClient
+@AutoConfigureBefore(EurekaClientAutoConfiguration.class)
 public class CloudAutoConfiguration {
-
 }
