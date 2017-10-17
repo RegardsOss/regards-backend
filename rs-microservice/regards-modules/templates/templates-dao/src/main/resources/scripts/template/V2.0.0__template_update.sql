113652186c86a2fc6a45aa6641224e6532059799
Olivier <olivier.rousselot@gmail.com>
2017-10-17 10:02:32 +0200

    [rs-microservice] Fix template SQL script

--- rs-microservice/regards-modules/templates/templates-dao/src/main/resources/scripts/template/V2.0.0__template_update.sql
+++ rs-microservice/regards-modules/templates/templates-dao/src/main/resources/scripts/template/V2.0.0__template_update.sql
@@ -1,5 +1,8 @@
-/* Template */
+-- Template
+-- Clean
 drop table t_template, t_template_data cascade;
+drop sequence seq_template;
+-- Re-create
 create table t_template (id int8 not null, code varchar(100) not null, content text, description varchar(100), subject varchar(100), primary key (id));
 create table t_template_data (template_id int8 not null, value varchar(128), name varchar(48) not null, primary key (template_id, name));
 alter table t_template add constraint uk_template_code unique (code);
