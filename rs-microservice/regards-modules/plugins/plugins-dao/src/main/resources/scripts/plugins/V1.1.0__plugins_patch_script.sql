2ec1a4f07a556f93464eb334392540f9815abb44
sbinda <sebastien.binda@c-s.fr>
2017-10-24 13:50:31 +0200

    [rs-microservice] Cherry pick first merge from feature/1.1.0/services to develop_V1.1 into develop

---
+++ rs-microservice/regards-modules/plugins/plugins-dao/src/main/resources/scripts/plugins/V1.1.0__plugins_patch_script.sql
@@ -0,0 +1,1 @@
+alter table t_plugin_configuration add column icon_url varchar(255);
--- rs-microservice/regards-starters/feign-regards-starter/src/main/java/fr/cnes/regards/framework/feign/FeignClientBuilder.java
+++ rs-microservice/regards-starters/feign-regards-starter/src/main/java/fr/cnes/regards/framework/feign/FeignClientBuilder.java
@@ -19,7 +19,6 @@
 package fr.cnes.regards.framework.feign;
 
 import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
-import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
 
 import com.google.gson.Gson;
 
@@ -50,7 +49,7 @@ public final class FeignClientBuilder {
     public static <T> T build(final Target<T> pTarget) {
         return Feign.builder() // Feign customization
                 .encoder(new GsonEncoder()).decoder(new ResponseEntityDecoder(new GsonDecoder()))
-                .errorDecoder(new ClientErrorDecoder()).decode404().contract(new SpringMvcContract()).target(pTarget);
+                .errorDecoder(new ClientErrorDecoder()).decode404().contract(new FeignContractSupplier().get()).target(pTarget);
     }
 
     /**
