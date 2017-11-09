d7fc2e01765d57c2376389b973fe0498aa426b00
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-11-09 14:05:31 +0100

    [rs-admin] change CI database name

--- rs-admin/regards-ci/postgres/init.sql
+++ rs-admin/regards-ci/postgres/init.sql
@@ -1,2 +1,2 @@
 -- Create REGARDS required databases, for CI it's rs_testdb_root because it is root which is launching the build inside docker
-CREATE DATABASE rs_testdb_root
+CREATE DATABASE rs_testdb_tux
