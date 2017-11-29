216fcc83a8ad0d73b668acaad39ba43f4300b20e
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-11-29 13:55:13 +0100

    [rs-microservice] fix module readiness

--- rs-microservice/regards-starters/microservice-regards-starter/src/main/java/fr/cnes/regards/framework/microservice/rest/MicroserviceReadyController.java
+++ rs-microservice/regards-starters/microservice-regards-starter/src/main/java/fr/cnes/regards/framework/microservice/rest/MicroserviceReadyController.java
@@ -29,7 +29,7 @@ public class MicroserviceReadyController {
     /**
      * List of all {@link IModuleReady} instantiated
      */
-    @Autowired
+    @Autowired(required = false)
     private List<IModuleReady> moduleReadies;
 
     @RequestMapping(method = RequestMethod.GET)
