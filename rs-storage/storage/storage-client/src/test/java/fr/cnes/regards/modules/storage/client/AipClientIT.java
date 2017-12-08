498609860819b0e1bfed7e3c351f59854ae4912a
svissier <sylvain.vissiere-guerinet@c-s.fr>
2017-12-08 11:03:59 +0100

    [rs-storage] add message converters to feign client builder

--- rs-storage/storage/storage-client/src/test/java/fr/cnes/regards/modules/storage/client/AipClientIT.java
+++ rs-storage/storage/storage-client/src/test/java/fr/cnes/regards/modules/storage/client/AipClientIT.java
@@ -32,8 +32,10 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.PagedResources;
@@ -138,6 +140,9 @@ public class AipClientIT extends AbstractRegardsWebIT {
 
     private PluginConfiguration catalogSecuDelegConf;
 
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
     @BeforeClass
     public static void initAll() throws IOException {
         if (Paths.get(workspace).toFile().exists()) {
@@ -157,6 +162,7 @@ public class AipClientIT extends AbstractRegardsWebIT {
             FileUtils.deleteDirectory(downloadDir.toFile());
         }
         Files.createDirectories(downloadDir);
+        FeignClientBuilder.setMessageConverters(messageConverters);
         client = FeignClientBuilder.build(new TokenClientProvider<>(IAipClient.class,
                                                                     "http://" + serverAddress + ":" + getPort(),
                                                                     feignSecurityManager), gson);
