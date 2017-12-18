f134496c439222128ae5bcf8ef155ddabf89c8c6
cmertz <christophe.mertz@c-s.fr>
2017-12-18 18:06:59 +0100

    [rs-dataprovider] Move DescriptorException to test
    Former-commit-id: 5d0313bf28fe1d3c38237fddabc708907dfe0792

--- rs-dataprovider/acquisition/acquisition-plugins-ssalto/src/test/java/fr/cnes/regards/modules/acquisition/plugins/ssalto/descriptor/DescriptorException.java
+++ rs-dataprovider/acquisition/acquisition-plugins-ssalto/src/test/java/fr/cnes/regards/modules/acquisition/plugins/ssalto/descriptor/DescriptorException.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.cnes.regards.modules.acquisition.exception;
+package fr.cnes.regards.modules.acquisition.plugins.ssalto.descriptor;
 
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 
