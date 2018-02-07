11b3d1b3ea423fca3f243d2d9d17fabd6eb5723a
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-02-07 10:31:03 +0100

    [rs-microservice] init regards/regards#195

---
+++ rs-microservice/regards-utils/encryption-utils/src/main/java/fr/cnes/regards/framework/utils/App.java
@@ -0,0 +1,13 @@
+package fr.cnes.regards.framework.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
