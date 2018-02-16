48624e356eec5935bbd2dda4ec0e187d14126b59
Marc Sordi <marc.sordi@c-s.fr>
2018-02-16 15:45:14 +0100

    [rs-microservice]
    manage plugin parameters in a class hierarchy

--- rs-microservice/regards-utils/plugin-utils/src/main/java/fr/cnes/regards/framework/utils/plugins/ReflectionUtils.java
+++ rs-microservice/regards-utils/plugin-utils/src/main/java/fr/cnes/regards/framework/utils/plugins/ReflectionUtils.java
@@ -22,6 +22,9 @@ package fr.cnes.regards.framework.utils.plugins;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -42,13 +45,13 @@ public final class ReflectionUtils {
      *
      * Make a field accessible
      *
-     * @param pField
+     * @param field
      *            the field to check
      */
-    public static void makeAccessible(final Field pField) {
-        if ((!Modifier.isPublic(pField.getModifiers()) || !Modifier.isPublic(pField.getDeclaringClass().getModifiers())
-                || Modifier.isFinal(pField.getModifiers())) && !pField.isAccessible()) {
-            pField.setAccessible(true);
+    public static void makeAccessible(Field field) {
+        if ((!Modifier.isPublic(field.getModifiers()) || !Modifier.isPublic(field.getDeclaringClass().getModifiers())
+                || Modifier.isFinal(field.getModifiers())) && !field.isAccessible()) {
+            field.setAccessible(true);
         }
     }
 
@@ -56,15 +59,26 @@ public final class ReflectionUtils {
      *
      * Make a method accessible
      *
-     * @param pMethod
+     * @param method
      *            the field to check
      */
-    public static void makeAccessible(final Method pMethod) {
-        if ((!Modifier.isPublic(pMethod.getModifiers())
-                || !Modifier.isPublic(pMethod.getDeclaringClass().getModifiers())
-                || Modifier.isFinal(pMethod.getModifiers())) && !pMethod.isAccessible()) {
-            pMethod.setAccessible(true);
+    public static void makeAccessible(Method method) {
+        if ((!Modifier.isPublic(method.getModifiers()) || !Modifier.isPublic(method.getDeclaringClass().getModifiers())
+                || Modifier.isFinal(method.getModifiers())) && !method.isAccessible()) {
+            method.setAccessible(true);
         }
+    }
+
+    /**
+     * Retrieve all declared fields of the class including inherited ones
+     */
+    public static List<Field> getAllDeclaredFields(Class<?> clazz) {
+        List<Field> fields = new ArrayList<>();
+        fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+        if (clazz.getSuperclass() != null) {
+            fields.addAll(getAllDeclaredFields(clazz.getSuperclass()));
+        }
+        return fields;
     }
 
 }
