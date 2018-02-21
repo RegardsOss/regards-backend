1ad661b73e6b154e5c18fa4957ea3ed0848cbbdd
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-02-21 10:11:50 +0100

    [rs-storage] add workspace error handling on store jobs

--- rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/job/StoreMetadataFilesJob.java
+++ rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/job/StoreMetadataFilesJob.java
@@ -3,10 +3,13 @@
  */
 package fr.cnes.regards.modules.storage.service.job;
 
+import java.io.IOException;
 import java.util.Map;
 
 import fr.cnes.regards.framework.modules.jobs.domain.JobParameter;
+import fr.cnes.regards.framework.modules.jobs.domain.exception.JobWorkspaceException;
 import fr.cnes.regards.modules.storage.domain.database.StorageDataFile;
+import fr.cnes.regards.modules.storage.domain.plugin.IWorkingSubset;
 
 /**
  * This job is executed by JobService while its scheduling is handled by an IAIPService. This means that the job context is prepared by an IAIPService.
@@ -26,5 +29,13 @@ public class StoreMetadataFilesJob extends AbstractStoreFilesJob {
     @Override
     protected void handleNotHandledDataFile(StorageDataFile notHandled) {
         progressManager.storageFailed(notHandled, NOT_HANDLED_MSG);
+    }
+
+    @Override
+    protected void handleWorkspaceException(IOException e) throws JobWorkspaceException {
+        StorageJobProgressManager progressManager = new StorageJobProgressManager(publisher, this);
+        IWorkingSubset workingSubset = parameters.get(WORKING_SUB_SET_PARAMETER_NAME).getValue();
+        workingSubset.getDataFiles().forEach(file -> progressManager.storageFailed(file, e.toString()));
+        super.handleWorkspaceException(e);
     }
 }
