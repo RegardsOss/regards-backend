3a1a187e8e11f7f4e2a4eb8bf9ed86e6f286684e
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-02-23 18:20:10 +0100

    [rs-access] switch all sql scripts to scripts/....

--- rs-access/uiconfiguration/uiconfiguration-dao/src/main/resources/instancescripts/uiconfiguration/V1.1.0__uiconfiguration_patch_script.sql
+++
@@ -1,5 +0,0 @@
-alter table t_ui_plugin add column icon_url varchar(255);
-create table t_ui_plugin_application_mode (ui_plugin_id int8 not null, application_mode varchar(255) not null, primary key (ui_plugin_id, application_mode));
-create table t_ui_plugin_entity_type (ui_plugin_id int8 not null, entity_type varchar(255) not null, primary key (ui_plugin_id, entity_type));
-alter table t_ui_plugin_application_mode add constraint fk_ui_plugin_application_mode_ui_plugin_id foreign key (ui_plugin_id) references t_ui_plugin;
-alter table t_ui_plugin_entity_type add constraint fk_ui_plugin_entity_type_ui_plugin_id foreign key (ui_plugin_id) references t_ui_plugin;
