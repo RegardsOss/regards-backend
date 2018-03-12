abfc3cb5fdc835d12d5b661e575765e978ceccdd
Olivier <olivier.rousselot@c-s.fr>
2018-03-12 15:43:34 +0100

    [rs-catalog] Remove bad SQL update script

--- rs-catalog/search/search-dao/src/main/resources/scripts/search/V2.0.0__search_update.sql
+++
@@ -1,2 +0,0 @@
--- Update SearchController computeFilesSummary default role
-UPDATE t_resources_access SET defaultrole = 'REGISTERED_USER' where resource = '/search/dataobjects/computefilessummary';
