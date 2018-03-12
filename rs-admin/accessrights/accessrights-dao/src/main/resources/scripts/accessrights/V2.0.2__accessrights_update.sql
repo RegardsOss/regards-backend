d91bb42ac8de3e20f5eaa86aad83ab5f31793860
Olivier <olivier.rousselot@c-s.fr>
2018-03-12 15:41:47 +0100

    [rs-admin] Update catalog resource_access

---
+++ rs-admin/accessrights/accessrights-dao/src/main/resources/scripts/accessrights/V2.0.2__accessrights_update.sql
@@ -0,0 +1,2 @@
+-- Update SearchController computeFilesSummary default role
+UPDATE t_resources_access SET defaultrole = 'REGISTERED_USER' where resource = '/search/dataobjects/computefilessummary' AND microservice = 'rs-catalog';
