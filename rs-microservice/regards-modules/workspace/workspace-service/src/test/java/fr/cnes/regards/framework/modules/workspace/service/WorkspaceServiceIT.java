ac4163d6bc801a18eb2c165da99e28c799fc97db
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-03-14 09:13:12 +0100

    [rs-microservice] regards/regards#260

--- rs-microservice/regards-modules/workspace/workspace-service/src/test/java/fr/cnes/regards/framework/modules/workspace/service/WorkspaceServiceIT.java
+++ rs-microservice/regards-modules/workspace/workspace-service/src/test/java/fr/cnes/regards/framework/modules/workspace/service/WorkspaceServiceIT.java
@@ -1,42 +1,18 @@
-/*
- * Copyright 2017 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
- *
- * This file is part of REGARDS.
- *
- * REGARDS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * REGARDS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
- */
 package fr.cnes.regards.framework.modules.workspace.service;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 
-import fr.cnes.regards.framework.microservice.manager.MaintenanceManager;
-import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
 import fr.cnes.regards.framework.test.integration.AbstractRegardsServiceIT;
-import fr.cnes.regards.framework.test.report.annotation.Requirement;
 import fr.cnes.regards.framework.utils.file.ChecksumUtils;
 
 /**
@@ -47,12 +23,6 @@ public class WorkspaceServiceIT extends AbstractRegardsServiceIT {
 
     @Autowired
     private IWorkspaceService workspaceService;
-
-    @Autowired
-    private IRuntimeTenantResolver runtimeTenantResolver;
-
-    @Autowired
-    private IWorkspaceNotifier notifier;
 
     @Test
     public void testSetIntoWorkspace() throws IOException {
@@ -73,27 +43,6 @@ public class WorkspaceServiceIT extends AbstractRegardsServiceIT {
         String srcChecksum = ChecksumUtils.computeHexChecksum(Files.newInputStream(src), "MD5");
         String wsChecksum = ChecksumUtils.computeHexChecksum(result, "MD5");
         Assert.assertEquals(srcChecksum, wsChecksum);
-    }
-
-    @Test(expected = UnsupportedOperationException.class) // because of default workspace notifier implementation
-    @Requirement("REGARDS_DSL_ING_CMP_010")
-    @Requirement("REGARDS_DSL_ING_CMP_020")
-    @Requirement("REGARDS_DSL_ING_CMP_030")
-    public void testMonitor() {
-        IWorkspaceNotifier spiedNotifier = Mockito.spy(notifier);
-        workspaceService.monitor(DEFAULT_TENANT);
-        Mockito.verify(spiedNotifier, Mockito.times(1))
-                .sendErrorNotification(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
-        Assert.assertTrue(MaintenanceManager.getMaintenance(DEFAULT_TENANT));
-    }
-
-    @After
-    public void cleanUp() throws IOException {
-        try {
-            Files.walk(workspaceService.getMicroserviceWorkspace()).forEach(p -> p.toFile().delete());
-        } catch (NoSuchFileException e) {
-            // we don't care
-        }
     }
 
 }
