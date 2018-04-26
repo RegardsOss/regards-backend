a8c4e3f0a29406e0dc01a73c87976ce5e72917dd
Olivier <olivier.rousselot@c-s.fr>
2018-04-26 14:55:03 +0200

    [rs-dam] Back to the future concerning facets (too complicated)
    Former-commit-id: 7fc4b882eea29c6b5524142278870b810ea2a7b9

--- rs-dam/indexer/indexer-dao/src/main/java/fr/cnes/regards/modules/indexer/dao/builder/AggregationBuilderFacetTypeVisitor.java
+++ rs-dam/indexer/indexer-dao/src/main/java/fr/cnes/regards/modules/indexer/dao/builder/AggregationBuilderFacetTypeVisitor.java
@@ -126,8 +126,8 @@ public class AggregationBuilderFacetTypeVisitor implements IFacetTypeVisitor<Agg
                 }
             }
             if (!i.hasNext()) { // last value
-                if (rangeAggBuilder.ranges().size() < 1) {
-                    // No range => no facet
+                if (rangeAggBuilder.ranges().size() <= 1) {
+                    // Only one range (-∞, previousValue) => only one value => facet is useless
                     return null;
                 }
                 rangeAggBuilder.addUnboundedFrom(previousValue);
