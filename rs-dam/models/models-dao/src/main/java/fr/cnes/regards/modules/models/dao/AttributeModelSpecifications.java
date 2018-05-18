a4b0873116db7a15eb25612940ce970b046d0b48
sbinda <sebastien.binda@c-s.fr>
2018-05-18 10:32:38 +0200

    [rs-dam] Update GPL License.
    Former-commit-id: b9c3d5ae255c57fd2227b30d780fa13bf99a624c

--- rs-dam/models/models-dao/src/main/java/fr/cnes/regards/modules/models/dao/AttributeModelSpecifications.java
+++ rs-dam/models/models-dao/src/main/java/fr/cnes/regards/modules/models/dao/AttributeModelSpecifications.java
@@ -1,6 +1,6 @@
 package fr.cnes.regards.modules.models.dao;
 /*
- * Copyright 2017 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
