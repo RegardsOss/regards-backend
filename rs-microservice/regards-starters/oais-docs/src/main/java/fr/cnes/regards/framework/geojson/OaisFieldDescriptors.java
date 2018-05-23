ac55fdd856a38daff4379d20f49e75ba8807a67f
cmertz <christophe.mertz@c-s.fr>
2018-05-23 15:23:47 +0200

    [rs-microservice] Add optional() and type()

--- rs-microservice/regards-starters/oais-docs/src/main/java/fr/cnes/regards/framework/geojson/OaisFieldDescriptors.java
+++ rs-microservice/regards-starters/oais-docs/src/main/java/fr/cnes/regards/framework/geojson/OaisFieldDescriptors.java
@@ -109,6 +109,7 @@ public class OaisFieldDescriptors {
         ConstrainedFields representationInformationField = new ConstrainedFields(RepresentationInformation.class);
 
         lfd.add(representationInformationField.withPath(addPrefix(path, "syntax"), "syntax", "A data objet syntax"));
+        // TODO manque Semantic
 
         lfd.addAll(buildSyntaxDescription(addPrefix(path, "syntax.")));
 
@@ -120,8 +121,8 @@ public class OaisFieldDescriptors {
 
         ConstrainedFields syntaxField = new ConstrainedFields(Syntax.class);
 
-        lfd.add(syntaxField.withPath(addPrefix(prefix, "name"), "A syntax name"));
-        lfd.add(syntaxField.withPath(addPrefix(prefix, "description"), "A description"));
+        lfd.add(syntaxField.withPath(addPrefix(prefix, "name"), "A syntax name").optional().type("String"));
+        lfd.add(syntaxField.withPath(addPrefix(prefix, "description"), "A description").optional().type("String"));
         lfd.add(syntaxField.withPath(addPrefix(prefix, "mimeType"), "mimetype",
                                      "A two-part identifier for file formats and format contents"));
 
@@ -140,11 +141,13 @@ public class OaisFieldDescriptors {
 
         lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "regardsDataType"), "regardsDataType",
                                              "REGARDS data object type", ". Allowed values : " + joiner.toString()));
-        lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "filename"), "The data object file name"));
+        lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "filename"), "The data object file name").optional()
+                .type("String"));
         lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "urls"), "urls", "A set of URL"));
         lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "algorithm"), "The checksum algorithm"));
         lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "checksum"), "The calculated data object checksum"));
-        lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "fileSize"), "The data object size in bytes"));
+        lfd.add(oaisDataObjectField.withPath(addPrefix(prefix, "fileSize"), "The data object size in bytes").optional()
+                .type("Long"));
 
         return lfd;
     }
@@ -179,14 +182,16 @@ public class OaisFieldDescriptors {
 
         ConstrainedFields provenanceField = new ConstrainedFields(ProvenanceInformation.class);
 
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "history[]"), "history", "A list of events"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "history[]"), "history", "A list of events").optional()
+                .type("String"));
         lfd.addAll(buildEventDescription(addPrefix(prefix, "history[].")));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "facility"), "A facility"));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "instrument"), "An instrument"));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "filter"), "A filter"));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "detector"), "A detector"));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "proposal"), "A proposal"));
-        lfd.add(provenanceField.withPath(addPrefix(prefix, "additional"), "An additional information"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "facility"), "A facility").optional().type("String"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "instrument"), "An instrument").optional().type("String"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "filter"), "A filter").optional().type("String"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "detector"), "A detector").optional().type("String"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "proposal"), "A proposal").optional().type("String"));
+        lfd.add(provenanceField.withPath(addPrefix(prefix, "additional"), "An additional information").optional()
+                .type("String"));
 
         return lfd;
     }
@@ -196,7 +201,7 @@ public class OaisFieldDescriptors {
 
         ConstrainedFields eventField = new ConstrainedFields(Event.class);
 
-        lfd.add(eventField.withPath(addPrefix(prefix, "type"), "The event's type"));
+        lfd.add(eventField.withPath(addPrefix(prefix, "type"), "The event's type").optional().type("String"));
         lfd.add(eventField.withPath(addPrefix(prefix, "comment"), "The event's comment"));
         lfd.add(eventField.withPath(addPrefix(prefix, "date"), "date", "ISO Date time",
                                     ". Required format : yyyy-MM-dd’T’HH:mm:ss.SSSZ"));
@@ -209,10 +214,11 @@ public class OaisFieldDescriptors {
 
         ConstrainedFields accessRightField = new ConstrainedFields(AccessRightInformation.class);
 
-        lfd.add(accessRightField.withPath(addPrefix(prefix, "licence"), "The licence"));
+        lfd.add(accessRightField.withPath(addPrefix(prefix, "licence"), "The licence").optional().type("String"));
         lfd.add(accessRightField.withPath(addPrefix(prefix, "dataRights"), "dataRights", "A data access rights"));
         lfd.add(accessRightField.withPath(addPrefix(prefix, "publicReleaseDate"), "publicReleaseDate", "ISO Date time",
-                                          "Required format : yyyy-MM-dd’T’HH:mm:ss.SSSZ"));
+                                          "Required format : yyyy-MM-dd’T’HH:mm:ss.SSSZ")
+                .optional().type("String"));
 
         return lfd;
     }
