d5963942181a1dbdce54d3d9fa8c5d9dd728cc20
cmertz <christophe.mertz@c-s.fr>
2018-06-06 15:58:04 +0200

    [rs-dam] Entities-service : fix unit tests
    Former-commit-id: e5004e1dea5515fd7c6685186cd11d1aa15264e6

--- rs-dam/entities/entities-service/src/test/java/fr/cnes/regards/modules/entities/service/DocumentServiceTest.java
+++ rs-dam/entities/entities-service/src/test/java/fr/cnes/regards/modules/entities/service/DocumentServiceTest.java
@@ -3,6 +3,7 @@ package fr.cnes.regards.modules.entities.service;
 import java.io.IOException;
 import java.time.OffsetDateTime;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
-import fr.cnes.regards.framework.jpa.multitenant.transactional.MultitenantTransactional;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.oais.urn.EntityType;
 import fr.cnes.regards.modules.entities.dao.IDocumentLSRepository;
@@ -26,10 +26,9 @@ import fr.cnes.regards.modules.entities.domain.Document;
 import fr.cnes.regards.modules.models.dao.IModelRepository;
 import fr.cnes.regards.modules.models.domain.Model;
 
-@TestPropertySource(locations = {"classpath:test.properties"})
+@TestPropertySource(locations = { "classpath:test.properties" })
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {ServiceConfiguration.class})
-@MultitenantTransactional
+@ContextConfiguration(classes = { ServiceConfiguration.class })
 public class DocumentServiceTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(DocumentServiceTest.class);
@@ -50,9 +49,10 @@ public class DocumentServiceTest {
 
     private Document document1;
 
-
     @Before
     public void init() throws ModuleException {
+        clean();
+        
         model1 = Model.build("modelName2", "model desc", EntityType.DOCUMENT);
         model1 = modelRepository.save(model1);
 
@@ -64,20 +64,32 @@ public class DocumentServiceTest {
 
     }
 
-
     @Test
     @Commit
     public void testAddFiles() throws ModuleException, IOException {
-        String fileLsUriTemplate = "/documents/" + document1.getId() + "/files/" + DocumentLSService.FILE_CHECKSUM_URL_TEMPLATE;
-        MockMultipartFile mockMultipartFile = new MockMultipartFile("document1.xml", "document1.xml", "doc/xml", "content of my file".getBytes());
-        MockMultipartFile mockMultipartFile2 = new MockMultipartFile("document2.png", "document2.png", "image/png", "some pixels informations".getBytes());
-        MultipartFile[] multipartFiles = {mockMultipartFile, mockMultipartFile2};
+        String fileLsUriTemplate = "/documents/" + document1.getId() + "/files/"
+                + DocumentLSService.FILE_CHECKSUM_URL_TEMPLATE;
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("document1.xml", "document1.xml", "doc/xml",
+                "content of my file".getBytes());
+        MockMultipartFile mockMultipartFile2 = new MockMultipartFile("document2.png", "document2.png", "image/png",
+                "some pixels informations".getBytes());
+        MultipartFile[] multipartFiles = { mockMultipartFile, mockMultipartFile2 };
         Document updatedDoc = documentService.addFiles(document1.getId(), multipartFiles, fileLsUriTemplate);
 
         Assert.assertEquals(updatedDoc.getDocumentFiles().size(), 2);
         Assert.assertEquals(documentLSRepository.count(), 2);
         documentService.deleteDocumentAndFiles(document1.getId());
         Assert.assertEquals(documentLSRepository.count(), 0);
+    }
+
+    @After
+    public void cleanAfter() throws ModuleException {
+        clean();
+    }
+    
+    private void clean() throws ModuleException {
+        documentRepository.deleteAll();
+        modelRepository.deleteAll();
     }
 
 }
