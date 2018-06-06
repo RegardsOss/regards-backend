6582998195e37af079ada6d728755c86c2c92718
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-06-06 17:12:28 +0200

    [rs-storage] fix syntax error

--- rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V2.1.0__storage_move_no_catalog_security_delegation_plugin_script.sql
+++ rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V2.1.0__storage_move_no_catalog_security_delegation_plugin_script.sql
@@ -1,3 +1,1 @@
-UPDATE t_plugin_configuration \
-    SET pluginclassname = 'fr.cnes.regards.modules.storage.plugin.security.NoCatalogSecurityDelegationPlugin' \
-    WHERE pluginclassname = 'fr.cnes.regards.modules.storage.domain.plugin.NoCatalogSecurityDelegationPlugin';
+UPDATE t_plugin_configuration SET pluginclassname = 'fr.cnes.regards.modules.storage.plugin.security.NoCatalogSecurityDelegationPlugin' WHERE pluginclassname = 'fr.cnes.regards.modules.storage.domain.plugin.NoCatalogSecurityDelegationPlugin';
