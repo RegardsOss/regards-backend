a1b82330888f8958e0e481ebacafa5aa16cf5d74
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-06-14 18:08:54 +0200

    [rs-storage] WIP: tests to be done

--- rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/StorageConfigurationManager.java
+++ rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/StorageConfigurationManager.java
@@ -20,13 +20,26 @@ package fr.cnes.regards.modules.storage.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.bouncycastle.openssl.EncryptionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import fr.cnes.regards.framework.module.manager.AbstractModuleConfigurationManager;
 import fr.cnes.regards.framework.module.manager.ModuleConfiguration;
 import fr.cnes.regards.framework.module.manager.ModuleConfigurationItem;
+import fr.cnes.regards.framework.module.rest.exception.EntityInvalidException;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.modules.plugins.dao.IPluginConfigurationRepository;
+import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
+import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
+import fr.cnes.regards.framework.utils.plugins.PluginUtils;
+import fr.cnes.regards.modules.storage.dao.IPrioritizedDataStorageRepository;
+import fr.cnes.regards.modules.storage.domain.database.PrioritizedDataStorage;
+import fr.cnes.regards.modules.storage.domain.plugin.IDataStorage;
 
 /**
  * Configuration manager for current module
@@ -35,22 +48,91 @@ import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 @Component
 public class StorageConfigurationManager extends AbstractModuleConfigurationManager {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StorageConfigurationManager.class);
+
+    @Autowired
+    private IPluginConfigurationRepository pluginConfigurationRepository;
+
+    @Autowired
+    private IPrioritizedDataStorageRepository prioritizedDataStorageRepository;
+
+    @Autowired
+    private IPluginService pluginService;
+
+    private IPrioritizedDataStorageService prioritizedDataStorageService;
+
     @Override
     public void importConfiguration(ModuleConfiguration configuration) throws ModuleException {
-        // TODO
-        // for (ModuleConfigurationItem<?> item : configuration.getConfiguration()) {
-        // if (MyPOJO.class.isAssignableFrom(item.getKey())) {
-        // MyPOJO pojo = item.getTypedValue();
-        // // Do something to import configuration
-        // }
-        // }
+        for (ModuleConfigurationItem<?> item : configuration.getConfiguration()) {
+            // first, is it a PrioritizedDataStorage?
+            if (PrioritizedDataStorage.class.isAssignableFrom(item.getKey())) {
+                importPrioritizedDataStorage(item);
+            }
+            if (PluginConfiguration.class.isAssignableFrom(item.getKey())) {
+                importPluginConf(item);
+            }
+        }
+    }
+
+    protected void importPluginConf(ModuleConfigurationItem<?> item)
+            throws EntityInvalidException {
+        PluginConfiguration plgConf = item.getTypedValue();
+        if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
+            LOGGER.warn("A plugin configuration already exists with same label, skipping import of {}.",
+                        plgConf.getLabel());
+        } else {
+            EntityInvalidException validationIssues = PluginUtils.validate(plgConf);
+            if (validationIssues == null) {
+                pluginService.savePluginConfiguration(plgConf);
+            } else {
+                LOGGER.warn("Skipping import of {} for these reasons: {}",
+                            plgConf.getLabel(),
+                            validationIssues.getMessages().stream().collect(Collectors.joining(",", "", ".")));
+            }
+        }
+    }
+
+    protected void importPrioritizedDataStorage(ModuleConfigurationItem<?> item) throws ModuleException {
+        PrioritizedDataStorage pds = item.getTypedValue();
+        PluginConfiguration dataStorageConf = pds.getDataStorageConfiguration();
+        if (pluginService.findPluginConfigurationByLabel(dataStorageConf.getLabel()).isPresent()) {
+            LOGGER.warn("A plugin configuration already exists with same label, skipping import of {}.",
+                        dataStorageConf.getLabel());
+        } else {
+            EntityInvalidException validationIssues = PluginUtils.validate(dataStorageConf);
+            if (validationIssues != null) {
+                LOGGER.warn("Skipping import of {} for these reasons: {}",
+                            dataStorageConf.getLabel(),
+                            validationIssues.getMessages().stream().collect(Collectors.joining(",", "", ".")));
+            } else {
+                if (prioritizedDataStorageRepository
+                        .findOneByDataStorageTypeAndPriority(pds.getDataStorageType(), pds.getPriority())
+                        != null) {
+                    LOGGER.warn(
+                            "A prioritized data storage with same priority already exists, skipping import of {}.",
+                            dataStorageConf.getLabel());
+                } else {
+                    Long oldPriority = pds.getPriority();
+                    PrioritizedDataStorage imported = prioritizedDataStorageService.create(dataStorageConf);
+                    imported.setPriority(oldPriority);
+                    prioritizedDataStorageRepository.save(imported);
+                }
+            }
+        }
     }
 
     @Override
     public ModuleConfiguration exportConfiguration() throws ModuleException {
-        List<ModuleConfigurationItem<?>> configuration = new ArrayList<>();
+        List<ModuleConfigurationItem<?>> configurations = new ArrayList<>();
         // Fill list using ModuleConfigurationItem#build
-        // TODO
-        return ModuleConfiguration.build(info, configuration);
+        for (PrioritizedDataStorage pds : prioritizedDataStorageRepository.findAll()) {
+            configurations.add(ModuleConfigurationItem.build(pds));
+        }
+        for (PluginConfiguration plgConf : pluginConfigurationRepository.findAll()) {
+            if (!plgConf.getInterfaceNames().contains(IDataStorage.class.getName())) {
+                configurations.add(ModuleConfigurationItem.build(plgConf));
+            }
+        }
+        return ModuleConfiguration.build(info, configurations);
     }
 }
---
+++ rs-storage/storage/storage-service/src/test/java/fr/cnes/regards/modules/storage/service/StorageConfigurationManagerIT.java
@@ -0,0 +1,22 @@
+package fr.cnes.regards.modules.storage.service;
+
+import org.junit.Test;
+
+import fr.cnes.regards.framework.test.integration.AbstractRegardsServiceTransactionalIT;
+
+/**
+ * @author Sylvain VISSIERE-GUERINET
+ */
+public class StorageConfigurationManagerIT extends AbstractRegardsServiceTransactionalIT {
+
+    @Test
+    public void testExport() {
+
+    }
+
+    @Test
+    public void testImport() {
+
+    }
+
+}
