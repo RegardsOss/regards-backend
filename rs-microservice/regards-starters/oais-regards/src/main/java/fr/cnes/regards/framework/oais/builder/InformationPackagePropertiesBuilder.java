7c98e1e3408657d62898f46f35eea917555f5208
Marc Sordi <marc.sordi@c-s.fr>
2018-07-19 10:34:38 +0200

    [rs-microservice] add reference to OAIS data object

--- rs-microservice/regards-starters/oais-regards/src/main/java/fr/cnes/regards/framework/oais/builder/InformationPackagePropertiesBuilder.java
+++ rs-microservice/regards-starters/oais-regards/src/main/java/fr/cnes/regards/framework/oais/builder/InformationPackagePropertiesBuilder.java
@@ -293,13 +293,25 @@ public class InformationPackagePropertiesBuilder implements IOAISBuilder<Informa
     }
 
     /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param filename
-     * @param algorithm
-     * @param checksum
-     * @param fileSize
-     * @param urls
+     * Set <b>required</b> data object properties for a data object reference<br/>
+     * Use this method to reference an external data object that will not be managed by archival storage (i.e. physical
+     * file will not be stored by the system)<br/>
+     * @param dataType {@link DataType}
+     * @param filename filename
+     * @param url external url
+     */
+    public void setDataObjectReference(DataType dataType, String filename, URL url) {
+        contentInformationBuilder.setDataObjectReference(dataType, filename, url);
+    }
+
+    /**
+     * Set <b>required</b> data object properties<br/>
+     * @param dataType {@link DataType}
+     * @param filename filename
+     * @param algorithm checksum algorithm
+     * @param checksum the checksum
+     * @param fileSize <b>optional</b> file size
+     * @param urls references to the physical file
      */
     public void setDataObject(DataType dataType, String filename, String algorithm, String checksum, Long fileSize,
             URL... urls) {
@@ -307,12 +319,13 @@ public class InformationPackagePropertiesBuilder implements IOAISBuilder<Informa
     }
 
     /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param filename
-     * @param algorithm
-     * @param checksum
-     * @param fileSize
+     * Set <b>required</b> data object properties
+     * @param dataType {@link DataType}
+     * @param filePath reference to the physical file
+     * @param filename filename
+     * @param algorithm checksum algorithm
+     * @param checksum the checksum
+     * @param fileSize file size
      */
     public void setDataObject(DataType dataType, Path filePath, String filename, String algorithm, String checksum,
             Long fileSize) {
@@ -320,63 +333,23 @@ public class InformationPackagePropertiesBuilder implements IOAISBuilder<Informa
     }
 
     /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param url
-     * @param filename
-     * @param checksum
-     * @param fileSize
-     */
-    public void setDataObject(DataType dataType, URL url, String filename, String checksum, Long fileSize) {
-        contentInformationBuilder.setDataObject(dataType, url, filename, checksum, fileSize);
-    }
-
-    /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param filename
-     * @param checksum
-     * @param fileSize
-     */
-    public void setDataObject(DataType dataType, Path filePath, String filename, String checksum, Long fileSize) {
-        contentInformationBuilder.setDataObject(dataType, filePath, filename, checksum, fileSize);
-    }
-
-    /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param url
-     * @param algorithm
-     * @param checksum
-     */
-    public void setDataObject(DataType dataType, URL url, String algorithm, String checksum) {
-        contentInformationBuilder.setDataObject(dataType, url, algorithm, checksum);
-    }
-
-    /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param algorithm
-     * @param checksum
+     * Alias for {@link ContentInformationBuilder#setDataObject(DataType, Path, String, String, String, Long)} (no
+     * file size)
+     * @param dataType {@link DataType}
+     * @param filePath reference to the physical file
+     * @param algorithm checksum algorithm
+     * @param checksum the checksum
      */
     public void setDataObject(DataType dataType, Path filePath, String algorithm, String checksum) {
         contentInformationBuilder.setDataObject(dataType, filePath, algorithm, checksum);
     }
 
     /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param url
-     * @param checksum
-     */
-    public void setDataObject(DataType dataType, URL url, String checksum) {
-        contentInformationBuilder.setDataObject(dataType, url, checksum);
-    }
-
-    /**
-     * Set the data object to the information package thanks to the underlying content information builder using the given parameters
-     * @param dataType
-     * @param checksum
+     * Alias for {@link ContentInformationBuilder#setDataObject(DataType, Path, String, String, String, Long)} (no file
+     * size and MD5 default checksum algorithm)
+     * @param dataType {@link DataType}
+     * @param filePath reference to the physical file
+     * @param checksum the checksum
      */
     public void setDataObject(DataType dataType, Path filePath, String checksum) {
         contentInformationBuilder.setDataObject(dataType, filePath, checksum);
