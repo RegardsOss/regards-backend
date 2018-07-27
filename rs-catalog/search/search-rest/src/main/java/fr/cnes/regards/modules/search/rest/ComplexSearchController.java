ed0b85b13296f204f9f909d30d9192442a3ba16e
sbinda <sebastien.binda@c-s.fr>
2018-07-27 17:37:03 +0200

    [rs-catalog] WIP.

--- rs-catalog/search/search-rest/src/main/java/fr/cnes/regards/modules/search/rest/ComplexSearchController.java
+++ rs-catalog/search/search-rest/src/main/java/fr/cnes/regards/modules/search/rest/ComplexSearchController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
@@ -16,19 +16,19 @@
  * You should have received a copy of the GNU General Public License
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.cnes.regards.modules.search.client;
+package fr.cnes.regards.modules.search.rest;
 
 import java.util.Collection;
 
 import org.springframework.hateoas.Resource;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import fr.cnes.regards.framework.feign.annotation.RestClient;
 import fr.cnes.regards.framework.oais.urn.DataType;
 import fr.cnes.regards.modules.entities.domain.feature.EntityFeature;
 import fr.cnes.regards.modules.indexer.domain.summary.DocFilesSummary;
@@ -37,19 +37,17 @@ import fr.cnes.regards.modules.search.domain.plugin.SearchEngineMappings;
 import fr.cnes.regards.modules.search.domain.plugin.legacy.FacettedPagedResources;
 
 /**
- * Complex search client. Handle complex searches on catalog with :
- *  - Multiple search request
- *  - Handling search engine implementations (legacy, opensearch, ...)
- *  - Handling a dataset on which apply search
- *  - Handling search date limit
- *  - Handling includes entity by ids
- *  - Handling excludes entity by ids
+ * Complex search controller. Handle complex searches on catalog with multiple search requests. Each request handles :
+ *  - search engine implementations (legacy, opensearch, ...)
+ *  - dataset on which apply search
+ *  - search date limit
+ *  - includes entity by ids
+ *  - excludes entity by ids
  * @author Sébastien Binda
- *
  */
-@RestClient(name = "rs-catalog")
-@RequestMapping(value = IComplexSearchClient.TYPE_MAPPING, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public interface IComplexSearchClient {
+@RestController
+@RequestMapping(path = ComplexSearchController.TYPE_MAPPING)
+public class ComplexSearchController {
 
     static final String TYPE_MAPPING = "/complex/search";
 
@@ -59,17 +57,24 @@ public interface IComplexSearchClient {
      * Compute a DocFileSummary for current user, for specified request context, for asked file types (see
      * {@link DataType})
      */
-    @RequestMapping(method = RequestMethod.POST, value = IComplexSearchClient.SUMMARY_MAPPING)
-    ResponseEntity<DocFilesSummary> computeDatasetsSummary(
-            @RequestBody Collection<ComplexSearchRequest> complexSearchRequests);
+    @RequestMapping(method = RequestMethod.POST, value = ComplexSearchController.SUMMARY_MAPPING)
+    public ResponseEntity<DocFilesSummary> computeDatasetsSummary(
+            @RequestBody Collection<ComplexSearchRequest> complexSearchRequests) {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 
     /**
      * Compute a complex search
      * {@link DataType})
      */
     @RequestMapping(method = RequestMethod.POST)
-    ResponseEntity<FacettedPagedResources<Resource<EntityFeature>>> search(
+    public ResponseEntity<FacettedPagedResources<Resource<EntityFeature>>> search(
             @RequestBody Collection<ComplexSearchRequest> complexSearchRequests,
-            @RequestParam(SearchEngineMappings.PAGE) int page, @RequestParam(SearchEngineMappings.SIZE) int size);
+            @RequestParam(SearchEngineMappings.PAGE) int page, @RequestParam(SearchEngineMappings.SIZE) int size) {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
