a89032febef7a2b55dd97fab90f59cad997dd89b
serut <leo.mieulet@c-s.fr>
2018-07-31 10:31:45 +0200

    [rs-microservice] Add an function that extracts from an user request the public URI

--- rs-microservice/regards-starters/module-regards/src/main/java/fr/cnes/regards/framework/module/rest/utils/HttpUtils.java
+++ rs-microservice/regards-starters/module-regards/src/main/java/fr/cnes/regards/framework/module/rest/utils/HttpUtils.java
@@ -18,6 +18,12 @@
  */
 package fr.cnes.regards.framework.module.rest.utils;
 
+import com.google.common.net.HttpHeaders;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -46,4 +52,80 @@ public final class HttpUtils {
         return (pHttpStatus.value() / HTTP_CODE_CLASS_MULTIPLIER) == 2;
     }
 
+
+    /**
+     * Build the public URL of the given endpoint by extracting headers from an user request
+     * @param request      Request receive by a REST endpoint
+     * @param endpointPath the endpoint you want to get the link
+     * @return A public URL (with the gateway address instead of this µService adress) that user can contact
+     */
+    public static URI retrievePublicURI(HttpServletRequest request, String endpointPath) throws MalformedURLException, URISyntaxException {
+        // Save the current URL
+        URL url = new URL(request.getRequestURL().toString());
+        String userInfo = url.getUserInfo();
+        String scheme = url.getProtocol();
+
+        String host;
+        int port;
+        // Here is the same implementation than spring-hateoas.
+        // see ControllerLinkBuilder.java for additional infos
+        String headerXForwardedHost = request.getHeader(HttpHeaders.X_FORWARDED_HOST);
+        if (headerXForwardedHost != null) {
+            // Handle several IP separated by a comma
+            if (headerXForwardedHost.contains(",")) {
+                String[] headerSplit = headerXForwardedHost.split(",");
+                host = retrieveHostFromHeader(headerSplit[0]);
+                // handle port extraction from this header
+                port = retrievePortFromHeader(headerSplit[0]);
+            } else {
+                host = retrieveHostFromHeader(headerXForwardedHost);
+                // handle port extraction from this header
+                port = retrievePortFromHeader(headerXForwardedHost);
+            }
+        } else {
+            // Fallback to the original request
+            host = url.getHost();
+            port = url.getPort();
+        }
+        // Check if the header with the port is provided
+        String portHeader = request.getHeader(HttpHeaders.X_FORWARDED_PORT);
+        if (portHeader != null && !portHeader.isEmpty()) {
+            port = Integer.parseInt(portHeader);
+        }
+
+        // Check if the protocol is provided
+        String sslHeader = request.getHeader("X-Forwarded-Ssl");
+        String protoHeader = request.getHeader(HttpHeaders.X_FORWARDED_PROTO);
+
+        if (protoHeader != null && !protoHeader.isEmpty()) {
+            scheme = protoHeader;
+        } else if (sslHeader != null && sslHeader.equalsIgnoreCase("on")) {
+            scheme = "https";
+        }
+
+        // Create the URL
+        return new URI(scheme, userInfo, host, port, endpointPath, null, null);
+    }
+
+    private static int retrievePortFromHeader(String header) {
+        int port;
+        if (header.contains(":")) {
+            String[] hostAndPort = header.split(":");
+            port = Integer.parseInt(hostAndPort[1]);
+        } else {
+            port = 80;
+        }
+        return port;
+    }
+
+    private static String retrieveHostFromHeader(String header) {
+        String host;
+        if (header.contains(":")) {
+            String[] hostAndPort = header.split(":");
+            host = hostAndPort[0];
+        } else {
+            host = header;
+        }
+        return host;
+    }
 }
