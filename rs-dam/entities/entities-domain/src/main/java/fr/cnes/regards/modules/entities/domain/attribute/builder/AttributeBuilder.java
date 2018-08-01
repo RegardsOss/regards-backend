7eb10d15bbded3a7a28a38ec52f3e6f3494ef959
sbinda <sebastien.binda@c-s.fr>
2018-08-01 10:47:32 +0200

    [rs-dam] Fix attribute cast to String by using toString method on object instead of direct cast.
    Former-commit-id: 9b0c7ab6bfef9dc9642973536254f84d2d2cdd64

--- rs-dam/entities/entities-domain/src/main/java/fr/cnes/regards/modules/entities/domain/attribute/builder/AttributeBuilder.java
+++ rs-dam/entities/entities-domain/src/main/java/fr/cnes/regards/modules/entities/domain/attribute/builder/AttributeBuilder.java
@@ -141,7 +141,7 @@ public final class AttributeBuilder {
                 case LONG_INTERVAL:
                     return (T) buildLongInterval(name, (Range<Long>) value);
                 case STRING:
-                    return (T) buildString(name, (String) value);
+                    return (T) buildString(name, value.toString());
                 case STRING_ARRAY:
                     if (value instanceof Collection) {
                         return (T) buildStringCollection(name, (Collection) value);
