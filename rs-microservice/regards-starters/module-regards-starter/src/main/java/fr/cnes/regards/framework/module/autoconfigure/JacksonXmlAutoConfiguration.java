5ea192a7a7d5282ff6336b5e0242eb11bde960d4
svissier <sylvain.vissiere-guerinet@c-s.fr>
2018-08-07 14:33:29 +0200

    [rs-microservice] fix jackson xml serialization

---
+++ rs-microservice/regards-starters/module-regards-starter/src/main/java/fr/cnes/regards/framework/module/autoconfigure/JacksonXmlAutoConfiguration.java
@@ -0,0 +1,38 @@
+package fr.cnes.regards.framework.module.autoconfigure;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
+/**
+ * We have to trick spring so we can access all of jackson xml converters to add jaxb annotation support
+ *
+ * @author Sylvain VISSIERE-GUERINET
+ */
+@Component
+public class JacksonXmlAutoConfiguration {
+
+    @Autowired
+    private List<MappingJackson2XmlHttpMessageConverter> jacksonConverters;
+
+    @EventListener
+    public void handle(ApplicationReadyEvent event) {
+        JaxbAnnotationModule jaxbModule = new JaxbAnnotationModule();
+        for (MappingJackson2XmlHttpMessageConverter jacksonConverter : jacksonConverters) {
+            ObjectMapper om = jacksonConverter.getObjectMapper();
+            if(om instanceof XmlMapper) {
+                ((XmlMapper) om).setDefaultUseWrapper(false);
+            }
+            jacksonConverter.getObjectMapper().registerModule(jaxbModule);
+        }
+    }
+
+}
