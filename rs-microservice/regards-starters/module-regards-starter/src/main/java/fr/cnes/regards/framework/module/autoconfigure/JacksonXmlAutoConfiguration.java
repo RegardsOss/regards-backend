a07a17abb641c414934ae06d80f335f6443fad2f
msi <marc.sordi@c-s.fr>
2018-09-12 15:32:41 +0200

    [rs-microservice] do not serialize null

--- rs-microservice/regards-starters/module-regards-starter/src/main/java/fr/cnes/regards/framework/module/autoconfigure/JacksonXmlAutoConfiguration.java
+++ rs-microservice/regards-starters/module-regards-starter/src/main/java/fr/cnes/regards/framework/module/autoconfigure/JacksonXmlAutoConfiguration.java
@@ -3,13 +3,13 @@ package fr.cnes.regards.framework.module.autoconfigure;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
  * @author Sylvain VISSIERE-GUERINET
  */
 @Component
-@ConditionalOnProperty(name = "regards.xml.jackson.as.jaxb", havingValue = "true")
+@ConditionalOnProperty(name = "regards.xml.jackson.as.jaxb", havingValue = "true", matchIfMissing = true)
 public class JacksonXmlAutoConfiguration {
 
     @Autowired
@@ -31,6 +31,7 @@ public class JacksonXmlAutoConfiguration {
         JaxbAnnotationModule jaxbModule = new JaxbAnnotationModule();
         for (MappingJackson2XmlHttpMessageConverter jacksonConverter : jacksonConverters) {
             ObjectMapper om = jacksonConverter.getObjectMapper();
+            om.setSerializationInclusion(Include.NON_NULL);
             if (om instanceof XmlMapper) {
                 ((XmlMapper) om).setDefaultUseWrapper(false);
             }
