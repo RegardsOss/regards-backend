a0ff3d1e2e5aa5a5a574e96d05f6a3118c8d2dfa
sbinda <sebastien.binda@c-s.fr>
2018-09-19 16:23:15 +0200

    [rs-storage] Add specifications to ready endpoint.

--- rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/ready/StorageModuleReady.java
+++ rs-storage/storage/storage-service/src/main/java/fr/cnes/regards/modules/storage/service/ready/StorageModuleReady.java
@@ -1,17 +1,22 @@
 package fr.cnes.regards.modules.storage.service.ready;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Lists;
+
 import fr.cnes.regards.framework.module.ready.IModuleReady;
 import fr.cnes.regards.framework.module.ready.ModuleReadiness;
+import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
 import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
 import fr.cnes.regards.modules.storage.domain.plugin.IAllocationStrategy;
-import fr.cnes.regards.modules.storage.domain.plugin.IOnlineDataStorage;
+import fr.cnes.regards.modules.storage.domain.plugin.IDataStorage;
 import fr.cnes.regards.modules.storage.domain.plugin.ISecurityDelegation;
+import fr.cnes.regards.modules.storage.domain.ready.StorageReadySpecifications;
 
 /**
  * Allows to know if module storage is ready or not.
@@ -23,7 +28,7 @@ import fr.cnes.regards.modules.storage.domain.plugin.ISecurityDelegation;
  * @author Sylvain VISSIERE-GUERINET
  */
 @Component
-public class StorageModuleReady implements IModuleReady {
+public class StorageModuleReady implements IModuleReady<StorageReadySpecifications> {
 
     /**
      * {@link IPluginService} instance
@@ -32,22 +37,22 @@ public class StorageModuleReady implements IModuleReady {
     private IPluginService pluginService;
 
     @Override
-    public ModuleReadiness isReady() {
+    public ModuleReadiness<StorageReadySpecifications> isReady() {
         boolean ready = true;
         List<String> reasons = Lists.newArrayList();
         //lets check allocation strategy
-        long numberAllocationStrategy = pluginService.getPluginConfigurationsByType(IAllocationStrategy.class).stream()
-                .filter(pc -> pc.isActive()).count();
-        if (numberAllocationStrategy != 1) {
-            reasons.add(
-                    "There should be one and only one Allocation Strategy configured and active in the system. There is currently: "
-                            + numberAllocationStrategy);
+        Set<PluginConfiguration> strategies = pluginService.getPluginConfigurationsByType(IAllocationStrategy.class)
+                .stream().filter(pc -> pc.isActive()).collect(Collectors.toSet());
+        if (strategies.size() <= 1) {
+            reasons.add("There should be one and only one Allocation Strategy configured and active in the system. There is currently: "
+                    + strategies.size());
             ready = false;
         }
+
         // check data storage
-        long numberDataStorage = pluginService.getPluginConfigurationsByType(IOnlineDataStorage.class).stream()
-                .filter(pc -> pc.isActive()).count();
-        if (numberDataStorage <= 0) {
+        Set<PluginConfiguration> dataStorages = pluginService.getPluginConfigurationsByType(IDataStorage.class).stream()
+                .filter(pc -> pc.isActive()).collect(Collectors.toSet());
+        if (dataStorages.isEmpty()) {
             reasons.add("There should be at least one ONLINE DataStorage configured and active in the system.");
             ready = false;
         }
@@ -55,11 +60,18 @@ public class StorageModuleReady implements IModuleReady {
         long numberSecurityDelegation = pluginService.getPluginConfigurationsByType(ISecurityDelegation.class).stream()
                 .filter(pc -> pc.isActive()).count();
         if (numberSecurityDelegation != 1) {
-            reasons.add(
-                    "There should be one and only one Security Delegation configured and active in the system. There is currently: "
-                            + numberSecurityDelegation);
+            reasons.add("There should be one and only one Security Delegation configured and active in the system. There is currently: "
+                    + numberSecurityDelegation);
             ready = false;
         }
-        return new ModuleReadiness(ready, reasons);
+
+        String allocationStrategy = null;
+        if (strategies.stream().findFirst().isPresent()) {
+            allocationStrategy = strategies.stream().findFirst().get().getLabel();
+        }
+        List<String> storages = dataStorages.stream().map(ds -> ds.getLabel()).collect(Collectors.toList());
+
+        return new ModuleReadiness<StorageReadySpecifications>(ready, reasons,
+                new StorageReadySpecifications(allocationStrategy, storages));
     }
 }
