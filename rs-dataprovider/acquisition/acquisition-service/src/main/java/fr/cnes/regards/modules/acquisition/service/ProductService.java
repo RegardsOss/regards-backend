868f15fde555e94b9105be8773fb3069d4d40327
msi <marc.sordi@c-s.fr>
2018-10-09 15:39:57 +0200

    [rs-dataprovider] limit working packet size
    Former-commit-id: e7211a883f06b9d2a24773a94106bbe1bc1f076e

--- rs-dataprovider/acquisition/acquisition-service/src/main/java/fr/cnes/regards/modules/acquisition/service/ProductService.java
+++ rs-dataprovider/acquisition/acquisition-service/src/main/java/fr/cnes/regards/modules/acquisition/service/ProductService.java
@@ -97,10 +97,10 @@ public class ProductService implements IProductService {
     @Autowired
     private IAcquisitionFileRepository acqFileRepository;
 
-    @Value("${regards.acquisition.sip.bulk.request.limit:1000}")
+    @Value("${regards.acquisition.sip.bulk.request.limit:100}")
     private Integer bulkRequestLimit;
 
-    @Value("${regards.acquisition.pagination.default.page.size:1000}")
+    @Value("${regards.acquisition.pagination.default.page.size:100}")
     private Integer defaultPageSize;
 
     @Override
