433ecd63203f0885a1f484d99e1e5d967dead645
Léo Mieulet <leo.mieulet@c-s.fr>
2018-10-15 11:52:12 +0200

    [rs-storage] Fix

--- rs-storage/storage/storage-dao/src/main/java/fr/cnes/regards/modules/storage/dao/AIPQueryGenerator.java
+++ rs-storage/storage/storage-dao/src/main/java/fr/cnes/regards/modules/storage/dao/AIPQueryGenerator.java
@@ -146,7 +146,7 @@ public class AIPQueryGenerator {
         for (String tag : tags) {
             tagPredicates.add("'" + tag + "'");
         }
-        return "(json_aip#>'{properties,pdi,contextInformation,tags}'?|array["
+        return "(json_aip#>'{properties,pdi,contextInformation,tags}'??|array["
                 + String.join(" , ", tagPredicates) + "])";
     }
 
