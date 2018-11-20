b4d765c659abb23eeb69636427f6a078c8b19b85
sbinda <sebastien.binda@c-s.fr>
2018-11-20 17:27:21 +0100

    [rs-dam] Fix plugin id
    Former-commit-id: 428298841af7f6958ad3ddc4d3fafc7f1d2a6f96

--- rs-dam/dam/dam-plugins/src/main/java/fr/cnes/regards/modules/dam/plugin/dataaccess/accessright/CustomDataObjectsAccessPlugin.java
+++ rs-dam/dam/dam-plugins/src/main/java/fr/cnes/regards/modules/dam/plugin/dataaccess/accessright/CustomDataObjectsAccessPlugin.java
@@ -33,8 +33,8 @@ import fr.cnes.regards.modules.opensearch.service.exception.OpenSearchParseExcep
  * Plugin to allow access to dataobjects with an opensearch request.
  * @author Sébastien Binda
  */
-@Plugin(id = "OldDataObjectsAccess", version = "4.0.0-SNAPSHOT",
-        description = "Allow access to dataObjects matching the given opensearch lucen formated query.",
+@Plugin(id = "CustomDataObjectsAccessPlugin", version = "4.0.0-SNAPSHOT",
+        description = "Allow access to dataObjects matching the given opensearch lucene formated query.",
         author = "REGARDS Team", contact = "regards@c-s.fr", licence = "LGPLv3.0", owner = "CSSI",
         url = "https://github.com/RegardsOss")
 public class CustomDataObjectsAccessPlugin implements IDataObjectAccessFilterPlugin {
