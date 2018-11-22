588f2eb979da2eebcac401766d5719f9aeb11a1e
sbinda <sebastien.binda@c-s.fr>
2018-11-22 15:16:34 +0100

    [rs-dam] Fix conf importation.
    Former-commit-id: 7ada2b8336931c561d74c0c4aaa3f58d9cb035b3

--- rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
+++ rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
@@ -1,13 +1,17 @@
 package fr.cnes.regards.modules.dam.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Sets;
 
 import fr.cnes.regards.framework.module.manager.AbstractModuleManager;
 import fr.cnes.regards.framework.module.manager.ModuleConfiguration;
@@ -46,43 +50,19 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
     @Override
     protected Set<String> importConfiguration(ModuleConfiguration configuration) {
         Set<String> importErrors = new HashSet<>();
-        for (ModuleConfigurationItem<?> item : configuration.getConfiguration()) {
-            // Lets import connection and data sources
-            if (PluginConfiguration.class.isAssignableFrom(item.getKey())) {
-                PluginConfiguration plgConf = item.getTypedValue();
-                if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
-                    importErrors.add(String.format(PLUGIN_CONFIGURATION_ALREADY_EXISTS, plgConf.getLabel()));
-                } else {
-                    EntityInvalidException validationIssues = PluginUtils.validate(plgConf);
-                    if (validationIssues == null) {
-                        // Now that we are about to create the plugin configuration, lets check for IDataStorage
-                        if (plgConf.getInterfaceNames().contains(IConnectionPlugin.class.getName())) {
-                            try {
-                                connectionService.createDBConnection(plgConf);
-                            } catch (ModuleException e) {
-                                importErrors.add(String.format("Skipping import of Data Storage %s: %s",
-                                                               plgConf.getLabel(), e.getMessage()));
-                                logger.error(e.getMessage(), e);
-                            }
-                        } else {
-                            if (plgConf.getInterfaceNames().contains(IDataSourcePlugin.class.getName())) {
-                                try {
-                                    dataSourceService.createDataSource(plgConf);
-                                } catch (ModuleException e) {
-                                    // This should not occurs, but we never know
-                                    importErrors.add(String.format("Skipping import of PluginConfiguration %s: %s",
-                                                                   plgConf.getLabel(), e.getMessage()));
-                                    logger.error(e.getMessage(), e);
-                                }
-                            }
-                        }
-                    } else {
-                        importErrors.add(String.format(VALIDATION_ISSUES, plgConf.getLabel(), validationIssues
-                                .getMessages().stream().collect(Collectors.joining(",", "", "."))));
-                    }
-                }
-            }
+        Pair<Set<PluginConfiguration>, Set<PluginConfiguration>> confs = dispatch(getPluginConfs(configuration
+                .getConfiguration()));
+
+        // First create connections
+        for (PluginConfiguration plgConf : confs.getLeft()) {
+            importErrors.addAll(createConnection(plgConf));
         }
+
+        // Then create datasources
+        for (PluginConfiguration plgConf : confs.getRight()) {
+            importErrors.addAll(createDatasource(plgConf));
+        }
+
         return importErrors;
     }
 
@@ -98,5 +78,72 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
             configurations.add(ModuleConfigurationItem.build(connection));
         }
         return ModuleConfiguration.build(info, configurations);
+    }
+
+    private Set<String> createConnection(PluginConfiguration plgConf) {
+        Set<String> importErrors = new HashSet<>();
+        if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
+            importErrors.add(String.format(PLUGIN_CONFIGURATION_ALREADY_EXISTS, plgConf.getLabel()));
+        } else {
+            EntityInvalidException validationIssues = PluginUtils.validate(plgConf);
+            if (validationIssues == null) {
+                try {
+                    connectionService.createDBConnection(plgConf);
+                } catch (ModuleException e) {
+                    importErrors.add(String.format("Skipping import of Data Storage %s: %s", plgConf.getLabel(),
+                                                   e.getMessage()));
+                    logger.error(e.getMessage(), e);
+                }
+            } else {
+                importErrors.add(String
+                        .format(VALIDATION_ISSUES, plgConf.getLabel(),
+                                validationIssues.getMessages().stream().collect(Collectors.joining(",", "", "."))));
+            }
+
+        }
+        return importErrors;
+    }
+
+    private Set<String> createDatasource(PluginConfiguration plgConf) {
+        Set<String> importErrors = new HashSet<>();
+        if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
+            importErrors.add(String.format(PLUGIN_CONFIGURATION_ALREADY_EXISTS, plgConf.getLabel()));
+        } else {
+            EntityInvalidException validationIssues = PluginUtils.validate(plgConf);
+            if (validationIssues == null) {
+                try {
+                    dataSourceService.createDataSource(plgConf);
+                } catch (ModuleException e) {
+                    // This should not occurs, but we never know
+                    importErrors.add(String.format("Skipping import of PluginConfiguration %s: %s", plgConf.getLabel(),
+                                                   e.getMessage()));
+                    logger.error(e.getMessage(), e);
+                }
+            } else {
+                importErrors.add(String
+                        .format(VALIDATION_ISSUES, plgConf.getLabel(),
+                                validationIssues.getMessages().stream().collect(Collectors.joining(",", "", "."))));
+            }
+
+        }
+        return importErrors;
+    }
+
+    private Set<PluginConfiguration> getPluginConfs(Collection<ModuleConfigurationItem<?>> items) {
+        return items.stream().filter(i -> PluginConfiguration.class.isAssignableFrom(i.getKey()))
+                .map(i -> (PluginConfiguration) i.getTypedValue()).collect(Collectors.toSet());
+    }
+
+    private Pair<Set<PluginConfiguration>, Set<PluginConfiguration>> dispatch(Collection<PluginConfiguration> confs) {
+        Set<PluginConfiguration> connections = Sets.newHashSet();
+        Set<PluginConfiguration> datasources = Sets.newHashSet();
+        confs.forEach(c -> {
+            if (c.getInterfaceNames().contains(IConnectionPlugin.class.getName())) {
+                connections.add(c);
+            } else if (c.getInterfaceNames().contains(IDataSourcePlugin.class.getName())) {
+                datasources.add(c);
+            }
+        });
+        return Pair.of(connections, datasources);
     }
 }
