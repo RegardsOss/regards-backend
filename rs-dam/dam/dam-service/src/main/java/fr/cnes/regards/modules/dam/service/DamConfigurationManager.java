bca590bb42a850666a0e65bdc2ab45f7985da136
sbinda <sebastien.binda@c-s.fr>
2018-11-22 17:26:44 +0100

    [rs-dam] Fix importaition of datasource plugin configurations.
    Former-commit-id: 957769849806e72bb75df8430239bf346af039b9

--- rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
+++ rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package fr.cnes.regards.modules.dam.service;
 
 import java.util.ArrayList;
@@ -8,17 +28,22 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Sets;
 
+import fr.cnes.regards.framework.encryption.exception.EncryptionException;
 import fr.cnes.regards.framework.module.manager.AbstractModuleManager;
 import fr.cnes.regards.framework.module.manager.ModuleConfiguration;
 import fr.cnes.regards.framework.module.manager.ModuleConfigurationItem;
 import fr.cnes.regards.framework.module.rest.exception.EntityInvalidException;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
+import fr.cnes.regards.framework.modules.plugins.domain.PluginParameter;
 import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
 import fr.cnes.regards.framework.utils.plugins.PluginUtils;
 import fr.cnes.regards.modules.dam.domain.datasources.plugins.IConnectionPlugin;
@@ -30,9 +55,12 @@ import fr.cnes.regards.modules.dam.service.datasources.IDataSourceService;
  * DAM configuration manager. Exports model & connection plugin configurations & datasource plugin configurations.
  *
  * @author Sylvain VISSIERE-GUERINET
+ * @author Sébastien Binda
  */
 @Component
 public class DamConfigurationManager extends AbstractModuleManager<Void> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DamConfigurationManager.class);
 
     public static final String PLUGIN_CONFIGURATION_ALREADY_EXISTS = "A plugin configuration already exists with same label, skipping import of %s.";
 
@@ -112,6 +140,10 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
             EntityInvalidException validationIssues = PluginUtils.validate(plgConf);
             if (validationIssues == null) {
                 try {
+                    // Firsts create missing linked plugin confs
+                    plgConf.getParameters().stream().filter(p -> p.getPluginConfiguration() != null)
+                            .forEach(this::createOrLinkDatasourceToConnection);
+                    // Then create plugin conf
                     dataSourceService.createDataSource(plgConf);
                 } catch (ModuleException e) {
                     // This should not occurs, but we never know
@@ -129,11 +161,45 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
         return importErrors;
     }
 
+    /**
+     * Allow to link existing connection {@link PluginConfiguration} as a {@link PluginParameter} to a datasource {@link PluginConfiguration}.
+     * As exported configuration does not export ids, we have to do this link before saving datasources.
+     * Indeed some datasource are linked to a connection {@link PluginConfiguration}. This connection {@link PluginConfiguration} needs
+     * to be created before.
+     * @param p {@link PluginParameter} {@link PluginConfiguration} to link as a plugin parameter
+     */
+    private void createOrLinkDatasourceToConnection(PluginParameter p) {
+        try {
+            PluginConfiguration linked = pluginService
+                    .getPluginConfigurationByLabel(p.getPluginConfiguration().getLabel());
+            p.setPluginConfiguration(linked);
+        } catch (EntityNotFoundException e) {
+            // Plugin conf does not exists, create it
+            try {
+                PluginConfiguration linked = pluginService.savePluginConfiguration(p.getPluginConfiguration());
+                p.setPluginConfiguration(linked);
+            } catch (EntityInvalidException | EntityNotFoundException | EncryptionException e1) {
+                LOGGER.error("Error creating pluging parameter.", e);
+            }
+        }
+    }
+
+    /**
+     * Get all {@link PluginConfiguration}s of the {@link ModuleConfigurationItem}s
+     * @param items {@link ModuleConfigurationItem}s
+     * @return  {@link PluginConfiguration}s
+     */
     private Set<PluginConfiguration> getPluginConfs(Collection<ModuleConfigurationItem<?>> items) {
         return items.stream().filter(i -> PluginConfiguration.class.isAssignableFrom(i.getKey()))
                 .map(i -> (PluginConfiguration) i.getTypedValue()).collect(Collectors.toSet());
     }
 
+    /**
+     * Dispatch given  {@link PluginConfiguration}s into two {@link Set}s.
+     * Left ones are {@link IConnectionPlugin}s and right ones are {@link IDataSourcePlugin}s
+     * @param confs
+     * @return {@link Pair} of {@link IConnectionPlugin}s/{@link IDataSourcePlugin}s
+     */
     private Pair<Set<PluginConfiguration>, Set<PluginConfiguration>> dispatch(Collection<PluginConfiguration> confs) {
         Set<PluginConfiguration> connections = Sets.newHashSet();
         Set<PluginConfiguration> datasources = Sets.newHashSet();
