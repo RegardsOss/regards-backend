8c299ba17cd96cc61d19aae29726aa41b1cae682
sbinda <sebastien.binda@c-s.fr>
2019-01-29 08:55:58 +0100

    [rs-microservice] Add contextId property to RestClient for uniq bean name

--- rs-microservice/regards-starters/feign-regards-starter/src/main/java/fr/cnes/regards/framework/feign/annotation/RestClient.java
+++ rs-microservice/regards-starters/feign-regards-starter/src/main/java/fr/cnes/regards/framework/feign/annotation/RestClient.java
@@ -46,16 +46,22 @@ public @interface RestClient {
      * Name of the microservice as it is registered in the eureka server.
      * @return name
      */
-    @AliasFor(annotation = FeignClient.class) String name();
+    @AliasFor(annotation = FeignClient.class)
+    String name();
+
+    @AliasFor(annotation = FeignClient.class)
+    String contextId() default "";
 
     /**
      * Fallback class implementation for the client
      * @return Class
      */
-    @AliasFor(annotation = FeignClient.class) Class<?> fallback() default void.class;
+    @AliasFor(annotation = FeignClient.class)
+    Class<?> fallback() default void.class;
 
     /**
      * An absolute URL or resolvable hostname (the protocol is optional).
      */
-    @AliasFor(annotation = FeignClient.class) String url() default "";
+    @AliasFor(annotation = FeignClient.class)
+    String url() default "";
 }
