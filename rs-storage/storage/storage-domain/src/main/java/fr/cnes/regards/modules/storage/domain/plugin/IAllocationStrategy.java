327ce8a62481b02710fb709da9ef21e9e301d048
msi <marc.sordi@c-s.fr>
2019-01-30 11:35:15 +0100

    [rs-storage] fix method name

--- rs-storage/storage/storage-domain/src/main/java/fr/cnes/regards/modules/storage/domain/plugin/IAllocationStrategy.java
+++ rs-storage/storage/storage-domain/src/main/java/fr/cnes/regards/modules/storage/domain/plugin/IAllocationStrategy.java
@@ -31,7 +31,7 @@ import fr.cnes.regards.modules.storage.domain.database.StorageDataFile;
  * @author Sylvain Vissiere-Guerinet
  *
  */
-@PluginInterface(description = "Interface for all AllocationStrategy plugin", allowMultipleConfigurationActive = false)
+@PluginInterface(description = "Interface for all AllocationStrategy plugin", allowMultipleActiveConfigurations = false)
 @FunctionalInterface
 public interface IAllocationStrategy {
 
