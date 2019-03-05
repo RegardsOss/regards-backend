5f8876c725f82de86923e3393286711eec26af3d
sbinda <sebastien.binda@c-s.fr>
2019-03-05 11:46:21 +0100

    [rs-microservice] WIP.

--- rs-microservice/regards-archetypes/module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/IGreetingsClient.java
+++ rs-microservice/regards-archetypes/module-archetype/src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/IGreetingsClient.java
@@ -1,3 +1,26 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ${package}.client;
+
 import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
@@ -7,29 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import fr.cnes.regards.framework.feign.annotation.RestClient;
 
-#set($symbol_pound='#')
-        #set($symbol_dollar='$')
-        #set($symbol_escape='\' )
-        /*
-         * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
-         *
-         * This file is part of REGARDS.
-         *
-         * REGARDS is free software: you can redistribute it and/or modify
-         * it under the terms of the GNU General Public License as published by
-         * the Free Software Foundation, either version 3 of the License, or
-         * (at your option) any later version.
-         *
-         * REGARDS is distributed in the hope that it will be useful,
-         * but WITHOUT ANY WARRANTY; without even the implied warranty of
-         * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-         * GNU General Public License for more details.
-         *
-         * You should have received a copy of the GNU General Public License
-         * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
-         */
-        package ${package}.client;
-        {package}.domain.Greeting;
+import ${package}.domain.Greeting;
 
 /**
  *
