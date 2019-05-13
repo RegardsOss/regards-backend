275959524eb424dd3730bd3dc002515bea063b15
svissier <sylvain.vissiere-guerinet@c-s.fr>
2019-05-13 17:28:32 +0200

    [rs-storage] simplify DefaultAllocationStrategy: get first online datastorage

--- rs-storage/storage/storage-plugin/src/main/java/fr/cnes/regards/modules/storage/plugin/allocation/strategy/DefaultAllocationStrategyPlugin.java
+++ rs-storage/storage/storage-plugin/src/main/java/fr/cnes/regards/modules/storage/plugin/allocation/strategy/DefaultAllocationStrategyPlugin.java
@@ -19,73 +19,50 @@
 package fr.cnes.regards.modules.storage.plugin.allocation.strategy;
 
 import java.util.Collection;
-import java.util.Comparator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-
 import fr.cnes.regards.framework.microservice.maintenance.MaintenanceException;
 import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
-import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
+import fr.cnes.regards.modules.storage.dao.IPrioritizedDataStorageRepository;
+import fr.cnes.regards.modules.storage.domain.database.DataStorageType;
+import fr.cnes.regards.modules.storage.domain.database.PrioritizedDataStorage;
 import fr.cnes.regards.modules.storage.domain.database.StorageDataFile;
 import fr.cnes.regards.modules.storage.domain.plugin.DispatchErrors;
 import fr.cnes.regards.modules.storage.domain.plugin.IAllocationStrategy;
-import fr.cnes.regards.modules.storage.domain.plugin.IDataStorage;
-import fr.cnes.regards.modules.storage.domain.plugin.IOnlineDataStorage;
 
 /**
  * Default Implementation of IAllocationStrategy.
- * Finds the PluginConfiguration with the highest priority(lowest value of PluginConfiguration#priorityOrder) and associate all the dataFiles to it.
+ * Finds Online data storage with the highest priority(lowest value of {@link PrioritizedDataStorage#priority}) and associate all the dataFiles to it.
  *
  * @author Sylvain Vissiere-Guerinet
  *
  */
-@Plugin(author = "REGARDS Team", description = "Default plugin of Allocation Strategy",
+@Plugin(author = "REGARDS Team",
+        description = "Finds Online data storage with the highest priority and associate all the dataFiles to it.",
         id = "DefaultAllocationStrategyPlugin", version = "1.0", contact = "regards@c-s.fr", licence = "GPLv3",
         owner = "CNES", url = "https://regardsoss.github.io/")
 public class DefaultAllocationStrategyPlugin implements IAllocationStrategy {
 
-    /**
-     * {@link IPluginService} instance
-     */
     @Autowired
-    private IPluginService pluginService;
+    private IPrioritizedDataStorageRepository pdsRepository;
 
     @Override
     public Multimap<Long, StorageDataFile> dispatch(Collection<StorageDataFile> dataFilesToHandle,
             DispatchErrors errors) {
-        // first lets get the plugin configuration of type IDataStorage, then lets get only the active ones,
+        // first lets get the online data storages, then lets get only the active ones,
         // eventually order them and choose the one with the highest priority
-        PluginConfiguration dataStorageConfToUse = pluginService.getPluginConfigurationsByType(IDataStorage.class)
-                .stream().filter(pc -> pc.isActive())
-                .sorted(Comparator.comparing(PluginConfiguration::getPriorityOrder)).findFirst()
+        PluginConfiguration dataStorageConfToUse = pdsRepository
+                .findAllByDataStorageTypeOrderByPriorityAsc(DataStorageType.ONLINE).stream()
+                .filter(pds -> pds.getDataStorageConfiguration().isActive()).sorted().findFirst()
                 .orElseThrow(() -> new MaintenanceException(
-                        "There is no active plugin configuration of type IDataStorage"));
-        PluginConfiguration dataStorageConfForQuicklook;
-        // quicklooks are to be stored online, so lets check if the conf to use is an online data storage, otherwise lets get the first online one
-        if (dataStorageConfToUse.getInterfaceNames().contains(IOnlineDataStorage.class.getName())) {
-            dataStorageConfForQuicklook = dataStorageConfToUse;
-        } else {
-            dataStorageConfForQuicklook = pluginService.getPluginConfigurationsByType(IOnlineDataStorage.class).stream()
-                    .filter(pc -> pc.isActive()).sorted(Comparator.comparing(PluginConfiguration::getPriorityOrder))
-                    .findFirst().orElseThrow(() -> new MaintenanceException(
-                            "There is no active plugin configuration of type IOnlineDataStorage to store quicklooks"));
-        }
+                        "There is no active plugin configuration of type IDataStorage")).getDataStorageConfiguration();
         HashMultimap<Long, StorageDataFile> result = HashMultimap.create(2, dataFilesToHandle.size());
 
-        dataFilesToHandle.forEach(dataFile -> {
-            //This allocation strategy only allows files to be stored into 1 DataStorage
-            if (dataFile.isOnlineMandatory()) {
-                result.put(dataStorageConfForQuicklook.getId(), dataFile);
-                return;
-            } else {
-                result.put(dataStorageConfToUse.getId(), dataFile);
-                return;
-            }
-        });
+        dataFilesToHandle.forEach(dataFile -> result.put(dataStorageConfToUse.getId(), dataFile));
         return result;
     }
 
