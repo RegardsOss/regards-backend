6ddbc5ce30495b408a74fed5814eecc228254f89
msi <marc.sordi@c-s.fr>
2019-05-24 18:07:22 +0200

    [rs-admin] add instance notification client

---
+++ rs-admin/notification/notification-client/src/main/java/fr/cnes/regards/modules/notification/client/IInstanceNotificationClient.java
@@ -0,0 +1,10 @@
+package fr.cnes.regards.modules.notification.client;
+
+/**
+ * Notification client interface for instance notification
+ *
+ * @author Marc SORDI
+ */
+public interface IInstanceNotificationClient extends INotificationClientContract {
+
+}
