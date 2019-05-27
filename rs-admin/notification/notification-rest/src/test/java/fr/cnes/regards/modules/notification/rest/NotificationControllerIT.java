93fe560351ae8940dae8832e2c326dab861cb48e
sbinda <sebastien.binda@c-s.fr>
2019-05-27 14:51:38 +0200

    [rs-admin] Add new endpoint to rerieve notification summary for long polling

--- rs-admin/notification/notification-rest/src/test/java/fr/cnes/regards/modules/notification/rest/NotificationControllerIT.java
+++ rs-admin/notification/notification-rest/src/test/java/fr/cnes/regards/modules/notification/rest/NotificationControllerIT.java
@@ -1,12 +1,12 @@
 package fr.cnes.regards.modules.notification.rest;
 
-import javax.mail.internet.MimeMessage;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import javax.mail.internet.MimeMessage;
+
 import org.hamcrest.Matchers;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -15,13 +15,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.google.common.collect.Sets;
+
 import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
 import fr.cnes.regards.framework.notification.NotificationDTO;
@@ -31,7 +31,6 @@ import fr.cnes.regards.framework.security.role.DefaultRole;
 import fr.cnes.regards.framework.test.integration.AbstractRegardsTransactionalIT;
 import fr.cnes.regards.modules.accessrights.instance.client.IAccountSettingsClient;
 import fr.cnes.regards.modules.accessrights.instance.client.IAccountsClient;
-import fr.cnes.regards.modules.emails.service.IEmailService;
 import fr.cnes.regards.modules.notification.dao.INotificationRepository;
 import fr.cnes.regards.modules.notification.dao.INotificationSettingsRepository;
 import fr.cnes.regards.modules.notification.domain.Notification;
@@ -111,9 +110,7 @@ public class NotificationControllerIT extends AbstractRegardsTransactionalIT {
         NotificationDTO notif = new NotificationDtoBuilder("Lets test", "test", NotificationLevel.INFO, "microservice")
                 .toRoles(new HashSet<>(Arrays.asList(roleName)));
 
-        performDefaultPost(NotificationController.NOTIFICATION_PATH,
-                           notif,
-                           customizer().expectStatusCreated(),
+        performDefaultPost(NotificationController.NOTIFICATION_PATH, notif, customizer().expectStatusCreated(),
                            "error");
         runtimeTenantResolver.forceTenant(getDefaultTenant());
         sendingScheduler.sendDaily();
@@ -134,9 +131,7 @@ public class NotificationControllerIT extends AbstractRegardsTransactionalIT {
         //some lorem ipsum so we have a notification with content
         notif = new NotificationDtoBuilder(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed magna turpis. Curabitur ultrices scelerisque magna pretium mollis. Sed suscipit, ligula eu tempus pretium, lorem quam vehicula urna, vel efficitur leo mauris quis mauris. Pellentesque ac ullamcorper lectus. Aliquam sed tempor massa. Proin ex massa, sodales vel turpis non, sodales rhoncus lacus. Maecenas a convallis nisi. Aliquam felis justo, pellentesque id vestibulum id, tempus sit amet dui. Quisque quis lacus vehicula, gravida lectus a, elementum erat. In vitae venenatis turpis, et venenatis lacus. Phasellus facilisis pellentesque elit, in lacinia enim placerat quis.",
-                "test",
-                NotificationLevel.INFO,
-                "microservice").toRoles(new HashSet<>(Arrays.asList(roleName)));
+                "test", NotificationLevel.INFO, "microservice").toRoles(new HashSet<>(Arrays.asList(roleName)));
         notificationService.createNotification(notif);
         String token = jwtService.generateToken(getDefaultTenant(), "project.admin@test.fr", roleName);
         //        performGet(NotificationController.NOTIFICATION_PATH, token,
@@ -148,34 +143,29 @@ public class NotificationControllerIT extends AbstractRegardsTransactionalIT {
         //                   customizer().expectStatusOk().expectToHaveSize(JSON_PATH_CONTENT, 4),
         //                   "Could not retrieve notifications");
 
-        performGet(NotificationController.NOTIFICATION_PATH,
-                   token,
+        performGet(NotificationController.NOTIFICATION_PATH, token,
                    customizer().expectStatusOk().expectToHaveSize(JSON_PATH_CONTENT, 4)
                            .addParameter("state", NotificationStatus.UNREAD.toString()),
                    "Could not retrieve notifications");
 
-        performGet(NotificationController.NOTIFICATION_PATH,
-                   token,
+        performGet(NotificationController.NOTIFICATION_PATH, token,
                    customizer().expectStatusOk().expectToHaveSize(JSON_PATH_CONTENT, 2)
                            .addParameter("state", NotificationStatus.UNREAD.toString()).addParameter("page", "0")
                            .addParameter("size", "2"),
                    "Could not retrieve notifications");
-        Assert.assertEquals(0, 0);
+
+        performGet(NotificationController.NOTIFICATION_PATH + NotificationController.SUMMARY, token,
+                   customizer().expectStatusOk(), "Could not retrieve notification summary");
     }
 
     @Test
     public void testSetNotifRead() throws EntityNotFoundException {
         String roleName = DefaultRole.PROJECT_ADMIN.name();
-        NotificationDTO notificationDTO = new NotificationDtoBuilder("Bonne",
-                                                                     "test",
-                                                                     NotificationLevel.INFO,
-                                                                     "microservice")
-                .toRoles(new HashSet<>(Arrays.asList(roleName)));
+        NotificationDTO notificationDTO = new NotificationDtoBuilder("Bonne", "test", NotificationLevel.INFO,
+                "microservice").toRoles(new HashSet<>(Arrays.asList(roleName)));
         Notification notification = notificationService.createNotification(notificationDTO);
         performDefaultPut(NotificationController.NOTIFICATION_PATH + NotificationController.NOTIFICATION_READ_PATH,
-                          null,
-                          customizer().expectStatusOk(),
-                          "Could not set the notification to READ",
+                          null, customizer().expectStatusOk(), "Could not set the notification to READ",
                           notification.getId());
     }
 
@@ -183,19 +173,14 @@ public class NotificationControllerIT extends AbstractRegardsTransactionalIT {
     public void testSetNotifUnread() throws EntityNotFoundException {
         //create a notification
         String roleName = DefaultRole.PROJECT_ADMIN.name();
-        NotificationDTO notificationDTO = new NotificationDtoBuilder("Bonne",
-                                                                     "test",
-                                                                     NotificationLevel.INFO,
-                                                                     "microservice")
-                .toRoles(new HashSet<>(Arrays.asList(roleName)));
+        NotificationDTO notificationDTO = new NotificationDtoBuilder("Bonne", "test", NotificationLevel.INFO,
+                "microservice").toRoles(new HashSet<>(Arrays.asList(roleName)));
         Notification notification = notificationService.createNotification(notificationDTO);
         // set the notification to read
         notificationService.updateNotificationStatus(notification.getId(), NotificationStatus.READ);
         // ask to set the notification to unread
         performDefaultPut(NotificationController.NOTIFICATION_PATH + NotificationController.NOTIFICATION_UNREAD_PATH,
-                          null,
-                          customizer().expectStatusOk(),
-                          "Could not set the notification to UNREAD",
+                          null, customizer().expectStatusOk(), "Could not set the notification to UNREAD",
                           notification.getId());
     }
 
@@ -204,10 +189,10 @@ public class NotificationControllerIT extends AbstractRegardsTransactionalIT {
         performDefaultGet(NotificationController.NOTIFICATION_PATH + NotificationController.NOTIFICATION_SETTINGS,
                           customizer().expectStatusOk()
                                   .expect(MockMvcResultMatchers.jsonPath("$.id", Matchers.notNullValue(Long.class)))
-                                  .expect(MockMvcResultMatchers
-                                                  .jsonPath("$.projectUserEmail", Matchers.notNullValue(Long.class)))
-                                  .expect(MockMvcResultMatchers
-                                                  .jsonPath("$.frequency", Matchers.notNullValue(Long.class))),
+                                  .expect(MockMvcResultMatchers.jsonPath("$.projectUserEmail",
+                                                                         Matchers.notNullValue(Long.class)))
+                                  .expect(MockMvcResultMatchers.jsonPath("$.frequency",
+                                                                         Matchers.notNullValue(Long.class))),
                           "could not retrieve notification settings");
     }
 }
