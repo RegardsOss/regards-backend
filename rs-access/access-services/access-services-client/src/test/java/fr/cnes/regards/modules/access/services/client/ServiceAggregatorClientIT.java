29a0154eb984d56ba3b60d12a91f169cb717bc16
sbinda <sebastien.binda@c-s.fr>
2019-07-24 10:11:36 +0200

    [rs-access] Fix license to add 2019 year.

--- rs-access/access-services/access-services-client/src/test/java/fr/cnes/regards/modules/access/services/client/ServiceAggregatorClientIT.java
+++ rs-access/access-services/access-services-client/src/test/java/fr/cnes/regards/modules/access/services/client/ServiceAggregatorClientIT.java
@@ -3,7 +3,7 @@ package fr.cnes.regards.modules.access.services.client;
 import java.util.Arrays;
 
 /*
- * Copyright 2017-2018 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
