5327e866844ac39e90f9cfb6952e1d485cfb59af
Unknown <unknown@example.com>
2019-09-18 16:51:06 +0200

    [rs-access] add unique key for application id + set type annotation for configuration field

--- rs-access/uiconfiguration/uiconfiguration-dao/src/main/resources/scripts/uiconfiguration/V5.0.0__uiconfiguration_patch_script.sql
+++ rs-access/uiconfiguration/uiconfiguration-dao/src/main/resources/scripts/uiconfiguration/V5.0.0__uiconfiguration_patch_script.sql
@@ -1,2 +1,3 @@
 create table t_ui_configuration (id bigint, configuration text, application_id varchar(16));
+alter table t_ui_configuration add constraint uk_ui_configuration_application_id unique (application_id);
 create sequence seq_ui_configuration start 1 increment 50;
