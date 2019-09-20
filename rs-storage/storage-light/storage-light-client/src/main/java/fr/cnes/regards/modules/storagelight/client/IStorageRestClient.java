4829cc1de16ff2a297a7a09b8b63eb63de48f794
sbinda <sebastien.binda@c-s.fr>
2019-09-20 11:51:08 +0200

    [rs-storage] Add new Rest client

---
+++ rs-storage/storage-light/storage-light-client/src/main/java/fr/cnes/regards/modules/storagelight/client/IStorageRestClient.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.storagelight.client;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import feign.Response;
+import fr.cnes.regards.framework.feign.annotation.RestClient;
+
+/**
+ * REST Client to to access storage microservice
+ * @author Sébastien Binda
+ */
+@RestClient(name = "rs-storage", contextId = "rs-storage.rest.client")
+@RequestMapping(value = IStorageRestClient.FILE_PATH, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public interface IStorageRestClient {
+
+    public static final String FILE_PATH = "/files";
+
+    public static final String DOWNLOAD_PATH = "/{checksum}/download";
+
+    /**
+     * Download a file by his checksum.
+     * @param checksum file to download
+     * @return
+     */
+    @RequestMapping(path = DOWNLOAD_PATH, method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public Response downloadFile(@PathVariable("checksum") String checksum);
+
+}
