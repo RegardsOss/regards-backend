17ee1130669b57e6174799d2da8d291e966944ca
msi <marc.sordi@c-s.fr>
2019-10-02 10:52:51 +0200

    [rs-dam] WIP
    Former-commit-id: c2d022a7002643448e0de7cbfd56b6e98b108894

--- rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/entities/validation/IEntityValidationService.java
+++ rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/entities/validation/IEntityValidationService.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.cnes.regards.modules.dam.service.entities;
+package fr.cnes.regards.modules.dam.service.entities.validation;
 
 import org.springframework.validation.Errors;
 
