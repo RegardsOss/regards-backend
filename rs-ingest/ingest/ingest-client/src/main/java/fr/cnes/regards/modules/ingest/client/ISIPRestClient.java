6063894459734e1b8a97bd3bd99639fddd3d6aeb
sbinda <sebastien.binda@c-s.fr>
2019-10-25 09:40:43 +0200

    [rs-ingest] Add new REST client for SIPs

---
+++ rs-ingest/ingest/ingest-client/src/main/java/fr/cnes/regards/modules/ingest/client/ISIPRestClient.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.ingest.client;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.cnes.regards.framework.feign.annotation.RestClient;
+import fr.cnes.regards.modules.ingest.dto.request.OAISDeletionRequestDto;
+
+/**
+ * @author sbinda
+ *
+ */
+@RestClient(name = "rs-ingest", contextId = "rs-ingest.rest.client.sip")
+@RequestMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public interface ISIPRestClient {
+
+    static final String DELETE_BY_SESSION_PATH = "/sips/delete";
+
+    @RequestMapping(value = ISIPRestClient.DELETE_BY_SESSION_PATH, method = RequestMethod.DELETE)
+    ResponseEntity<OAISDeletionRequestDto> deleteBySession(OAISDeletionRequestDto request);
+
+}
