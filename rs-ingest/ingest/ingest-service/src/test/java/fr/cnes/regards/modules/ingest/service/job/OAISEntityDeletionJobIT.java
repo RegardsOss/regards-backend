ed3519c00d9dba48e64e08c799de15783a786777
sbinda <sebastien.binda@c-s.fr>
2019-11-18 14:12:50 +0100

    [rs-ingest] Fix tests.

--- rs-ingest/ingest/ingest-service/src/test/java/fr/cnes/regards/modules/ingest/service/job/OAISEntityDeletionJobIT.java
+++ rs-ingest/ingest/ingest-service/src/test/java/fr/cnes/regards/modules/ingest/service/job/OAISEntityDeletionJobIT.java
@@ -26,18 +26,22 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import com.google.common.collect.Lists;
 
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.modules.ingest.dao.AIPEntitySpecification;
 import fr.cnes.regards.modules.ingest.dao.IAIPRepository;
 import fr.cnes.regards.modules.ingest.dao.IStorageDeletionRequestRepository;
 import fr.cnes.regards.modules.ingest.domain.aip.AIPEntity;
 import fr.cnes.regards.modules.ingest.domain.aip.AIPState;
 import fr.cnes.regards.modules.ingest.domain.request.InternalRequestStep;
 import fr.cnes.regards.modules.ingest.domain.sip.SIPState;
+import fr.cnes.regards.modules.ingest.dto.aip.SearchAIPsParameters;
 import fr.cnes.regards.modules.ingest.dto.request.OAISDeletionRequestDto;
 import fr.cnes.regards.modules.ingest.dto.request.SessionDeletionMode;
 import fr.cnes.regards.modules.ingest.dto.request.SessionDeletionSelectionMode;
@@ -171,17 +175,31 @@ public class OAISEntityDeletionJobIT extends IngestMultitenantServiceTest {
         assertDeletedAIPs(3); // AIPs are deleted and not just marked deleted
     }
 
+    /**
+     * Test that a deletion error from storage client does not affect results. AIPs & SIPs should be deleted
+     * as well.
+     * @throws ModuleException
+     */
     @Test
     public void testDeletionJobFailed() throws ModuleException {
         storageClient.setBehavior(true, true);
         initData();
         storageClient.setBehavior(true, false);
 
+        Page<AIPEntity> aips = aipRepository.findAll(AIPEntitySpecification
+                .searchAll(SearchAIPsParameters.build().withSessionOwner(SESSION_OWNER_0).withSession(SESSION_0),
+                           PageRequest.of(0, 10)), PageRequest.of(0, 10));
+        Assert.assertEquals(2, aips.getContent().size());
         // 2 SIPs linked to SESSION_OWNER_0, SESSION_0 will be marked as ERROR
         ingestService.registerOAISDeletionRequest(OAISDeletionRequestDto.build(SESSION_OWNER_0, SESSION_0,
                                                                                SessionDeletionMode.IRREVOCABLY,
                                                                                SessionDeletionSelectionMode.INCLUDE));
-        waitUntilNbDeletionRequestInErrorReach(FIVE_SECONDS, 2);
+        // waitUntilNbDeletionRequestInErrorReach(FIVE_SECONDS, 2);
+        ingestServiceTest.waitAllRequestsFinished(FIVE_SECONDS * 10);
+        aips = aipRepository.findAll(AIPEntitySpecification
+                .searchAll(SearchAIPsParameters.build().withSessionOwner(SESSION_OWNER_0).withSession(SESSION_0),
+                           PageRequest.of(0, 10)), PageRequest.of(0, 10));
+        Assert.assertEquals(0, aips.getContent().size());
 
     }
 }
