c028f5d0a46339a1cdb2c082373ee9e6d95ece7c
svissier <sylvain.vissiere-guerinet@c-s.fr>
2019-11-22 14:13:19 +0100

    [rs-microservice] add filter for security logs

---
+++ rs-microservice/regards-starters/microservice-logger/src/main/java/fr/cnes/regards/framwork/logbackappender/LogConstants.java
@@ -0,0 +1,12 @@
+package fr.cnes.regards.framwork.logbackappender;
+
+/**
+ * @author Sylvain VISSIERE-GUERINET
+ */
+public final class LogConstants {
+
+    public static final String SECURITY_MARKER = "SECURITY: ";
+
+    private LogConstants() {
+    }
+}
