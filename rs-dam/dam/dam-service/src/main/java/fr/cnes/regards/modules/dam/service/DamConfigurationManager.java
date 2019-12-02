7d266bbffb15f18f6ee70d8d6fd5a16f04dcbcc1
sbinda <sebastien.binda@c-s.fr>
2019-12-02 17:13:35 +0100

    [rs-dam] Fix ms configuration import/export
    Former-commit-id: 31bb71d4cba8e9e3976509382b69408edb9af5ae

--- rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
+++ rs-dam/dam/dam-service/src/main/java/fr/cnes/regards/modules/dam/service/DamConfigurationManager.java
@@ -27,23 +27,20 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Sets;
-
+import fr.cnes.regards.framework.encryption.exception.EncryptionException;
 import fr.cnes.regards.framework.module.manager.AbstractModuleManager;
 import fr.cnes.regards.framework.module.manager.ModuleConfiguration;
 import fr.cnes.regards.framework.module.manager.ModuleConfigurationItem;
+import fr.cnes.regards.framework.module.rest.exception.EntityInvalidException;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
 import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
-import fr.cnes.regards.framework.utils.plugins.PluginUtils;
 import fr.cnes.regards.modules.dam.domain.datasources.plugins.IConnectionPlugin;
 import fr.cnes.regards.modules.dam.domain.datasources.plugins.IDataSourcePlugin;
-import fr.cnes.regards.modules.dam.service.datasources.IDBConnectionService;
-import fr.cnes.regards.modules.dam.service.datasources.IDataSourceService;
 
 /**
  * DAM configuration manager. Exports model & connection plugin configurations & datasource plugin configurations.
@@ -61,26 +58,19 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
     @Autowired
     private IPluginService pluginService;
 
-    @Autowired
-    private IDBConnectionService connectionService;
-
-    @Autowired
-    private IDataSourceService dataSourceService;
-
     @Override
     protected Set<String> importConfiguration(ModuleConfiguration configuration) {
+
         Set<String> importErrors = new HashSet<>();
-        Pair<Set<PluginConfiguration>, Set<PluginConfiguration>> confs = dispatch(getPluginConfs(configuration
-                .getConfiguration()));
+        Set<PluginConfiguration> configurations = getPluginConfs(configuration.getConfiguration());
 
         // First create connections
-        for (PluginConfiguration plgConf : confs.getLeft()) {
-            importErrors.addAll(createConnection(plgConf));
-        }
-
-        // Then create datasources
-        for (PluginConfiguration plgConf : confs.getRight()) {
-            importErrors.addAll(createDatasource(plgConf));
+        for (PluginConfiguration plgConf : configurations) {
+            try {
+                pluginService.savePluginConfiguration(plgConf);
+            } catch (EntityInvalidException | EncryptionException | EntityNotFoundException e) {
+                importErrors.add(e.getMessage());
+            }
         }
 
         return importErrors;
@@ -103,55 +93,6 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
         return ModuleConfiguration.build(info, configurations);
     }
 
-    private Set<String> createConnection(PluginConfiguration plgConf) {
-        Set<String> importErrors = new HashSet<>();
-        if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
-            importErrors.add(String.format(PLUGIN_CONFIGURATION_ALREADY_EXISTS, plgConf.getLabel()));
-        } else {
-            List<String> validationIssues = PluginUtils.validateOnCreate(plgConf);
-            if (validationIssues == null) {
-                try {
-                    // Force activation of all connections.
-                    plgConf.setIsActive(true);
-                    connectionService.createDBConnection(plgConf);
-                } catch (ModuleException e) {
-                    importErrors.add(String.format("Skipping import of Data Storage %s: %s", plgConf.getLabel(),
-                                                   e.getMessage()));
-                    logger.error(e.getMessage(), e);
-                }
-            } else {
-                importErrors.add(String.format(VALIDATION_ISSUES, plgConf.getLabel(),
-                                               validationIssues.stream().collect(Collectors.joining(",", "", "."))));
-            }
-
-        }
-        return importErrors;
-    }
-
-    private Set<String> createDatasource(PluginConfiguration plgConf) {
-        Set<String> importErrors = new HashSet<>();
-        if (pluginService.findPluginConfigurationByLabel(plgConf.getLabel()).isPresent()) {
-            importErrors.add(String.format(PLUGIN_CONFIGURATION_ALREADY_EXISTS, plgConf.getLabel()));
-        } else {
-            List<String> validationIssues = PluginUtils.validateOnCreate(plgConf);
-            if (validationIssues == null) {
-                try {
-                    dataSourceService.createDataSource(plgConf);
-                } catch (ModuleException e) {
-                    // This should not occurs, but we never know
-                    importErrors.add(String.format("Skipping import of PluginConfiguration %s: %s", plgConf.getLabel(),
-                                                   e.getMessage()));
-                    logger.error(e.getMessage(), e);
-                }
-            } else {
-                importErrors.add(String.format(VALIDATION_ISSUES, plgConf.getLabel(),
-                                               validationIssues.stream().collect(Collectors.joining(",", "", "."))));
-            }
-
-        }
-        return importErrors;
-    }
-
     /**
      * Get all {@link PluginConfiguration}s of the {@link ModuleConfigurationItem}s
      * @param items {@link ModuleConfigurationItem}s
@@ -160,24 +101,5 @@ public class DamConfigurationManager extends AbstractModuleManager<Void> {
     private Set<PluginConfiguration> getPluginConfs(Collection<ModuleConfigurationItem<?>> items) {
         return items.stream().filter(i -> PluginConfiguration.class.isAssignableFrom(i.getKey()))
                 .map(i -> (PluginConfiguration) i.getTypedValue()).collect(Collectors.toSet());
-    }
-
-    /**
-     * Dispatch given  {@link PluginConfiguration}s into two {@link Set}s.
-     * Left ones are {@link IConnectionPlugin}s and right ones are {@link IDataSourcePlugin}s
-     * @param confs
-     * @return {@link Pair} of {@link IConnectionPlugin}s/{@link IDataSourcePlugin}s
-     */
-    private Pair<Set<PluginConfiguration>, Set<PluginConfiguration>> dispatch(Collection<PluginConfiguration> confs) {
-        Set<PluginConfiguration> connections = Sets.newHashSet();
-        Set<PluginConfiguration> datasources = Sets.newHashSet();
-        confs.forEach(c -> {
-            if (c.getInterfaceNames().contains(IConnectionPlugin.class.getName())) {
-                connections.add(c);
-            } else if (c.getInterfaceNames().contains(IDataSourcePlugin.class.getName())) {
-                datasources.add(c);
-            }
-        });
-        return Pair.of(connections, datasources);
     }
 }
