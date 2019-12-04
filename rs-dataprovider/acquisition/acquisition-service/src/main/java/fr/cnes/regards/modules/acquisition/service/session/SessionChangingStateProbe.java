427f236945140018cedfe4f75c08ba1ca7b2bfee
sbinda <sebastien.binda@c-s.fr>
2019-12-04 12:15:05 +0100

    [rs-dataprovider] Fix session notifications to handle product deletion & product session changed.
    Former-commit-id: 11b17941eaed54a70eeda8dc75182b3a165bd516

--- rs-dataprovider/acquisition/acquisition-service/src/main/java/fr/cnes/regards/modules/acquisition/service/session/SessionChangingStateProbe.java
+++ rs-dataprovider/acquisition/acquisition-service/src/main/java/fr/cnes/regards/modules/acquisition/service/session/SessionChangingStateProbe.java
@@ -1,20 +1,29 @@
 package fr.cnes.regards.modules.acquisition.service.session;
 
 import com.google.common.base.Strings;
+
 import fr.cnes.regards.modules.acquisition.domain.Product;
 import fr.cnes.regards.modules.acquisition.domain.ProductState;
 import fr.cnes.regards.modules.ingest.domain.sip.ISipState;
 
 public class SessionChangingStateProbe {
+
     // Gathers info about initial product
     private String initialSession;
+
     private ProductState initialProductState;
+
     private ISipState initialProductSIPState;
+
+    private long initalNbAcquiredFiles = 0L;
 
     // Gathers info about updated product
     private String ingestionChain;
+
     private String session;
+
     private ProductState productState;
+
     private ISipState productSIPState;
 
     public void addUpdatedProduct(Product updatedProduct) {
@@ -30,6 +39,7 @@ public class SessionChangingStateProbe {
             sessionChangingStateProbe.initialSession = initialProduct.getSession();
             sessionChangingStateProbe.initialProductState = initialProduct.getState();
             sessionChangingStateProbe.initialProductSIPState = initialProduct.getSipState();
+            sessionChangingStateProbe.setInitalNbAcquiredFiles(initialProduct.getAcquisitionFiles().size());
         }
         return sessionChangingStateProbe;
     }
@@ -39,11 +49,9 @@ public class SessionChangingStateProbe {
     }
 
     public boolean shouldUpdateState() {
-        return getInitialProductState() != getProductState() &&
-                !(
-                        // Ignore FINISHED -> COMPLETED state change
-                        getProductState() == ProductState.FINISHED && getInitialProductState() == ProductState.COMPLETED
-                );
+        return isSessionChanged() || ((getInitialProductState() != getProductState()) && !(
+        // Ignore FINISHED -> COMPLETED state change
+        (getProductState() == ProductState.FINISHED) && (getInitialProductState() == ProductState.COMPLETED)));
     }
 
     public String getInitialSession() {
@@ -72,5 +80,13 @@ public class SessionChangingStateProbe {
 
     public ISipState getProductSIPState() {
         return productSIPState;
+    }
+
+    public long getInitalNbAcquiredFiles() {
+        return initalNbAcquiredFiles;
+    }
+
+    public void setInitalNbAcquiredFiles(long initalNbAcquiredFiles) {
+        this.initalNbAcquiredFiles = initalNbAcquiredFiles;
     }
 }
