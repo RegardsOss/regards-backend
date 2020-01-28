cab23a50a29a420af4c84525be391657afdf3bc8
sbinda <sebastien.binda@c-s.fr>
2020-01-28 11:36:31 +0100

    [rs-storage] Fix tests

--- rs-storage/storage/storage-rest/src/test/java/fr/cnes/regards/modules/storage/rest/FileReferenceControllerIT.java
+++ rs-storage/storage/storage-rest/src/test/java/fr/cnes/regards/modules/storage/rest/FileReferenceControllerIT.java
@@ -53,6 +53,7 @@ import fr.cnes.regards.modules.storage.dao.IGroupRequestInfoRepository;
 import fr.cnes.regards.modules.storage.domain.database.FileReferenceMetaInfo;
 import fr.cnes.regards.modules.storage.domain.plugin.StorageType;
 import fr.cnes.regards.modules.storage.rest.plugin.SimpleOnlineDataStorage;
+import fr.cnes.regards.modules.storage.service.file.FileReferenceService;
 import fr.cnes.regards.modules.storage.service.file.request.FileStorageRequestService;
 import fr.cnes.regards.modules.storage.service.location.StorageLocationConfigurationService;
 import fr.cnes.regards.modules.storage.service.location.StoragePluginConfigurationHandler;
@@ -70,6 +71,9 @@ public class FileReferenceControllerIT extends AbstractRegardsTransactionalIT {
 
     @Autowired
     private FileStorageRequestService storeReqService;
+
+    @Autowired
+    private FileReferenceService fileRefService;
 
     @Autowired
     private StorageLocationConfigurationService prioritizedDataStorageService;
@@ -121,7 +125,7 @@ public class FileReferenceControllerIT extends AbstractRegardsTransactionalIT {
         boolean found = false;
         int loops = 100;
         do {
-            found = !fileRepo.findByMetaInfoChecksum(metaInfo.getChecksum()).isEmpty();
+            found = fileRefService.search(TARGET_STORAGE, metaInfo.getChecksum()).isPresent();
             Thread.sleep(1_000);
             loops--;
         } while (!found && (loops > 0));
