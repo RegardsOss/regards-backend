70ae57d483e82119b4d7247fdec661c36196f9d5
sbinda <sebastien.binda@c-s.fr>
2020-02-04 15:48:51 +0100

    [rs-dam] Add equals method on QueryableAttribute pojo.
    Former-commit-id: 013ded488c4d9445af16d4649de4729f4d6ff564

--- rs-dam/indexer/indexer-domain/src/main/java/fr/cnes/regards/modules/indexer/domain/aggregation/QueryableAttribute.java
+++ rs-dam/indexer/indexer-domain/src/main/java/fr/cnes/regards/modules/indexer/domain/aggregation/QueryableAttribute.java
@@ -91,4 +91,34 @@ public class QueryableAttribute {
         this.termsLimit = termsLimit;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((attributeName == null) ? 0 : attributeName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        QueryableAttribute other = (QueryableAttribute) obj;
+        if (attributeName == null) {
+            if (other.attributeName != null) {
+                return false;
+            }
+        } else if (!attributeName.equals(other.attributeName)) {
+            return false;
+        }
+        return true;
+    }
+
 }
