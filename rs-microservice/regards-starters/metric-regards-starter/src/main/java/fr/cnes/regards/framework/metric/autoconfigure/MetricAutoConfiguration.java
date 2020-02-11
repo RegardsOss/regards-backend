a594e1dbb60943c91d912197b74f702c766b4ba8
Marc SORDI <marc.sordi@c-s.fr>
2020-02-11 18:24:54 +0100

    [rs-microservice] enable registry configuration

--- rs-microservice/regards-starters/metric-regards-starter/src/main/java/fr/cnes/regards/framework/metric/autoconfigure/MetricAutoConfiguration.java
+++ rs-microservice/regards-starters/metric-regards-starter/src/main/java/fr/cnes/regards/framework/metric/autoconfigure/MetricAutoConfiguration.java
@@ -1,30 +1,50 @@
-/**
+/*
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.cnes.regards.framework.metric.autoconfigure;
 
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 
 /**
  * @author kevin
- *
+ * @author Marc SORDI
  */
 @Configuration
+@EnableConfigurationProperties(LoggingRegistryConfiguration.class)
+@AutoConfigureBefore(CompositeMeterRegistryAutoConfiguration.class)
 public class MetricAutoConfiguration {
 
     @Bean
-    TimedAspect timedAspect(MeterRegistry registry) {
+    public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
     }
 
     @Bean
-    LoggingMeterRegistry loggingMeterRegistry() {
-        return new LoggingMeterRegistry();
-
+    public LoggingMeterRegistry loggingMeterRegistry(LoggingRegistryConfiguration configuration) {
+        return new LoggingMeterRegistry(configuration, Clock.SYSTEM);
     }
 }
