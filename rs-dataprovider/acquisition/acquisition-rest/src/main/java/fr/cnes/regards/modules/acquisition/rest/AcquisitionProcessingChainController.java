4257e4f854f7d26d194c80125edfe81337630921
sbinda <sebastien.binda@c-s.fr>
2020-03-09 09:17:02 +0100

    [rs-dataprovider] Update licence to 2020
    Former-commit-id: 75ccd0726d102519d6911a7934ffcc3c3339a577

--- rs-dataprovider/acquisition/acquisition-rest/src/main/java/fr/cnes/regards/modules/acquisition/rest/AcquisitionProcessingChainController.java
+++ rs-dataprovider/acquisition/acquisition-rest/src/main/java/fr/cnes/regards/modules/acquisition/rest/AcquisitionProcessingChainController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2020 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
@@ -24,7 +24,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 /*
- * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2020 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
