3601db972d740cf830aea6ee28aacf9a90dc41bf
sbinda <sebastien.binda@c-s.fr>
2020-03-09 09:29:56 +0100

    [rs-microservice] Update licence to 2020

--- rs-microservice/regards-starters/gson-regards-starter/src/test/java/fr/cnes/regards/framework/gson/autoconfigure/GsonAutoConfigurationTest.java
+++ rs-microservice/regards-starters/gson-regards-starter/src/test/java/fr/cnes/regards/framework/gson/autoconfigure/GsonAutoConfigurationTest.java
@@ -1,6 +1,6 @@
 package fr.cnes.regards.framework.gson.autoconfigure;
 /*
- * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2020 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
