77fb4b154a3407926f37e651187abbc6066e0764
sbinda <sebastien.binda@c-s.fr>
2020-03-11 09:09:39 +0100

    [rs-access] Create new initial db schema creation for version 1.0.0.

--- rs-access/uiconfiguration/uiconfiguration-dao/src/main/resources/scripts/uiconfiguration/V2.0.2__uiconfiguration_patch_script.sql
+++
@@ -1,1 +0,0 @@
-alter table t_ui_module alter column title type text;
--- rs-access/uiconfiguration/uiconfiguration-dao/src/main/resources/scripts/uiconfiguration/V5.0.0__uiconfiguration_patch_script.sql
+++
@@ -1,2 +0,0 @@
-create table t_ui_configuration (id bigint, configuration text, application_id varchar(16));
-create sequence seq_ui_configuration start 1 increment 50;
