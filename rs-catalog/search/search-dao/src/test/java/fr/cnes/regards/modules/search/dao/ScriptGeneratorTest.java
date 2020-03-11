7de935d7cd96c074457664c1b95a16ab559ea4f6
sbinda <sebastien.binda@c-s.fr>
2020-03-11 10:14:33 +0100

    [rs-catalog] Create new initial db schema creation for version 1.0.0.

--- rs-catalog/search/search-dao/src/test/java/fr/cnes/regards/modules/search/dao/ScriptGeneratorTest.java
+++ rs-catalog/search/search-dao/src/test/java/fr/cnes/regards/modules/search/dao/ScriptGeneratorTest.java
@@ -18,14 +18,13 @@
  */
 package fr.cnes.regards.modules.search.dao;
 
-import org.junit.Ignore;
-
 import fr.cnes.regards.framework.jpa.multitenant.test.AbstractScriptGeneratorTest;
 
 /**
  * @author Sébastien Binda
  */
-@Ignore
+// @TestPropertySource(properties = { "regards.jpa.multitenant.migrationTool=FLYWAYDB" })
+//@Ignore
 public class ScriptGeneratorTest extends AbstractScriptGeneratorTest {
 
 }
