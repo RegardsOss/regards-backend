b5d0316aaa8e51085e211c65f341172488b88ba0
svissier <sylvain.vissiere-guerinet@c-s.fr>
2020-03-27 16:28:32 +0100

    [rs-cloud] configure openapi-ui to use eureka to discover endpoints

--- rs-cloud/bootstrap-openapi-ui/src/main/java/fr/cnes/regards/microservices/openapi/Application.java
+++ rs-cloud/bootstrap-openapi-ui/src/main/java/fr/cnes/regards/microservices/openapi/Application.java
@@ -18,6 +18,8 @@
 
 package fr.cnes.regards.microservices.openapi;
 
+import java.util.Arrays;
+
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -25,51 +27,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.Scopes;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "org.springdoc.demo.app2" })
+@ComponentScan(basePackages = { "fr.cnes.regards.microservices.openapi.web" })
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
-    @Bean
-    public GroupedOpenApi userOpenApi() {
-        String[] paths = { "/user/**" };
-        String[] packagedToMatch = { "org.springdoc.demo.app2" };
-        return GroupedOpenApi.builder().setGroup("users").pathsToMatch(paths).packagesToScan(packagedToMatch).build();
-    }
+        @Bean
+        public GroupedOpenApi userOpenApi() {
+            String[] paths = { "/**" };
+    //        String[] packagedToMatch = { "fr.cnes.regards" };
+    //        return GroupedOpenApi.builder().setGroup("adminInstance").pathsToMatch(paths).packagesToScan(packagedToMatch).build();
+            return GroupedOpenApi.builder().setGroup("adminInstance").pathsToMatch(paths).build();
 
-    @Bean
-    public GroupedOpenApi storeOpenApi() {
-        String[] paths = { "/store/**" };
-        return GroupedOpenApi.builder().setGroup("stores").pathsToMatch(paths).build();
-    }
+        }
 
-    // FIXME configure OAuth parameters dynamically
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-        return new OpenAPI().externalDocs(new ExternalDocumentation().url("rs-admin/v3/api-docs?scope=public"))
-                .components(new Components()
-                        .addSecuritySchemes("REGARDS", new SecurityScheme().type(SecurityScheme.Type.OAUTH2)
-                                .flows(new OAuthFlows().password(new OAuthFlow()
-                                        //                                .authorizationUrl("http://172.26.47.176:9030/api/v1/rs-authentication/oauth/token")
-                                        // .tokenUrl("http://172.26.47.176:9030/api/v1/rs-authentication/oauth/token")
-                                        .tokenUrl("http://localhost:9030/api/v1/rs-authentication/oauth/token")
-                                        .scopes(new Scopes().addString("project1", "target project"))))))
-                .info(new Info().title("REGARDS API").version(appVersion).description("TODO Description")
-                        // .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("GPLV3").url("https://github.com/regardsOss")));
-    }
+        // FIXME configure OAuth parameters dynamically
+        @Bean
+        public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+            Server adminInstanceServer = new Server();
+            adminInstanceServer.setUrl("http://127.0.0.1/api/v1/rs-admin-instance/v2/api-docs");
+            adminInstanceServer.setDescription("Admin Instance OpenAPI");
+            return new OpenAPI().servers(Arrays.asList(adminInstanceServer));
+    //        .externalDocs(new ExternalDocumentation().url("http://127.0.0.1/api/v1/rs-admin-instance/v2/api-docs?scope=public"))
+    //                .components(new Components()
+    ////                        .addSecuritySchemes("REGARDS", new SecurityScheme().type(SecurityScheme.Type.OAUTH2)
+    ////                                .flows(new OAuthFlows().password(new OAuthFlow()
+    ////                                        //                                .authorizationUrl("http://172.26.47.176:9030/api/v1/rs-authentication/oauth/token")
+    ////                                        // .tokenUrl("http://172.26.47.176:9030/api/v1/rs-authentication/oauth/token")
+    ////                                        .tokenUrl("http://localhost:9030/api/v1/rs-authentication/oauth/token")
+    ////                                        .scopes(new Scopes().addString("project1", "target project")))))
+    //                                     )
+    //                .info(new Info().title("REGARDS API").version(appVersion).description("TODO Description")
+    //                        // .termsOfService("http://swagger.io/terms/")
+    //                        .license(new License().name("GPLV3").url("https://github.com/regardsOss")));
+        }
 
 }
