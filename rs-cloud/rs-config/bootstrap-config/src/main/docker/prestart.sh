45833ece0b97929e87c91af347e88d4f0c74fec7
Marc SORDI <marc.sordi@c-s.fr>
2020-03-30 16:56:19 +0200

    [rs-cloud] WIP

--- rs-cloud/rs-config/bootstrap-config/src/main/docker/prestart.sh
+++ rs-cloud/rs-config/bootstrap-config/src/main/docker/prestart.sh
@@ -1,7 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
-jar -xvf /@project.build.finalName@.@project.packaging@ BOOT-INF/classes/regards/application.properties
-sed -i s/\$\{regards\.registry\.host}/"$REGISTRY_URL"/g BOOT-INF/classes/regards/application.properties
-cat BOOT-INF/classes/regards/application.properties
-jar -uvf /@project.build.finalName@.@project.packaging@ BOOT-INF/classes/regards/application.properties
-sed -i s/\$\{regards\.registry\.host}/"$REGISTRY_URL"/g /config/regards/application.properties
+declare -r TEMPLATE_HOME="/config-templates"
+declare -r CONFIG_HOME="/config/regards"
+
+declare -r CONFIG_HOME /home/marc/git/rs-cloud/rs-config/bootstrap-config/src/main/docker/tmp
+
+# Check config directory
+if [ ! -d "${CONFIG_HOME}" ]; then
+    echo "Config home must exist! Add a bind mount pointing to ${CONFIG_HOME}!"
+    exit 1
+fi
+
+# Check config directory is empty
+# IF NOT, continue without changing config
+if [ "$(ls -A "${CONFIG_HOME}")" ]; then 
+    echo "Configuration directory ${CONFIG_HOME} not empty. Continuing without copying properties and replacing theirs variables."
+    exit 0
+fi
+
+# Check config directory
+if [ ! -d "${TEMPLATE_HOME}" ]; then
+    echo "Template home must exist! Add a bind mount pointing to ${TEMPLATE_HOME}!" 
+    exit 1
+fi
+
+# Copy templates into target config directory
+cp -R "${TEMPLATE_HOME}/." "${CONFIG_HOME}"
+
+# Resolve placeholders related to env vars
+for env_var in $(env | grep "^REGARDS.*=")
+do
+    regards_var="@@${env_var%%=*}@@" 
+    regards_val=${env_var#*=}
+    # printf >&2 "${regards_var}\t->\t${regards_val}\n"
+    if [[ ${regards_val} == *"#"* ]]; then
+        warning "Environment variable ${regards_var} contains a # which is a reserved character. Skipping replacement."
+        # Indeed, hastag is used in sed clause below!
+    else 
+        find "${CONFIG_HOME}" -type f -name "*.properties" -exec sed -i "s#${regards_var}#${regards_val}#g" {} \;
+    fi
+done
