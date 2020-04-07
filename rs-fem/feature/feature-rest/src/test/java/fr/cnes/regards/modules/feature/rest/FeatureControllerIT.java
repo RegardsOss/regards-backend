fb4e4075870643e0c831715125cf4de635186202
sbinda <sebastien.binda@c-s.fr>
2020-04-07 10:37:07 +0200

    [rs-fem] Add FeatureEntityControler tests

--- rs-fem/feature/feature-rest/src/test/java/fr/cnes/regards/modules/feature/rest/FeatureControllerIT.java
+++ rs-fem/feature/feature-rest/src/test/java/fr/cnes/regards/modules/feature/rest/FeatureControllerIT.java
@@ -1,8 +1,5 @@
 package fr.cnes.regards.modules.feature.rest;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,13 +7,9 @@ import java.util.UUID;
 
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.restdocs.snippet.Attributes;
@@ -24,22 +17,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.util.MimeType;
 import org.springframework.validation.MapBindingResult;
 
 import fr.cnes.regards.framework.geojson.GeoJsonMediaType;
 import fr.cnes.regards.framework.geojson.geometry.IGeometry;
 import fr.cnes.regards.framework.jpa.multitenant.test.AbstractMultitenantServiceTest;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
-import fr.cnes.regards.framework.test.integration.AbstractRegardsTransactionalIT;
 import fr.cnes.regards.framework.test.integration.ConstrainedFields;
 import fr.cnes.regards.framework.test.integration.RequestBuilderCustomizer;
-import fr.cnes.regards.framework.urn.DataType;
 import fr.cnes.regards.framework.urn.EntityType;
 import fr.cnes.regards.modules.feature.dto.Feature;
-import fr.cnes.regards.modules.feature.dto.FeatureFile;
-import fr.cnes.regards.modules.feature.dto.FeatureFileAttributes;
-import fr.cnes.regards.modules.feature.dto.FeatureFileLocation;
 import fr.cnes.regards.modules.feature.dto.FeatureMetadata;
 import fr.cnes.regards.modules.feature.dto.FeatureSessionMetadata;
 import fr.cnes.regards.modules.feature.dto.FeatureUpdateCollection;
@@ -48,25 +35,14 @@ import fr.cnes.regards.modules.feature.dto.StorageMetadata;
 import fr.cnes.regards.modules.feature.dto.urn.FeatureIdentifier;
 import fr.cnes.regards.modules.feature.dto.urn.FeatureUniformResourceName;
 import fr.cnes.regards.modules.feature.service.IFeatureValidationService;
-import fr.cnes.regards.modules.model.client.IModelAttrAssocClient;
 import fr.cnes.regards.modules.model.client.IModelClient;
-import fr.cnes.regards.modules.model.domain.Model;
-import fr.cnes.regards.modules.model.domain.ModelAttrAssoc;
-import fr.cnes.regards.modules.model.domain.attributes.AttributeModel;
-import fr.cnes.regards.modules.model.dto.properties.IProperty;
-import fr.cnes.regards.modules.model.gson.MultitenantFlattenedAttributeAdapterFactory;
-import fr.cnes.regards.modules.model.service.exception.ImportException;
-import fr.cnes.regards.modules.model.service.xml.IComputationPluginService;
-import fr.cnes.regards.modules.model.service.xml.XmlImportHelper;
 
 @TestPropertySource(properties = { "spring.jpa.properties.hibernate.default_schema=feature",
         "regards.amqp.enabled=true", "spring.jpa.properties.hibernate.jdbc.batch_size=1024",
         "spring.jpa.properties.hibernate.order_inserts=true" })
 @ActiveProfiles(value = { "testAmqp", "noscheduler" })
 @ContextConfiguration(classes = { AbstractMultitenantServiceTest.ScanningConfiguration.class })
-public class FeatureControllerIT extends AbstractRegardsTransactionalIT {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureControllerIT.class);
+public class FeatureControllerIT extends AbstractFeatureIT {
 
     @Autowired
     private IFeatureValidationService validationMock;
@@ -75,65 +51,11 @@ public class FeatureControllerIT extends AbstractRegardsTransactionalIT {
     private IRuntimeTenantResolver runtimeTenantResolver;
 
     @Autowired
-    private IComputationPluginService cps;
-
-    @Autowired
     protected IModelClient modelClientMock;
-
-    @Autowired
-    private MultitenantFlattenedAttributeAdapterFactory factory;
-
-    @Autowired
-    private IModelAttrAssocClient modelAttrAssocClientMock;
 
     private static final String FEATURE_CREATION_REQUEST_ERROR = "Something goes wrong during FeatureCreationRequest creation";
 
     private static final String FEATURE_UPDATE_REQUEST_ERROR = "Something goes wrong during FeatureUpdateRequest creation";
-
-    /**
-     * Mock model client importing model specified by its filename
-     * @param filename model filename found using {@link Class#getResourceAsStream(String)}
-     * @return mocked model name
-     */
-    public String mockModelClient(String filename, IComputationPluginService cps,
-            MultitenantFlattenedAttributeAdapterFactory factory, String tenant,
-            IModelAttrAssocClient modelAttrAssocClientMock) {
-
-        try (InputStream input = FeatureControllerIT.class.getResourceAsStream(filename)) {
-            // Import model
-            Iterable<ModelAttrAssoc> assocs = XmlImportHelper.importModel(input, cps);
-
-            // Translate to resources and attribute models and extract model name
-            String modelName = null;
-            List<AttributeModel> atts = new ArrayList<>();
-            List<EntityModel<ModelAttrAssoc>> resources = new ArrayList<>();
-            for (ModelAttrAssoc assoc : assocs) {
-                atts.add(assoc.getAttribute());
-                resources.add(new EntityModel<ModelAttrAssoc>(assoc));
-                if (modelName == null) {
-                    modelName = assoc.getModel().getName();
-                }
-            }
-
-            // Property factory registration
-            factory.registerAttributes(tenant, atts);
-
-            // Mock client
-            List<EntityModel<Model>> models = new ArrayList<EntityModel<Model>>();
-            Model mockModel = Mockito.mock(Model.class);
-            Mockito.when(mockModel.getName()).thenReturn(modelName);
-            models.add(new EntityModel<Model>(mockModel));
-            Mockito.when(modelClientMock.getModels(null)).thenReturn(ResponseEntity.ok(models));
-            Mockito.when(modelAttrAssocClientMock.getModelAttrAssocs(modelName))
-                    .thenReturn(ResponseEntity.ok(resources));
-
-            return modelName;
-        } catch (IOException | ImportException e) {
-            String errorMessage = "Cannot import model";
-            LOGGER.debug(errorMessage);
-            throw new AssertionError(errorMessage);
-        }
-    }
 
     @Test
     public void testCreateValidFeatureCreationRequest() throws Exception {
@@ -204,34 +126,6 @@ public class FeatureControllerIT extends AbstractRegardsTransactionalIT {
 
         performDefaultPatch(FeatureController.PATH_FEATURES, collection, requestBuilderCustomizer,
                             FEATURE_UPDATE_REQUEST_ERROR).andDo(MockMvcResultHandlers.print());
-    }
-
-    private Feature initValidFeature() {
-        String model = mockModelClient("feature_model_01.xml", cps, factory, this.getDefaultTenant(),
-                                       modelAttrAssocClientMock);
-
-        Feature feature = Feature.build("MyId", null, IGeometry.point(IGeometry.position(10.0, 20.0)), EntityType.DATA,
-                                        model);
-        feature.addProperty(IProperty.buildString("data_type", "TYPE01"));
-        feature.addProperty(IProperty.buildObject("file_characterization", IProperty.buildBoolean("valid", true)));
-        feature.withFiles(FeatureFile.build(FeatureFileAttributes
-                .build(DataType.RAWDATA, MimeType.valueOf("application/xml"), "filename", 100l, "MD5", "checksum"),
-                                            FeatureFileLocation.build("http://www.test.com/filename.xml")));
-
-        return feature;
-    }
-
-    private Feature initValidUpdateFeature() {
-        String model = mockModelClient("feature_model_01.xml", cps, factory, this.getDefaultTenant(),
-                                       modelAttrAssocClientMock);
-
-        Feature feature = Feature.build("MyId",
-                                        FeatureUniformResourceName.build(FeatureIdentifier.FEATURE, EntityType.DATA,
-                                                                         "tenant", UUID.randomUUID(), 1),
-                                        null, EntityType.DATA, model);
-        feature.addProperty(IProperty.buildObject("file_characterization", IProperty.buildBoolean("valid", false),
-                                                  IProperty.buildDate("invalidation_date", OffsetDateTime.now())));
-        return feature;
     }
 
     @Test
