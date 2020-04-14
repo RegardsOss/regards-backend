37e6976bdcf9f2bb8343039949e09f0f1d511621
Marc SORDI <marc.sordi@c-s.fr>
2020-04-14 17:06:49 +0200

    [rs-fem] fix bad result due to error cardinality change

--- rs-fem/feature/feature-service/src/test/java/fr/cnes/regards/modules/feature/service/FeatureCreationIT.java
+++ rs-fem/feature/feature-service/src/test/java/fr/cnes/regards/modules/feature/service/FeatureCreationIT.java
@@ -223,7 +223,7 @@ public class FeatureCreationIT extends AbstractFeatureMultitenantServiceTest {
         RequestInfo<String> infos = this.featureCreationService.registerRequests(collection);
 
         assertEquals(0, infos.getGranted().size());
-        assertEquals(properties.getMaxBulkSize().intValue(), infos.getDenied().size());
+        assertEquals(properties.getMaxBulkSize().intValue(), infos.getDenied().keySet().size());
     }
 
     /**
