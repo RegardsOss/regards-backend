8b4d760235d5ac642d58641bb75305ecd3d9f7db
Kevin Marchois <kevin.marchois.ext@c-s.fr>
2020-04-20 15:36:53 +0200

    [rs-notifier] fix fail test plugin
    Former-commit-id: 4c13205f9fac02f4ee9672eab4a2a0758b914cf7

--- rs-notifier/notifier/notifier-service/src/main/java/fr/cnes/regards/modules/notifier/service/plugin/RecipientSenderTestFail.java
+++ rs-notifier/notifier/notifier-service/src/main/java/fr/cnes/regards/modules/notifier/service/plugin/RecipientSenderTestFail.java
@@ -18,11 +18,19 @@
  */
 package fr.cnes.regards.modules.notifier.service.plugin;
 
+import java.util.HashMap;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.google.gson.JsonElement;
 
+import fr.cnes.regards.framework.amqp.IPublisher;
 import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
+import fr.cnes.regards.framework.modules.plugins.annotations.PluginParameter;
+import fr.cnes.regards.modules.notifier.dto.out.NotificationEvent;
+import fr.cnes.regards.modules.notifier.plugin.IRecipientNotifier;
 
 /**
  * Fail sender to test notification resending after an error occured during the first tie
@@ -31,18 +39,29 @@ import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
  */
 @Plugin(author = "REGARDS Team", description = "Fail recipient sender", id = "TestSendFail", version = "1.0.0",
         contact = "regards@c-s.fr", license = "GPLv3", owner = "CNES", url = "https://regardsoss.github.io/")
-public class RecipientSenderTestFail extends RabbitMQSender {
+public class RecipientSenderTestFail implements IRecipientNotifier {
 
     // if if fail = true the send will deliberaly fail
     @Value("${notifier.test.fail:true}")
     private boolean isFail;
+
+    @PluginParameter(label = "RabbitMQ exchange name", name = "exchange")
+    private String exchange;
+
+    @PluginParameter(label = "RabbitMQ queue name", name = "queueName", optional = true)
+    private String queueName;
+
+    @Autowired
+    private IPublisher publisher;
 
     @Override
     public boolean send(JsonElement element, String action) {
         if (isFail) {
             return false;
         }
-        return super.send(element, action);
+        this.publisher.broadcast(exchange, Optional.ofNullable(queueName), 0, NotificationEvent.build(element, action),
+                                 new HashMap<>());
+        return true;
     }
 
 }
