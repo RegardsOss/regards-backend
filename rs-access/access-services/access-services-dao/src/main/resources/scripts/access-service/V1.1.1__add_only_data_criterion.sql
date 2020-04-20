fe1260d131c7b1a6b219e1c52eac14046e3d6c4a
RaphaelMechali <raphael.mechali@gmail.com>
2020-04-20 16:20:21 +0200

    [rs-access] With ID generator correction

--- rs-access/access-services/access-services-dao/src/main/resources/scripts/access-service/V1.1.1__add_only_data_criterion.sql
+++ rs-access/access-services/access-services-dao/src/main/resources/scripts/access-service/V1.1.1__add_only_data_criterion.sql
@@ -1,2 +1,2 @@
 insert into t_ui_plugin (id,name,sourcepath,type)
-VALUES(DEFAULT,'data-with-picture-only','/plugins/criterion/data-with-picture-only/plugin.js','CRITERIA');
+VALUES(nextval('seq_ui_plugin'),'data-with-picture-only','/plugins/criterion/data-with-picture-only/plugin.js','CRITERIA');
