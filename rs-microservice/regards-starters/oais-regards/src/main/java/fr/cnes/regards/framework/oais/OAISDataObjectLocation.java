dc49af43a58b660f2d6f84ecf1b65553e9d82ee5
svissier <sylvain.vissiere-guerinet@c-s.fr>
2020-04-30 17:28:19 +0200

    [rs-microservice] add pattern validation on urns

--- rs-microservice/regards-starters/oais-regards/src/main/java/fr/cnes/regards/framework/oais/OAISDataObjectLocation.java
+++ rs-microservice/regards-starters/oais-regards/src/main/java/fr/cnes/regards/framework/oais/OAISDataObjectLocation.java
@@ -22,6 +22,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Path;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,6 +50,7 @@ public class OAISDataObjectLocation {
      * URL to access the file
      */
     @NotNull(message = URL_REQUIRED)
+    @Pattern(regexp = "\\b[a-zA-Z]+://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
     private String url;
 
     /**
