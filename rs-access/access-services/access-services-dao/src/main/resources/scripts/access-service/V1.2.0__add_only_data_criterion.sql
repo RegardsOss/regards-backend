4d02a63d910a06e51aaab5c706691eea209175f5
RaphaelMechali <raphael.mechali@gmail.com>
2020-05-20 11:56:29 +0200

    [rs-access] Added role_name column (appeared in 1.1.0, work was made in 1.0.0)

--- rs-access/access-services/access-services-dao/src/main/resources/scripts/access-service/V1.2.0__add_only_data_criterion.sql
+++ rs-access/access-services/access-services-dao/src/main/resources/scripts/access-service/V1.2.0__add_only_data_criterion.sql
@@ -1,2 +1,2 @@
-insert into t_ui_plugin (id,name,sourcepath,type)
-VALUES(nextval('seq_ui_plugin'),'data-with-picture-only','/plugins/criterion/data-with-picture-only/plugin.js','CRITERIA');
+insert into t_ui_plugin (id,name,sourcepath,type, role_name)
+VALUES(nextval('seq_ui_plugin'),'data-with-picture-only','/plugins/criterion/data-with-picture-only/plugin.js','CRITERIA', 'PUBLIC');
