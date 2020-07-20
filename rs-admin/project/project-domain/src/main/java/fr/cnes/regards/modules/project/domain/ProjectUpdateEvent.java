c8b45690f2272bb536034a32804e831b809258b6
sbinda <sebastien.binda@c-s.fr>
2020-07-20 10:27:52 +0200

    [rs-admin] Fix new ProjectUpdateEvent

--- rs-admin/project/project-domain/src/main/java/fr/cnes/regards/modules/project/domain/ProjectUpdateEvent.java
+++ rs-admin/project/project-domain/src/main/java/fr/cnes/regards/modules/project/domain/ProjectUpdateEvent.java
@@ -18,13 +18,16 @@
  */
 package fr.cnes.regards.modules.project.domain;
 
+import fr.cnes.regards.framework.amqp.event.Event;
 import fr.cnes.regards.framework.amqp.event.ISubscribable;
+import fr.cnes.regards.framework.amqp.event.Target;
 
 /**
  *
  * @author Sébastien Binda
  *
  */
+@Event(target = Target.ALL)
 public class ProjectUpdateEvent implements ISubscribable {
 
     Project project;
