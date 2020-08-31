44db0b76308a2f2aeadf6777f0aa6cdca685a018
Sylvain VISSIERE-GUERINET <sylvain.vissiere-guerinet@telecomnancy.net>
2020-08-31 13:48:17 +0200

    [rs-ingest] fix sql migration script

--- rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.3.0__add_version.sql
+++ rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.3.0__add_version.sql
@@ -6,6 +6,6 @@ ALTER TABLE t_sip ALTER COLUMN last SET  NOT NULL;
 
 -- now lets handle versioning on AIPs
 ALTER TABLE t_aip ADD COLUMN last bool;
-UPDATE t_aip a1 SET last = true WHERE a1.id = (SELECT a2.id FROM t_sip a2 WHERE a2.provider_id = a1.provider_id AND a2.version = (SELECT max(a3.version) FROM t_sip a3 WHERE a3.provider_id = a1.provider_id AND a3.state <> 'DELETED') );
-UPDATE t_aip a1 SET last = false WHERE a1.id IN (SELECT a2.id FROM t_sip a2 WHERE a2.provider_id = a1.provider_id AND a2.version <> (SELECT max(a3.version) FROM t_sip a3 WHERE a3.provider_id = a1.provider_id AND a3.state <> 'DELETED') );
+UPDATE t_aip a1 SET last = true WHERE a1.id = (SELECT a2.id FROM t_aip a2 WHERE a2.provider_id = a1.provider_id AND a2.version = (SELECT max(a3.version) FROM t_aip a3 WHERE a3.provider_id = a1.provider_id AND a3.state <> 'DELETED') );
+UPDATE t_aip a1 SET last = false WHERE a1.id IN (SELECT a2.id FROM t_aip a2 WHERE a2.provider_id = a1.provider_id AND a2.version <> (SELECT max(a3.version) FROM t_aip a3 WHERE a3.provider_id = a1.provider_id AND a3.state <> 'DELETED') );
 ALTER TABLE t_aip ALTER COLUMN last SET  NOT NULL;
