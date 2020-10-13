0ed353a0b4dcc2e3f079696ffa66b50ade1dd31f
Sylvain VISSIERE-GUERINET <sylvain.vissiere-guerinet@telecomnancy.net>
2020-10-13 15:36:27 +0200

    [rs-ingest] add unique constrains to fail as early as possible

--- rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.3.0__add_version.sql
+++ rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.3.0__add_version.sql
@@ -4,6 +4,7 @@ UPDATE t_sip s1 SET last = true WHERE s1.id = (SELECT s2.id FROM t_sip s2 WHERE 
 UPDATE t_sip s1 SET last = false WHERE s1.id IN (SELECT s2.id FROM t_sip s2 WHERE s2.provider_id = s1.provider_id AND s2.version <> (SELECT max(s3.version) FROM t_sip s3 WHERE s3.provider_id = s1.provider_id AND s3.state <> 'DELETED') );
 UPDATE t_sip s1 SET last = false WHERE s1.state = 'DELETED';
 ALTER TABLE t_sip ALTER COLUMN last SET  NOT NULL;
+ALTER TABLE t_sip ADD CONSTRAINT uk_sip_provider_id_last unique (last,provider_id);
 
 -- now lets handle versioning on AIPs
 ALTER TABLE t_aip ADD COLUMN last bool;
@@ -11,3 +12,4 @@ UPDATE t_aip a1 SET last = true WHERE a1.id = (SELECT a2.id FROM t_aip a2 WHERE 
 UPDATE t_aip a1 SET last = false WHERE a1.id IN (SELECT a2.id FROM t_aip a2 WHERE a2.provider_id = a1.provider_id AND a2.version <> (SELECT max(a3.version) FROM t_aip a3 WHERE a3.provider_id = a1.provider_id AND a3.state <> 'DELETED') );
 UPDATE t_aip a1 SET last = false WHERE a1.state = 'DELETED';
 ALTER TABLE t_aip ALTER COLUMN last SET  NOT NULL;
+ALTER TABLE t_aip ADD CONSTRAINT uk_aip_provider_id_last unique (last,provider_id);
