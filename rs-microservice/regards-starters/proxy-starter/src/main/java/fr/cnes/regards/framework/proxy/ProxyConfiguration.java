fe16873c218ac91529c9d9404dc82edcb6cbce93
Sylvain VISSIERE-GUERINET <sylvain.vissiere-guerinet@telecomnancy.net>
2020-11-04 11:10:02 +0100

    [rs-microservice] fix proxy-starter dependencies & add possible fix for some ssl issues(not activated for now)

--- rs-microservice/regards-starters/proxy-starter/src/main/java/fr/cnes/regards/framework/proxy/ProxyConfiguration.java
+++ rs-microservice/regards-starters/proxy-starter/src/main/java/fr/cnes/regards/framework/proxy/ProxyConfiguration.java
@@ -18,7 +18,6 @@
  */
 package fr.cnes.regards.framework.proxy;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,9 +31,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
@@ -45,7 +42,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import fr.cnes.httpclient.HttpClientFactory;
-import fr.cnes.httpclient.HttpClientFactory.Type;
 
 /**
  * Creates a {@link HttpClient} with proxy configuration.
@@ -85,21 +81,34 @@ public class ProxyConfiguration {
         LOGGER.info("####################################");
         LOGGER.info("#### REGARDS HTTP Proxy enabled ####");
         LOGGER.info("####################################");
+        //  Kept in case there is some server that do not support being ask to create a TLSv1 connection while we can also speak in TLSv1.2...
+        //  This allows use to force the usage of only TLSv1.2
+        //  You just need to add a call to HttpClientBuilder#setSSLSocketFactor(sslsf) to activate it
+        //        // specify some SSL parameter for clients only
+        //        SSLContext sslcontext = SSLContexts.createDefault();
+        //        // Allow TLSv1.2 protocol only
+        //        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext,
+        //                                                                          new String[] { "TLSv1.2" },
+        //                                                                          null,
+        //                                                                          SSLConnectionSocketFactory
+        //                                                                                  .getDefaultHostnameVerifier());
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
         if ((proxyHost != null) && !proxyHost.isEmpty()) {
             HttpClientBuilder builder = HttpClientBuilder.create();
             HttpHost proxy = new HttpHost(proxyHost, proxyPort);
-            if (((proxyLogin != null) && !proxyLogin.isEmpty())
-                    && ((proxyPassword != null) && !proxyPassword.isEmpty())) {
+            if (((proxyLogin != null) && !proxyLogin.isEmpty()) && ((proxyPassword != null) && !proxyPassword
+                    .isEmpty())) {
                 CredentialsProvider credsProvider = new BasicCredentialsProvider();
                 credsProvider.setCredentials(new AuthScope(proxy.getHostName(), proxy.getPort()),
                                              new UsernamePasswordCredentials(proxyLogin, proxyPassword));
                 builder.setDefaultCredentialsProvider(credsProvider);
             }
-            if(noProxy!=null) {
+            if (noProxy != null) {
                 HttpRoutePlanner routePlannerHandlingNoProxy = new DefaultProxyRoutePlanner(proxy) {
 
                     @Override
-                    public HttpRoute determineRoute(final HttpHost host, final HttpRequest request, final HttpContext context) throws HttpException {
+                    public HttpRoute determineRoute(final HttpHost host, final HttpRequest request,
+                            final HttpContext context) throws HttpException {
                         String hostname = host.getHostName();
                         if (noProxy.contains(hostname)) {
                             // Return direct route
@@ -108,11 +117,11 @@ public class ProxyConfiguration {
                         return super.determineRoute(host, request, context);
                     }
                 };
-                return HttpClientBuilder.create().setProxy(proxy).setRoutePlanner(routePlannerHandlingNoProxy).build();
+                return httpClientBuilder.setProxy(proxy).setRoutePlanner(routePlannerHandlingNoProxy).build();
             }
-            return HttpClientBuilder.create().setProxy(proxy).build();
+            return httpClientBuilder.setProxy(proxy).build();
         } else {
-            return HttpClientBuilder.create().build();
+            return httpClientBuilder.build();
         }
     }
 
@@ -124,7 +133,9 @@ public class ProxyConfiguration {
     private HttpClient buildJspNegoClient() {
         // https://github.com/CNES/JSPNego
         if ((proxyHost != null) && !proxyHost.isEmpty()) {
-            LOGGER.info("HTTP Proxy initialized with values host={}, port={},login={}", proxyHost, proxyPort,
+            LOGGER.info("HTTP Proxy initialized with values host={}, port={},login={}",
+                        proxyHost,
+                        proxyPort,
                         proxyLogin);
             if (proxyPort != null) {
                 fr.cnes.httpclient.configuration.ProxyConfiguration.HTTP_PROXY.setValue(proxyHost + ":" + proxyPort);
@@ -132,18 +143,18 @@ public class ProxyConfiguration {
                 fr.cnes.httpclient.configuration.ProxyConfiguration.HTTP_PROXY.setValue(proxyHost);
             }
             if ((noProxy != null) && !noProxy.isEmpty()) {
-                fr.cnes.httpclient.configuration.ProxyConfiguration.NO_PROXY.setValue(noProxy.stream().collect(
-                        Collectors.joining(",")));
+                fr.cnes.httpclient.configuration.ProxyConfiguration.NO_PROXY
+                        .setValue(noProxy.stream().collect(Collectors.joining(",")));
             }
             if ((proxyLogin != null) && (proxyPassword != null)) {
                 fr.cnes.httpclient.configuration.ProxyConfiguration.USERNAME.setValue(proxyLogin);
                 fr.cnes.httpclient.configuration.ProxyConfiguration.PASSWORD.setValue(proxyPassword);
             }
 
-            return HttpClientFactory.create(Type.PROXY_BASIC);
+            return HttpClientFactory.create(HttpClientFactory.Type.PROXY_BASIC);
         } else {
             LOGGER.info("No HTTP Proxy configured.");
-            return HttpClientFactory.create(Type.NO_PROXY);
+            return HttpClientFactory.create(HttpClientFactory.Type.NO_PROXY);
         }
     }
 
