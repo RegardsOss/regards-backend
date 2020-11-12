a7eed0776dec87acbf2f4907d4bfbaec1dae239f
Arnaud Bos <arnaud@monkeypatch.io>
2020-11-12 11:14:22 +0100

    [rs-storage] fix: wrong content types for quota API

--- rs-storage/storage/storage-client/src/main/java/fr/cnes/regards/modules/storage/client/IStorageDownloadQuotaRestClient.java
+++ rs-storage/storage/storage-client/src/main/java/fr/cnes/regards/modules/storage/client/IStorageDownloadQuotaRestClient.java
@@ -24,6 +24,7 @@ import fr.cnes.regards.modules.storage.domain.database.DefaultDownloadQuotaLimit
 import fr.cnes.regards.modules.storage.domain.database.UserCurrentQuotas;
 import fr.cnes.regards.modules.storage.domain.dto.quota.DownloadQuotaLimitsDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,33 +45,47 @@ public interface IStorageDownloadQuotaRestClient {
 
     public static final String USER_EMAIL_PARAM = "user_email";
 
-    @GetMapping(value = PATH_DEFAULT_QUOTA)
+    @GetMapping(value = PATH_DEFAULT_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<DefaultDownloadQuotaLimits> getDefaultDownloadQuotaLimits();
 
-    @PutMapping(value = PATH_DEFAULT_QUOTA)
+    @PutMapping(value = PATH_DEFAULT_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<DefaultDownloadQuotaLimits> changeDefaultDownloadQuotaLimits(@Valid @RequestBody DefaultDownloadQuotaLimits newDefaults);
 
-    @GetMapping(path = PATH_USER_QUOTA)
+    @GetMapping(path = PATH_USER_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<DownloadQuotaLimitsDto> getQuotaLimits(@PathVariable(USER_EMAIL_PARAM) String userEmail);
 
-    @PutMapping(path = PATH_USER_QUOTA)
+    @PutMapping(path = PATH_USER_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<DownloadQuotaLimitsDto> upsertQuotaLimits(
         @PathVariable(USER_EMAIL_PARAM) String userEmail,
         @Valid @RequestBody DownloadQuotaLimitsDto quotaLimits);
 
-    @GetMapping(path = PATH_QUOTA_LIST)
+    @GetMapping(path = PATH_QUOTA_LIST,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<List<DownloadQuotaLimitsDto>> getQuotaLimits(@RequestParam(value = USER_EMAIL_PARAM) String[] userEmails);
 
-    @GetMapping(path = PATH_QUOTA)
+    @GetMapping(path = PATH_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<DownloadQuotaLimitsDto> getQuotaLimits();
 
-    @GetMapping(path = PATH_CURRENT_QUOTA)
+    @GetMapping(path = PATH_CURRENT_QUOTA,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     ResponseEntity<UserCurrentQuotas> getCurrentQuotas();
 }
