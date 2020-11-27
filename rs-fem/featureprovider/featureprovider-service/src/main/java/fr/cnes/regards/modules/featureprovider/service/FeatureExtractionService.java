c471eacb880a574f944d085371eee926331f5a24
Marc SORDI <marc.sordi@csgroup.eu>
2020-11-27 16:04:36 +0100

    [rs-fem] [#197212] propagate override properties

--- rs-fem/featureprovider/featureprovider-service/src/main/java/fr/cnes/regards/modules/featureprovider/service/FeatureExtractionService.java
+++ rs-fem/featureprovider/featureprovider-service/src/main/java/fr/cnes/regards/modules/featureprovider/service/FeatureExtractionService.java
@@ -328,11 +328,11 @@ public class FeatureExtractionService implements IFeatureExtractionService {
             FeatureCreationMetadataEntity metadata = request.getMetadata();
             StorageMetadata[] array = new StorageMetadata[metadata.getStorages().size()];
             array = metadata.getStorages().toArray(array);
-            return FeatureCreationRequestEvent.build(request.getRequestOwner(), request.getRequestId(),
-                                                     FeatureCreationSessionMetadata
-                                                             .build(metadata.getSessionOwner(), metadata.getSession(),
-                                                                    request.getPriority(), false, array),
-                                                     feature);
+            return FeatureCreationRequestEvent
+                    .build(request.getRequestOwner(), request.getRequestId(),
+                           FeatureCreationSessionMetadata.build(metadata.getSessionOwner(), metadata.getSession(),
+                                                                request.getPriority(), metadata.isOverride(), array),
+                           feature);
         } catch (ModuleException e) {
             throw new ModuleException(String.format("Error generating feature for request %s", request.getRequestId()),
                     e);
