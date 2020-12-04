3d312d8617016060fc68805a111c521272a4beb7
sbinda <sebastien.binda@c-s.fr>
2020-12-04 12:05:17 +0100

    [rs-order] Fix missing tenant  before call to multitenant  repository

--- rs-order/rs-processing/processing/processing-order-processes/src/main/java/fr/cnes/regards/modules/processing/repository/OrderProcessRepositoryImpl.java
+++ rs-order/rs-processing/processing/processing-order-processes/src/main/java/fr/cnes/regards/modules/processing/repository/OrderProcessRepositoryImpl.java
@@ -24,26 +24,20 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
 import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
-import fr.cnes.regards.framework.utils.plugins.PluginUtils;
 import fr.cnes.regards.framework.utils.plugins.exception.NotAvailablePluginConfigurationException;
 import fr.cnes.regards.modules.processing.domain.PBatch;
 import fr.cnes.regards.modules.processing.domain.PProcess;
-import fr.cnes.regards.modules.processing.domain.PUserAuth;
 import fr.cnes.regards.modules.processing.domain.repository.IPProcessRepository;
 import fr.cnes.regards.modules.processing.domain.repository.IWorkloadEngineRepository;
-import fr.cnes.regards.modules.processing.domain.service.IRoleCheckerService;
 import fr.cnes.regards.modules.processing.entity.RightsPluginConfiguration;
 import fr.cnes.regards.modules.processing.order.OrderProcessInfoMapper;
 import fr.cnes.regards.modules.processing.plugins.IProcessDefinition;
-import fr.cnes.regards.modules.processing.plugins.exception.RightsPluginConfigurationNotFoundException;
 import io.vavr.collection.Map;
 import io.vavr.control.Try;
 import reactor.core.publisher.Flux;
@@ -57,8 +51,6 @@ import reactor.core.publisher.Mono;
 @Component
 public class OrderProcessRepositoryImpl implements IPProcessRepository {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProcessRepositoryImpl.class);
-
     private final IPluginService pluginService;
 
     private final IWorkloadEngineRepository enginRepo;
@@ -67,30 +59,32 @@ public class OrderProcessRepositoryImpl implements IPProcessRepository {
 
     private final IRuntimeTenantResolver tenantResolver;
 
-    private final IRoleCheckerService roleChecker;
-
     @Autowired
     public OrderProcessRepositoryImpl(IPluginService pluginService, IWorkloadEngineRepository enginRepo,
-            IRightsPluginConfigurationRepository rightsPluginConfigRepo, IRuntimeTenantResolver tenantResolver,
-            IRoleCheckerService roleChecker) {
+            IRightsPluginConfigurationRepository rightsPluginConfigRepo, IRuntimeTenantResolver tenantResolver) {
         this.pluginService = pluginService;
         this.enginRepo = enginRepo;
         this.rightsPluginConfigRepo = rightsPluginConfigRepo;
         this.tenantResolver = tenantResolver;
-        this.roleChecker = roleChecker;
     }
 
     @Override
     public Flux<PProcess> findAllByTenant(String tenant) {
-        return Flux.fromIterable(rightsPluginConfigRepo.findAll())
+        tenantResolver.forceTenant(tenant);
+        Flux<PProcess> process = Flux.fromIterable(rightsPluginConfigRepo.findAll())
                 .flatMap(rights -> buildPProcess(tenant, rights));
+        tenantResolver.clearTenant();
+        return process;
     }
 
     @Override
     public Mono<PProcess> findByTenantAndProcessBusinessID(String tenant, UUID processId) {
-        return rightsPluginConfigRepo.findByPluginConfigurationBusinessId(processId.toString()).map(Mono::just)
-                .getOrElse(() -> Mono.error(new RuntimeException("Unfound " + processId)))
+        tenantResolver.forceTenant(tenant);
+        Mono<PProcess> process = rightsPluginConfigRepo.findByPluginConfigurationBusinessId(processId.toString())
+                .map(Mono::just).getOrElse(() -> Mono.error(new RuntimeException("Unfound " + processId)))
                 .flatMap(rights -> buildPProcess(tenant, rights));
+        tenantResolver.clearTenant();
+        return process;
     }
 
     @Override
@@ -109,30 +103,15 @@ public class OrderProcessRepositoryImpl implements IPProcessRepository {
 
     public Mono<PProcess> fromPlugin(RightsPluginConfiguration rpc, IProcessDefinition processDef, String tenant) {
         OrderProcessInfoMapper mapper = new OrderProcessInfoMapper();
-        return tryToMono(processDef.sizeForecast())
-            .flatMap(sizeForecast -> tryToMono(processDef.durationForecast())
-                .flatMap(durationForecast -> enginRepo.findByName(processDef.engineName())
-                    .map(engine -> {
-                        PluginConfiguration pc = rpc.getPluginConfiguration();
-                        return new PProcess.ConcretePProcess(
-                            UUID.fromString(pc.getBusinessId()),
-                            pc.getLabel(),
-                            addTenantRole(
-                                mapper.toMap(processDef.processInfo()),
-                                tenant,
-                                rpc.getRole()
-                            ),
-                            pc.isActive(),
-                            processDef.batchChecker(),
-                            processDef.executionChecker(),
-                            processDef.parameters(),
-                            sizeForecast,
-                            durationForecast,
-                            engine,
-                            processDef.executable(),
-                            processDef.inputOutputMapper()
-                        );
-                    })));
+        return tryToMono(processDef.sizeForecast()).flatMap(sizeForecast -> tryToMono(processDef.durationForecast())
+                .flatMap(durationForecast -> enginRepo.findByName(processDef.engineName()).map(engine -> {
+                    PluginConfiguration pc = rpc.getPluginConfiguration();
+                    return new PProcess.ConcretePProcess(UUID.fromString(pc.getBusinessId()), pc.getLabel(),
+                            addTenantRole(mapper.toMap(processDef.processInfo()), tenant, rpc.getRole()), pc.isActive(),
+                            processDef.batchChecker(), processDef.executionChecker(), processDef.parameters(),
+                            sizeForecast, durationForecast, engine, processDef.executable(),
+                            processDef.inputOutputMapper());
+                })));
     }
 
     private Map<String, String> addTenantRole(Map<String, String> processInfo, String tenant, String role) {
@@ -146,7 +125,9 @@ public class OrderProcessRepositoryImpl implements IPProcessRepository {
     private Optional<IProcessDefinition> getOptionalPlugin(String tenant, String processName)
             throws NotAvailablePluginConfigurationException {
         tenantResolver.forceTenant(tenant);
-        return pluginService.getOptionalPlugin(processName);
+        Optional<IProcessDefinition> process = pluginService.getOptionalPlugin(processName);
+        tenantResolver.clearTenant();
+        return process;
     }
 
     private <T> Mono<T> fromOptional(Callable<Optional<T>> copt) {
