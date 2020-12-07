73ea2e54f5bed5ef2236da870a4c80e5e28ea519
sbinda <sebastien.binda@c-s.fr>
2020-12-07 17:43:58 +0100

    [rs-order] Handle processing error  state in compute order status

--- rs-order/order/order-service/src/main/java/fr/cnes/regards/modules/order/service/OrderDataFileService.java
+++ rs-order/order/order-service/src/main/java/fr/cnes/regards/modules/order/service/OrderDataFileService.java
@@ -18,9 +18,37 @@
  */
 package fr.cnes.regards.modules.order.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
 import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
+
 import feign.Response;
 import fr.cnes.regards.framework.authentication.IAuthenticationResolver;
 import fr.cnes.regards.framework.feign.security.FeignSecurityManager;
@@ -31,24 +59,13 @@ import fr.cnes.regards.framework.utils.file.DownloadUtils;
 import fr.cnes.regards.modules.order.dao.IFilesTasksRepository;
 import fr.cnes.regards.modules.order.dao.IOrderDataFileRepository;
 import fr.cnes.regards.modules.order.dao.IOrderRepository;
-import fr.cnes.regards.modules.order.domain.*;
+import fr.cnes.regards.modules.order.domain.DatasetTask;
+import fr.cnes.regards.modules.order.domain.FileState;
+import fr.cnes.regards.modules.order.domain.FilesTask;
+import fr.cnes.regards.modules.order.domain.Order;
+import fr.cnes.regards.modules.order.domain.OrderDataFile;
+import fr.cnes.regards.modules.order.domain.OrderStatus;
 import fr.cnes.regards.modules.storage.client.IStorageRestClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URL;
-import java.time.OffsetDateTime;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author oroussel
@@ -114,11 +131,10 @@ public class OrderDataFileService implements IOrderDataFileService {
         FilesTask filesTask = filesTasksRepository.findDistinctByFilesContaining(dataFile);
         // In case FilesTask does not yet exist
         if (filesTask != null) {
-            if (filesTask.getFiles().stream().allMatch(f -> (f.getState() == FileState.DOWNLOADED)
-                    || (f.getState() == FileState.ERROR)
-                    || (f.getState() == FileState.DOWNLOAD_ERROR)
-                    || (f.getState() == FileState.PROCESSING_ERROR))
-            ) {
+            if (filesTask.getFiles().stream()
+                    .allMatch(f -> (f.getState() == FileState.DOWNLOADED) || (f.getState() == FileState.ERROR)
+                            || (f.getState() == FileState.DOWNLOAD_ERROR)
+                            || (f.getState() == FileState.PROCESSING_ERROR))) {
                 filesTask.setEnded(true);
             }
             // ...and if it is waiting for user
@@ -143,15 +159,15 @@ public class OrderDataFileService implements IOrderDataFileService {
     @Override
     public void launchNextFilesTasks(Iterable<OrderDataFile> dataFiles) {
         // Look at FilesTasks if they are ended (no more file to download)...
-        List<FilesTask> filesTasks = filesTasksRepository.findDistinctByFilesIn(io.vavr.collection.List.ofAll(dataFiles).toJavaList());
+        List<FilesTask> filesTasks = filesTasksRepository
+                .findDistinctByFilesIn(io.vavr.collection.List.ofAll(dataFiles).toJavaList());
         Long orderId = null;
         // Update all these FileTasks
         for (FilesTask filesTask : filesTasks) {
-            if (filesTask.getFiles().stream().allMatch(f ->
-                    (f.getState() == FileState.DOWNLOADED)
-                    || (f.getState() == FileState.ERROR)
-                    || (f.getState() == FileState.DOWNLOAD_ERROR)
-                    || (f.getState() == FileState.PROCESSING_ERROR))) {
+            if (filesTask.getFiles().stream()
+                    .allMatch(f -> (f.getState() == FileState.DOWNLOADED) || (f.getState() == FileState.ERROR)
+                            || (f.getState() == FileState.DOWNLOAD_ERROR)
+                            || (f.getState() == FileState.PROCESSING_ERROR))) {
                 filesTask.setEnded(true);
             }
             // Save order id for later
@@ -273,11 +289,12 @@ public class OrderDataFileService implements IOrderDataFileService {
         // Map { order_id -> treated files size  }
         Map<Long, Long> treatedSizeMap = repos
                 .selectSumSizesByOrderIdAndStates(now, FileState.AVAILABLE, FileState.DOWNLOADED,
-                        FileState.DOWNLOAD_ERROR, FileState.PROCESSING_ERROR, FileState.ERROR)
+                                                  FileState.DOWNLOAD_ERROR, FileState.PROCESSING_ERROR, FileState.ERROR)
                 .stream().collect(Collectors.toMap(getOrderIdFct, getValueFct));
         // Map { order_id -> files in error count } Files with status DOWNLOAD_ERROR are not taken into account
         // because they are not considered as errors (available from storage)
-        Map<Long, Long> errorCountMap = repos.selectCountFilesByOrderIdAndStates(now, FileState.ERROR).stream()
+        Map<Long, Long> errorCountMap = repos
+                .selectCountFilesByOrderIdAndStates(now, FileState.ERROR, FileState.PROCESSING_ERROR).stream()
                 .collect(Collectors.toMap(getOrderIdFct, getValueFct));
         // Map {order_id -> available files count }
         Map<Long, Long> availableCountMap = repos.selectCountFilesByOrderIdAndStates4AllOrders(now, FileState.AVAILABLE)
