d1ee3bfaf009b309c9a02298bce3035ec24b287b
sbinda <sebastien.binda@c-s.fr>
2020-12-22 12:39:01 +0100

    [rs-order] Use new proxy for Feign responses

--- rs-order/rs-processing/processing/processing-order-processes/src/main/java/fr/cnes/regards/modules/processing/service/DownloadService.java
+++ rs-order/rs-processing/processing/processing-order-processes/src/main/java/fr/cnes/regards/modules/processing/service/DownloadService.java
@@ -17,7 +17,33 @@
 */
 package fr.cnes.regards.modules.processing.service;
 
+import static fr.cnes.regards.modules.processing.exceptions.ProcessingException.mustWrap;
+import static fr.cnes.regards.modules.processing.exceptions.ProcessingExceptionType.EXTERNAL_DOWNLOAD_ERROR;
+import static fr.cnes.regards.modules.processing.exceptions.ProcessingExceptionType.INTERNAL_DOWNLOAD_ERROR;
+import static fr.cnes.regards.modules.processing.utils.ReactorErrorTransformers.errorWithContextMono;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Proxy;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
 import feign.Response;
+import fr.cnes.regards.framework.feign.ResponseStreamProxy;
 import fr.cnes.regards.framework.feign.security.FeignSecurityManager;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
 import fr.cnes.regards.framework.security.role.DefaultRole;
@@ -30,32 +56,8 @@ import fr.cnes.regards.modules.processing.order.OrderInputFileMetadata;
 import fr.cnes.regards.modules.processing.order.OrderInputFileMetadataMapper;
 import fr.cnes.regards.modules.storage.client.IStorageRestClient;
 import io.vavr.collection.Set;
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Proxy;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-
-import static fr.cnes.regards.modules.processing.exceptions.ProcessingException.mustWrap;
-import static fr.cnes.regards.modules.processing.exceptions.ProcessingExceptionType.EXTERNAL_DOWNLOAD_ERROR;
-import static fr.cnes.regards.modules.processing.exceptions.ProcessingExceptionType.INTERNAL_DOWNLOAD_ERROR;
-import static fr.cnes.regards.modules.processing.utils.ReactorErrorTransformers.errorWithContextMono;
 
 /**
  * This class provides implementations for downloading files from storage or through a proxy.
@@ -72,14 +74,16 @@ public class DownloadService implements IDownloadService {
     private static final DataBufferFactory bufferFactory = new DefaultDataBufferFactory();
 
     private final Proxy proxy;
+
     private final Set<String> nonProxyHosts;
+
     private final IStorageRestClient storageClient;
+
     private final IRuntimeTenantResolver runtimeTenantResolver;
 
     @Autowired
     public DownloadService(Proxy proxy, @Qualifier("nonProxyHosts") Set<String> nonProxyHosts,
-                           IStorageRestClient storageClient,
-                           IRuntimeTenantResolver runtimeTenantResolver) {
+            IStorageRestClient storageClient, IRuntimeTenantResolver runtimeTenantResolver) {
         this.proxy = proxy;
         this.nonProxyHosts = nonProxyHosts;
         this.storageClient = storageClient;
@@ -89,7 +93,7 @@ public class DownloadService implements IDownloadService {
     @Override
     public Mono<Path> download(PInputFile file, Path dest) {
         return createParentFolderIfNeeded(dest).flatMap(d -> discriminateInternalExternal(file, d))
-            .doOnError(t -> LOGGER.error("Failed to download {} into {}", file, dest, t));
+                .doOnError(t -> LOGGER.error("Failed to download {} into {}", file, dest, t));
     }
 
     private Mono<Path> discriminateInternalExternal(PInputFile file, Path dest) {
@@ -106,9 +110,8 @@ public class DownloadService implements IDownloadService {
     }
 
     private Mono<Path> internalDownload(String checksum, Path dest) {
-        return Mono.subscriberContext()
-            .map(ctx -> ctx.get(PExecution.class))
-            .flatMap(exec -> internalDownloadWithTenant(checksum, dest, exec.getTenant(), exec.getUserName()));
+        return Mono.subscriberContext().map(ctx -> ctx.get(PExecution.class))
+                .flatMap(exec -> internalDownloadWithTenant(checksum, dest, exec.getTenant(), exec.getUserName()));
     }
 
     private Mono<Path> internalDownloadWithTenant(String checksum, Path dest, String tenant, String user) {
@@ -116,40 +119,33 @@ public class DownloadService implements IDownloadService {
             Files.createDirectories(dest.getParent());
             Flux<DataBuffer> dataBufferFlux = downloadUsingStorageRestClient(tenant, user, checksum);
             return DataBufferUtils.write(dataBufferFlux, dest, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-        })
-        .flatMap(voidMono -> voidMono.map(n -> dest))
-        .onErrorResume(mustWrap(),
-                       errorWithContextMono(PExecution.class, (exec, t) -> new InternalDownloadException(exec,
-                               "Failed to download internal " + checksum + " into " + dest, t)));
+        }).flatMap(voidMono -> voidMono.map(n -> dest))
+                .onErrorResume(mustWrap(),
+                               errorWithContextMono(PExecution.class, (exec, t) -> new InternalDownloadException(exec,
+                                       "Failed to download internal " + checksum + " into " + dest, t)));
     }
 
     public Flux<DataBuffer> downloadUsingStorageRestClient(String tenant, String user, String checksum) {
-        return DataBufferUtils.readInputStream(
-            () -> {
-                try {
-                    runtimeTenantResolver.forceTenant(tenant);
-                    FeignSecurityManager.asUser(user, DefaultRole.PROJECT_ADMIN.name());
-                    Response response = storageClient.downloadFile(checksum);
-                    HttpStatus httpStatus = HttpStatus.valueOf(response.status());
-                    if (httpStatus.is2xxSuccessful()) {
-                        return response.body().asInputStream();
-                    } else if (httpStatus == HttpStatus.TOO_MANY_REQUESTS) {
-                        throw new DownloadQuotaExceededException(user, checksum);
-                    }
-                    else {
-                        throw new IOException(
-                                String.format("Internal download failed for user %s for checksum %s, storage answered with status %s",
-                                        user, checksum, response.status()));
-                    }
+        return DataBufferUtils.readInputStream(() -> {
+            try {
+                runtimeTenantResolver.forceTenant(tenant);
+                FeignSecurityManager.asUser(user, DefaultRole.PROJECT_ADMIN.name());
+                Response response = storageClient.downloadFile(checksum);
+                HttpStatus httpStatus = HttpStatus.valueOf(response.status());
+                if (httpStatus.is2xxSuccessful()) {
+                    return new ResponseStreamProxy(response);
+                } else if (httpStatus == HttpStatus.TOO_MANY_REQUESTS) {
+                    throw new DownloadQuotaExceededException(user, checksum);
+                } else {
+                    throw new IOException(String
+                            .format("Internal download failed for user %s for checksum %s, storage answered with status %s",
+                                    user, checksum, response.status()));
                 }
-                finally {
-                    FeignSecurityManager.reset();
-                    runtimeTenantResolver.clearTenant();
-                }
-            },
-            bufferFactory,
-            4096
-        );
+            } finally {
+                FeignSecurityManager.reset();
+                runtimeTenantResolver.clearTenant();
+            }
+        }, bufferFactory, 4096);
     }
 
     private Mono<Path> externalDownload(URL url, Path dest) {
