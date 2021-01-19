528b962d17189264bb1a7b380333788b14bcf720
sbinda <sebastien.binda@c-s.fr>
2021-01-19 14:40:30 +0100

    [rs-access] Update license

--- rs-access/access-services/access-services-client/src/test/java/fr/cnes/regards/modules/access/services/client/ServiceAggregatorClientIT.java
+++ rs-access/access-services/access-services-client/src/test/java/fr/cnes/regards/modules/access/services/client/ServiceAggregatorClientIT.java
@@ -3,7 +3,7 @@ package fr.cnes.regards.modules.access.services.client;
 import java.util.Arrays;
 
 /*
- * Copyright 2017-2020 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2017-2021 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  *
  * This file is part of REGARDS.
  *
