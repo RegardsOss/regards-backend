486dfc8cfcd27f99f259987d784ca48165da62b4
Arnaud Bos <arnaud@monkeypatch.io>
2021-03-03 14:20:48 +0100

    [rs-cloud] Create public endpoint for get service provider element

--- rs-cloud/rs-authentication/authentication/authentication-rest/src/main/java/fr/cnes/regards/modules/authentication/rest/ServiceProviderPublicController.java
+++ rs-cloud/rs-authentication/authentication/authentication-rest/src/main/java/fr/cnes/regards/modules/authentication/rest/ServiceProviderPublicController.java
@@ -4,10 +4,13 @@ import fr.cnes.regards.framework.hateoas.IResourceController;
 import fr.cnes.regards.framework.hateoas.IResourceService;
 import fr.cnes.regards.framework.hateoas.LinkRels;
 import fr.cnes.regards.framework.hateoas.MethodParamFactory;
+import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.security.annotation.ResourceAccess;
 import fr.cnes.regards.framework.security.role.DefaultRole;
+import fr.cnes.regards.modules.authentication.domain.data.ServiceProvider;
 import fr.cnes.regards.modules.authentication.domain.service.IServiceProviderCrudService;
+import fr.cnes.regards.modules.authentication.rest.dto.ServiceProviderDto;
 import fr.cnes.regards.modules.authentication.rest.dto.ServiceProviderPublicDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +24,16 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
 import java.util.function.Function;
+
+import static com.google.common.base.Predicates.instanceOf;
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
 
 @RestController
 public class ServiceProviderPublicController implements IResourceController<ServiceProviderPublicDto> {
@@ -32,6 +41,8 @@ public class ServiceProviderPublicController implements IResourceController<Serv
     private static final Logger LOGGER = LoggerFactory.getLogger(ServiceProviderPublicController.class);
 
     public static final String PATH_SERVICE_PROVIDER_PUBLIC = "/serviceproviders/public";
+
+    public static final String PATH_SERVICE_PROVIDER_PUBLIC_BY_NAME = "/serviceproviders/{name}/public";
 
     @Autowired
     private IServiceProviderCrudService serviceProviderCrud;
@@ -50,6 +61,22 @@ public class ServiceProviderPublicController implements IResourceController<Serv
             .map(page -> page.map(ServiceProviderPublicDto::new))
             .map(page -> new ResponseEntity<>(toPagedResources(page, assembler), HttpStatus.OK))
             .getOrElseThrow((Function<Throwable, ModuleException>) ModuleException::new);
+    }
+
+    @ResponseBody
+    @GetMapping(value = PATH_SERVICE_PROVIDER_PUBLIC_BY_NAME)
+    @ResourceAccess(description = "Retrieve the service provider.", role = DefaultRole.PUBLIC)
+    public ResponseEntity<EntityModel<ServiceProviderPublicDto>> getServiceProvider(@PathVariable("name") String name)
+    {
+        //noinspection unchecked
+        return serviceProviderCrud.findByName(name)
+            .map(ServiceProviderPublicDto::new)
+            .map(sp -> new ResponseEntity<>(toResource(sp), HttpStatus.OK))
+            .mapFailure(
+                Case($(instanceOf(NoSuchElementException.class)), ex -> new EntityNotFoundException(name, ServiceProvider.class)),
+                Case($(), (Function<Throwable, ModuleException>) ModuleException::new)
+            )
+            .get();
     }
 
     @Override
