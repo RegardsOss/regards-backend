d4f270be3a5b2cb1f2481d4d0a7998ca08a524f4
Arnaud Bos <arnaud@monkeypatch.io>
2021-03-17 14:01:26 +0100

    [rs-cloud] Tinker with external auth

--- rs-cloud/rs-gateway/bootstrap-gateway/src/main/java/fr/cnes/regards/cloud/gateway/Application.java
+++ rs-cloud/rs-gateway/bootstrap-gateway/src/main/java/fr/cnes/regards/cloud/gateway/Application.java
@@ -25,7 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Class GatewayApplication
@@ -37,7 +37,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableEurekaClient
-@EnableFeignClients
+//@EnableFeignClients
 public class Application { // NOSONAR
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
