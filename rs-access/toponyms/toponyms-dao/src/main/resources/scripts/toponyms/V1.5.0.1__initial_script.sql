da22b5a8bbc1a30f5eee839717caf43792c46d8b
sbinda <sebastien.binda@c-s.fr>
2021-03-26 11:53:17 +0100

    [rs-access] Fix toponyms postigs db init for CNES env

--- rs-access/toponyms/toponyms-dao/src/main/resources/scripts/toponyms/V1.5.0.1__initial_script.sql
+++ rs-access/toponyms/toponyms-dao/src/main/resources/scripts/toponyms/V1.5.0.1__initial_script.sql
@@ -1,1 +1,1 @@
-create table t_toponyms (bid varchar(16) not null UNIQUE, label varchar(256) NOT NULL, label_fr varchar(256) NOT NULL, copyright varchar(512), description varchar(512), geom geometry(Geometry,4326), primary key (bid));
+create table t_toponyms (bid varchar(16) not null UNIQUE, label varchar(256) NOT NULL, label_fr varchar(256) NOT NULL, copyright varchar(512), description varchar(512), geom public.geometry(Geometry,4326), primary key (bid));
