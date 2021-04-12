3044ba21902767377e9cf45d0601e5b656c11263
Patrice FABRE <patrice.fabre@csgroup.eu>
2021-04-12 10:29:23 +0200

    [rs-microservice] feature/PM77  - add common methods to Abstract Setting Service

--- rs-microservice/regards-modules/tenant-settings/tenant-settings-service/src/main/java/fr/cnes/regards/framework/modules/tenant/settings/service/AbstractSettingService.java
+++ rs-microservice/regards-modules/tenant-settings/tenant-settings-service/src/main/java/fr/cnes/regards/framework/modules/tenant/settings/service/AbstractSettingService.java
@@ -1,16 +1,21 @@
 package fr.cnes.regards.framework.modules.tenant.settings.service;
 
 import fr.cnes.regards.framework.jpa.utils.RegardsTransactional;
+import fr.cnes.regards.framework.module.rest.exception.EntityException;
 import fr.cnes.regards.framework.module.rest.exception.EntityInvalidException;
 import fr.cnes.regards.framework.module.rest.exception.EntityNotFoundException;
 import fr.cnes.regards.framework.module.rest.exception.EntityOperationForbiddenException;
 import fr.cnes.regards.framework.modules.tenant.settings.domain.DynamicTenantSetting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 @RegardsTransactional
 public abstract class AbstractSettingService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractSettingService.class);
 
     protected final IDynamicTenantSettingService dynamicTenantSettingService;
 
@@ -30,6 +35,23 @@ public abstract class AbstractSettingService {
         for (DynamicTenantSetting dynamicTenantSetting : getSettingList()) {
             dynamicTenantSettingService.delete(dynamicTenantSetting.getName());
         }
+    }
+
+    public List<DynamicTenantSetting> retrieve() {
+        return dynamicTenantSettingService.readAll();
+    }
+
+    public void update(DynamicTenantSetting dynamicTenantSetting) {
+        try {
+            dynamicTenantSettingService.update(dynamicTenantSetting.getName(), dynamicTenantSetting.getValue());
+        } catch (EntityException e) {
+            LOG.error("Unable to update setting {}", dynamicTenantSetting.getName());
+        }
+    }
+
+    public void resetSettings() throws EntityException {
+        deleteAll();
+        init();
     }
 
     public <T> T getValue(String name) {
