e6b5eb0e44fbd65d4025a92d140263209b5b3702
sbinda <sebastien.binda@c-s.fr>
2021-07-09 14:16:10 +0200

    Update catalog service helper tool

--- rs-catalog/catalog-services/catalog-services-helper/src/main/java/fr/cnes/regards/modules/catalog/services/helper/CatalogPluginResponseFactory.java
+++ rs-catalog/catalog-services/catalog-services-helper/src/main/java/fr/cnes/regards/modules/catalog/services/helper/CatalogPluginResponseFactory.java
@@ -79,9 +79,12 @@ public class CatalogPluginResponseFactory {
      * @return {@link ResponseEntity}
      */
     public static ResponseEntity<StreamingResponseBody> createStreamSuccessResponse(HttpServletResponse response,
-            StreamingResponseBody responseContent, String fileName, MediaType mimeType) {
+            StreamingResponseBody responseContent, String fileName, MediaType mimeType, Optional<Long> size) {
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.CONTENT_DISPOSITION, getContentDisposition(fileName));
+        if (size.isPresent()) {
+            headers.set(HttpHeaders.CONTENT_LENGTH, size.toString());
+        }
         if (mimeType == null) {
             headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
             response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
