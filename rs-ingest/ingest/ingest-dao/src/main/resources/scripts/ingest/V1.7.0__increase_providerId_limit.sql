0bfa018c573a0d595b1101a34f619268da2daa4d
Sebastien Binda <sebastien.binda@csgroup.eu>
2021-09-24 13:42:00 +0200

    Fix script syntax

--- rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.7.0__increase_providerId_limit.sql
+++ rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.7.0__increase_providerId_limit.sql
@@ -1,2 +1,2 @@
-ALTER TABLE t_aip ALTER COLUMN provider_id TYPE varchar(120)
-ALTER TABLE t_sip ALTER COLUMN provider_id TYPE varchar(120)
+ALTER TABLE t_aip ALTER COLUMN provider_id TYPE varchar(120);
+ALTER TABLE t_sip ALTER COLUMN provider_id TYPE varchar(120);
