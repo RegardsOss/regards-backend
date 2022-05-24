ab0efaf1e71c8f415f8b5b3cd9c435afd141815f
Binda <sebastien.binda@csgroup.eu>
2022-05-24 10:10:50 +0200

    fix(notif): Add some logs for notifier

--- rs-notifier/notifier/notifier-service/src/main/java/fr/cnes/regards/modules/notifier/service/job/NotificationJob.java
+++ rs-notifier/notifier/notifier-service/src/main/java/fr/cnes/regards/modules/notifier/service/job/NotificationJob.java
@@ -77,11 +77,13 @@ public class NotificationJob extends AbstractJob<Void> {
         logger.info("[{}] Notification job starts", jobInfoId);
         long start = System.currentTimeMillis();
         Pair<Integer, Integer> processResult = notificationProcessingService.processRequests(notificationRequests, recipient);
-        logger.info("[{}]{}{} Notifications sent in {} ms, {} notifications failed", jobInfoId, INFO_TAB,
-                    processResult.getFirst(), System.currentTimeMillis() - start, processResult.getSecond());
+        logger.info("[{}]{}{} Notifications sent to <{} - {} - {}> in {} ms, {} notifications failed", jobInfoId, INFO_TAB,
+                    processResult.getFirst(), recipient.getBusinessId(), recipient.getPluginId(), recipient.getLabel(),
+                System.currentTimeMillis() - start, processResult.getSecond());
         // if there are exception we throw an exception to stop the job in error
         if (!processResult.getSecond().equals(0)) {
-            throw new RsRuntimeException(String.format("Some Recipient failed for the Job %s", jobInfoId));
+            throw new RsRuntimeException(String.format("Some notifications (%s) failed for the Job %s for recipient <%s - %s - %s>",
+                    processResult.getSecond(), jobInfoId, recipient.getBusinessId(), recipient.getPluginId(), recipient.getLabel()));
         }
     }
 
