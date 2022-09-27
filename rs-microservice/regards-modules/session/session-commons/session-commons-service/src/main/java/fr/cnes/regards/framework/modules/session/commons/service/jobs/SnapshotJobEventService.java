01798d17f0a207b4f8df366a972baea1ea504a3f
Sebastien Binda <sebastien.binda@csgroup.eu>
2022-09-27 11:10:01 +0200

    fix(session): Fix job event handler to stop snapshot process

--- rs-microservice/regards-modules/session/session-commons/session-commons-service/src/main/java/fr/cnes/regards/framework/modules/session/commons/service/jobs/SnapshotJobEventService.java
+++ rs-microservice/regards-modules/session/session-commons/session-commons-service/src/main/java/fr/cnes/regards/framework/modules/session/commons/service/jobs/SnapshotJobEventService.java
@@ -19,9 +19,6 @@
 package fr.cnes.regards.framework.modules.session.commons.service.jobs;
 
 import fr.cnes.regards.framework.jpa.multitenant.transactional.MultitenantTransactional;
-import fr.cnes.regards.framework.modules.jobs.dao.IJobInfoRepository;
-import fr.cnes.regards.framework.modules.jobs.domain.JobInfo;
-import fr.cnes.regards.framework.modules.jobs.domain.JobStatusInfo;
 import fr.cnes.regards.framework.modules.jobs.domain.event.JobEvent;
 import fr.cnes.regards.framework.modules.jobs.domain.event.JobEventType;
 import fr.cnes.regards.framework.modules.session.commons.dao.ISnapshotProcessRepository;
@@ -31,10 +28,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * Service for {@link SnapshotJobEventHandler}. Update {@link SnapshotProcess} according to the
@@ -48,9 +42,6 @@ public class SnapshotJobEventService {
     @Autowired
     private ISnapshotProcessRepository snapshotRepo;
 
-    @Autowired
-    private IJobInfoRepository jobInfoRepo;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SnapshotJobEventService.class);
 
     /**
@@ -59,34 +50,10 @@ public class SnapshotJobEventService {
      * @param events {@link JobEvent}s
      */
     public void updateSnapshotProcess(List<JobEvent> events) {
-        // Filter out all jobs with RUNNING state
-        events.removeIf(jobEvent -> jobEvent.getJobEventType().equals(JobEventType.RUNNING));
-
-        // Filter out all jobs not related to AgentSnapshotJob ids saved in snapshot processes repo
-        List<UUID> jobIds = events.stream().map(JobEvent::getJobId).collect(Collectors.toList());
-        Set<SnapshotProcess> snapshotProcesses = this.snapshotRepo.findByJobIdIn(jobIds);
-
-        // Update snapshot processes
-        for (SnapshotProcess snapshotProcess : snapshotProcesses) {
-            Optional<JobInfo> jobInfoOpt = this.jobInfoRepo.findById(snapshotProcess.getJobId());
-
-            if (jobInfoOpt.isPresent()) {
-                JobInfo jobInfo = jobInfoOpt.get();
-                JobStatusInfo jobInfoStatus = jobInfo.getStatus();
-
-                switch (jobInfoStatus.getStatus()) {
-                    case SUCCEEDED:
-                    case FAILED:
-                    case ABORTED:
-                        snapshotProcess.setJobId(null);
-                        LOGGER.trace("Updated SnapshotProcess with source {} following a SnapshotJobEvent",
-                                     snapshotProcess.getSource());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        this.snapshotRepo.saveAll(snapshotProcesses);
+        List<UUID> terminatedJobIds = events.stream()
+                                            .filter(event -> JobEventType.runnings().contains(event.getJobEventType()))
+                                            .map(JobEvent::getJobId)
+                                            .toList();
+        this.snapshotRepo.removeTerminatedJobsById(terminatedJobIds);
     }
 }
