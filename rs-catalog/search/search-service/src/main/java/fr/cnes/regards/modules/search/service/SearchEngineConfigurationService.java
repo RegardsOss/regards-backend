960846425d974893c7add862afd0f86456a56f5f
lmieulet <leo.mieulet@csgroup.eu>
2023-04-12 16:48:28 +0200

    feat(clean): remove unused deps

--- rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/SearchEngineConfigurationService.java
+++ rs-catalog/search/search-service/src/main/java/fr/cnes/regards/modules/search/service/SearchEngineConfigurationService.java
@@ -49,7 +49,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
