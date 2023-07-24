87ae39732d7360867f9457ef3c115001f5467d0e
lmieulet <leo.mieulet@csgroup.eu>
2023-07-24 09:54:26 +0200

    feat(ingest): add missing index on last_update while removing / relaunching requests

--- rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.13.1.0__add_request_last_update.sql
+++ rs-ingest/ingest/ingest-dao/src/main/resources/scripts/ingest/V1.13.1.0__add_request_last_update.sql
@@ -1,2 +1,3 @@
 ALTER TABLE t_request
     ADD COLUMN IF NOT EXISTS last_update timestamp DEFAULT CURRENT_TIMESTAMP;
+create index IF NOT EXISTS idx_request_state_last_update on t_request (state, last_update);
