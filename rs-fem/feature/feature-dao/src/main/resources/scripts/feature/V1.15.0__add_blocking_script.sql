a1fd192b3c3758fd48121c069bcb15e0dee496d4
lmieulet <leo.mieulet@csgroup.eu>
2023-12-07 16:18:51 +0100

    fix(merge): rename migration + fix merge content

--- rs-fem/feature/feature-dao/src/main/resources/scripts/feature/V1.15.0__add_blocking_script.sql
+++
@@ -1,9 +0,0 @@
--- Add column blocking_required from recipient
-alter table t_feature_update_dissemination
-    add column blocking_required boolean default false;
--- Add column blocking from recipient
-alter table t_feature_dissemination_info
-    add column blocking boolean default false;
--- Add column acknowledged_recipient from recipient
-alter table t_feature_request
-    add column acknowledged_recipient varchar(255);
