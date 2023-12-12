76909b34d7b600c35b1f74a150e75c8a6de7e9ce
CORTINE Stephane <stephane.cortine@csgroup.eu>
2023-12-12 18:26:02 +0000

    feat(script_sql): add if not exists in the adding of columns in database

--- rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V1.15.0.1__update_file_cache_request_script.sql
+++ rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V1.15.0.1__update_file_cache_request_script.sql
@@ -1,6 +1,6 @@
 -- availability_hours column replaces expiration_date column
 -- availability_hours column: duration in hours of available files in the cache internal or external (by default 24h)
 alter table t_file_cache_request
-    add column availability_hours int2 not null default 24;
+    add column IF NOT EXISTS availability_hours int2 not null default 24;
 alter table t_file_cache_request
-    drop column if exists expiration_date;
+    drop column IF EXISTS expiration_date;
--- rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V1.15.0__update_cache_file_script.sql
+++ rs-storage/storage/storage-dao/src/main/resources/scripts/storage/V1.15.0__update_cache_file_script.sql
@@ -1,6 +1,6 @@
 -- true: internal cache / false: external cache
 alter table t_cache_file
-    add column internal_cache boolean not null default true;
+    add column IF NOT EXISTS internal_cache boolean not null default true;
 -- business identifier of plugin to access file in external cache
 alter table t_cache_file
-    add column external_cache_plugin varchar(36) default null;
+    add column IF NOT EXISTS external_cache_plugin varchar(36) default null;
