583cf9bc73a6b9d3565e495577288a20b550ac8b
Thibaud M <thibaud.michaudel@csgroup.eu>
2025-04-18 19:03:38 +0200

    fix(110): add dummy DownloadQuotaController

---
+++ rs-file-access/file-access/file-access-service/src/main/java/fr/cnes/regards/modules/fileaccess/service/DownloadQuotaService.java
@@ -0,0 +1,48 @@
+package fr.cnes.regards.modules.fileaccess.service;
+
+import fr.cnes.regards.framework.jpa.multitenant.transactional.MultitenantTransactional;
+import fr.cnes.regards.modules.fileaccess.dto.quota.DownloadQuotaLimitsDto;
+import fr.cnes.regards.modules.fileaccess.dto.quota.UserCurrentQuotasDto;
+import io.vavr.control.Try;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@MultitenantTransactional
+/**
+ * FIXME Lot 2
+ * Uses 0 quota for now
+ */ public class DownloadQuotaService<T> {
+
+    public DownloadQuotaService() {
+    }
+
+    public Try<DownloadQuotaLimitsDto> getDownloadQuotaLimits(String userEmail) {
+        return Try.of(() -> new DownloadQuotaLimitsDto(userEmail, 0L, 0L));
+    }
+
+    public Try<List<DownloadQuotaLimitsDto>> getDownloadQuotaLimits(String[] userEmails) {
+        return Try.of(() -> Arrays.stream(userEmails)
+                                  .map(userEmail -> new DownloadQuotaLimitsDto(userEmail, 0L, 0L))
+                                  .toList());
+    }
+
+    public UserCurrentQuotasDto getCurrentQuotas(String userEmail) {
+
+        return new UserCurrentQuotasDto(userEmail, 0L, 0L, 0L, 0L);
+    }
+
+    public Try<List<UserCurrentQuotasDto>> getCurrentQuotas(String[] userEmails) {
+        return Try.of(() -> Arrays.stream(userEmails).map(this::getCurrentQuotas).toList());
+    }
+
+    public Try<DownloadQuotaLimitsDto> upsertDownloadQuotaLimits(DownloadQuotaLimitsDto newLimits) {
+        return Try.of(() -> newLimits);
+    }
+
+    public Long getMaxQuota() {
+        return 0L;
+    }
+}
